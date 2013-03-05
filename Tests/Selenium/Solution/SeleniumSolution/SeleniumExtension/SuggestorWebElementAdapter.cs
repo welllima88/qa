333c8ae3faa289e2975c0e047a4cb293517062f6
Marc Siegmund <TKCDEV\siegmund>
2013-03-05 14:15:27 +0000

    added suggestor log info
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40059

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/SuggestorWebElementAdapter.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using OpenQA.Selenium;
+
+namespace SIX.SCS.QA.Selenium.Extension
+{
+    /// <summary>
+    /// This class adds to the common extension of WebElementAdapter just for suggestors. Because they will not be used in future, they are considered as separate class and interface.
+    /// </summary>
+    public class SuggestorWebElementAdapter : WebElementAdapter, ISuggesterWebElement
+    {
+        private readonly IWebDriverAdapter _driver;
+
+        public SuggestorWebElementAdapter(IWebElement webElement, IWebDriver webDriver) : base(webElement)
+        {
+            _driver = new WebDriverAdapter(webDriver);
+        }
+
+        #region ISuggesterWebElement Members
+
+        public void Suggestor(string intValue)
+        {
+            // these suggestors are soooo ?x$*! -> disabled the click on this element
+            // driver.FindElement(By.CssSelector("div.suggestBox div div[textContent='" + text + "'].active")).Click();
+            // click | //div[@inttext="Englisch [en]"] |         
+            TypeText(intValue);
+
+            // f*** o**:
+            //_driver.WaitForElementPresent(By.XPath("//div[@class='suggestBox']")).Click();
+            IWebElement e;
+            try
+            {
+                //IWebElement e = _driver.WaitForElementPresent(By.XPath("//div[@inttext='" + intValue + "']"));
+                //IWebElement e = _driver.WaitForElementPresent(By.CssSelector("div[intvalue='" + "CH"+ "']"));
+                //IWebElement e = _driver.WaitForElementPresent(By.CssSelector("div[intvalue='" + intValue + "']"));
+                e = _driver.WaitForElementPresent(By.CssSelector("div.suggestBox div.item[inttext='" + intValue + "']"));
+                e.Click();
+            }
+                //catch (NoSuchElementException)
+            catch (Exception ex)
+            {
+                Console.Out.WriteLine("suggestor element failure [" + intValue + "]" + ex.Message);
+            }
+            //_driver.WaitForElementPresent(By.CssSelector("div.suggestBox div div")).Click();                        
+        }
+
+        #endregion
+    }
+}
