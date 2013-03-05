e1e5f328ce3b09d1f189d68cfc0d585f4a559f8f
Marc Siegmund <TKCDEV\siegmund>
2013-03-05 09:48:51 +0000

    added abstract WebObject and IWebObject to encapsulate the common test object features (overriding and preventing default constructor, reference to WebDriver)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40044

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/IWebObject.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/IWebObject.cs
@@ -1,9 +1,13 @@
-using OpenQA.Selenium;
-
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects
 {
+    /// <summary>
+    /// defines interface for a test object
+    /// </summary>
     public interface IWebObject
     {
+        /// <summary>
+        /// a WebDriver reference is necessary for all selenium test objects (initialized by constructor)
+        /// </summary>
         IWebDriverAdapter Driver { get; }
     }
 }
--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/WebObject.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/WebObject.cs
@@ -1,19 +1,32 @@
-using System;
-using OpenQA.Selenium;
-
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects
 {
     public abstract class WebObject : IWebObject
     {
+        /// <summary>
+        /// This abstract class of an WebObject defines the essential constructor for initializing the WebDriver reference.
+        /// It is also used for adding features to all test objects
+        /// </summary>
+        /// <param name="driver"></param>
         protected WebObject(IWebDriverAdapter driver)
         {
             Driver = driver;
         }
-        //private WebObject()
+
+        /// <summary>
+        /// override the default constructor to prevent initializing without a WebDriver reference
+        /// </summary>
+        // private WebObject()
         //{
         //   throw new NotSupportedException("The default constructor is not allowed to be used to avoid missing webdriver reference");
         //}
 
+        #region IWebObject Members
+
+        /// <summary>
+        /// overrides the acessor for this public WebDriver reference, so that the constructor can set (internally usually by the constructor)
+        /// </summary>
         public IWebDriverAdapter Driver { get; private set; }
+
+        #endregion
     }
 }
