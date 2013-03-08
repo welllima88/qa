c3f6f0fc74be99654152e2d65364e9e42025e388
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:31:38 +0000

    used fields suffix and prefix of base class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40158

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/TerminalInfo.cs
@@ -0,0 +1,34 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
+{
+    public class TerminalInfo : WebObject
+    {
+        public TerminalInfo(IWebDriverAdapter driver) : base(driver)
+        {
+            Prefix = "div.portletSummaryColumn";
+        }
+        //TODO: better locators 
+        public string TerminalId
+        {
+            get { return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']")).Text; }
+        }
+
+        public string ExternTerminalId
+        {
+            get { return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']")).Text; }
+        }
+
+        public string Article
+        {
+            get { return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']")).Text; }
+        }
+
+        public string Status
+        {
+            get { return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']")).Text; }
+        }
+    }
+}
