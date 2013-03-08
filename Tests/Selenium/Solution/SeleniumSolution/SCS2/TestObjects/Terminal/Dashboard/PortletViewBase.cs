fefa57683bfd0ff2f27893e08e54c1138ee3347f
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:03:17 +0000

    changed PortletViewBase (using WebObject)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40149

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
@@ -1,16 +1,15 @@
-using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
 
 namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
 {
-    public abstract class PortletViewBase
+    public abstract class PortletViewBase: WebObject
     {
         protected static string Pre;
         protected static string Suf = " div.portletKeyValueRow div.treeGridValueCol";
-        protected readonly IWebDriver Driver;
-
-        protected PortletViewBase(IWebDriver driver)
-        {
-            Driver = driver;
+        
+        protected PortletViewBase(IWebDriverAdapter driver):base(driver)
+        {         
         }
     }
 }
