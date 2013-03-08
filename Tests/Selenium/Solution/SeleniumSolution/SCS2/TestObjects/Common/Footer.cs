aa85db05f2409ad90ea00352a1e75b481d1da391
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 15:47:15 +0000

    made use of prefix for footer
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40171

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Common/Footer.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Common/Footer.cs
@@ -8,26 +8,27 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common
     {
         public Footer(IWebDriverAdapter driver) : base(driver)
         {
+            Prefix = "td#footerNavigationBar>";
         }
 
         public IWebElement Feedback
         {
-            get { return Driver.FindElement(By.CssSelector("td#footerNavigationBar>a#ctl00_hplFeedback")); }
+            get { return Driver.FindElement(By.CssSelector(Prefix + "a#ctl00_hplFeedback")); }
         }
 
         public IWebElement Disclaimer
         {
-            get { return Driver.FindElement(By.CssSelector("td#footerNavigationBar>a[href*='/disclaimer.aspx']")); }
+            get { return Driver.FindElement(By.CssSelector(Prefix + "a[href*='/disclaimer.aspx']")); }
         }
 
         public IWebElement Copyright
         {
-            get { return Driver.FindElement(By.CssSelector("div#copyright")); }
+            get { return Driver.FindElement(By.CssSelector(Prefix + "div#copyright")); }
         }
 
         public IWebElement PrintPage
         {
-            get { return Driver.FindElement(By.CssSelector("td#footerFunctionBar>a[href*='JavaScript:Page.Print()']")); }
+            get { return Driver.FindElement(By.CssSelector(Prefix + "a[href*='JavaScript:Page.Print()']")); }
         }
 
         public IWebElement ScrollUp
@@ -35,7 +36,7 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common
             get
             {
                 return
-                    Driver.FindElement(By.CssSelector("td#footerFunctionBar>a[href*='JavaScript:window.scroll(0,0)']"));
+                    Driver.FindElement(By.CssSelector(Prefix + "a[href*='JavaScript:window.scroll(0,0)']"));
             }
         }
     }
