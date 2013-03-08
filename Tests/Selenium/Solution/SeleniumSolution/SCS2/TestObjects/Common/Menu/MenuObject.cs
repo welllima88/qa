7ed7cc55750b6c5b56de439e370738afe3700fc9
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 15:58:35 +0000

    created an abstract menu object, which sets and decorates typical menu features)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40177

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Common/Menu/MenuObject.cs
@@ -0,0 +1,12 @@
+﻿using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common.Menu
+{
+    public class MenuObject:WebObject
+    {
+        public MenuObject(IWebDriverAdapter driver) : base(driver)
+        {
+        }
+    }
+}
