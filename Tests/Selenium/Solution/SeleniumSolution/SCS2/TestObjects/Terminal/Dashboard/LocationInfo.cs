49748f72665db87db5221863ebc7b521c60d260b
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:32:25 +0000

    added classes for common portlet infos: Customer, [opt]Location, Terminal
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40159

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/LocationInfo.cs
@@ -0,0 +1,12 @@
+﻿using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
+{
+    public class LocationInfo:WebObject
+    {
+        public LocationInfo(IWebDriverAdapter driver) : base(driver)
+        {
+        }
+    }
+}
