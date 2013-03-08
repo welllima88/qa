0f0e282793a38b6631811154396467a2887e81d2
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:32:55 +0000

    put the common infos in a separate class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40160

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/CommonTerminalInfo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using SIX.SCS.QA.Selenium.Extension;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
+{
+    public class CommonTerminalInfo : PortletViewBase
+    {
+        public CommonTerminalInfo(IWebDriverAdapter webDriver) : base(webDriver)
+        {
+        }
+
+        public CustomerInfo Customer
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public LocationInfo Location
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public TerminalInfo Terminal
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
