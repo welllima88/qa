e13cb234bf655a291f0cc45af6237cfd77efa8f3
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:14:52 +0000

    implemented constructor with initializing all sub test objects (portlet views)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40152

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/TerminalDashboard.cs
@@ -0,0 +1,33 @@
+using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
+{
+    public class TerminalDashboard:WebObject
+    {
+        public Brands Brands;
+        public CommonTerminalInfo CommonTerminalInfo;
+        public Contacts Contacts;
+        public TechnicalView TechnicalView;
+        public TerminalSaleContract TerminalSaleContract;
+        public TerminalTexts TerminalTexts;
+        public TroubleTickets TroubleTickets;
+
+        public TerminalDashboard(IWebDriverAdapter driver):base(driver)
+        {
+
+            CommonTerminalInfo = new CommonTerminalInfo(Driver);
+            Contacts=new Contacts(Driver);
+            TechnicalView=new TechnicalView(Driver);
+            TerminalSaleContract = new TerminalSaleContract(Driver);
+            TerminalTexts = new TerminalTexts(Driver);
+            TroubleTickets=new TroubleTickets(Driver);
+        }
+
+        public IWebElement EditButton
+        {
+            get { return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']")); }
+        }
+    }
+}
