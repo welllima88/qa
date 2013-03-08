88e9e785771c676638d02044b1342b442723a428
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:28:05 +0000

    put prefix and suffix in to base class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40155

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
@@ -7,11 +7,9 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
 {
     public abstract class PortletViewBase : WebObject
     {
-        protected static string Pre;
-        protected static string Suf = " div.portletKeyValueRow div.treeGridValueCol";
-
         protected PortletViewBase(IWebDriverAdapter driver) : base(driver)
         {
+            Suffix=" div.portletKeyValueRow div.treeGridValueCol";
         }
 
         public IWebElement Refresh
