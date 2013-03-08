c25a68d60c98eddc9c79d31890c31331db4ccd54
Marc Siegmund <TKCDEV\siegmund>
2013-03-08 12:10:23 +0000

    added Minimize and Refresh element in portlet base class, so it can be used in any portlet view
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40150

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/PortletViewBase.cs
@@ -1,15 +1,35 @@
+using System;
+using OpenQA.Selenium;
 using SIX.SCS.QA.Selenium.Extension;
 using SIX.SCS.QA.Selenium.Extension.TestObjects;
 
 namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
 {
-    public abstract class PortletViewBase: WebObject
+    public abstract class PortletViewBase : WebObject
     {
         protected static string Pre;
         protected static string Suf = " div.portletKeyValueRow div.treeGridValueCol";
-        
-        protected PortletViewBase(IWebDriverAdapter driver):base(driver)
-        {         
+
+        protected PortletViewBase(IWebDriverAdapter driver) : base(driver)
+        {
+        }
+
+        public IWebElement Refresh
+        {
+            get
+            {
+                throw new NotImplementedException("Refresh to Portlet not implemented");
+                return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']"));
+            }
+        }
+
+        public IWebElement Minimize
+        {
+            get
+            {
+                throw new NotImplementedException("Refresh to Portlet not implemented");
+                return Driver.FindElement(By.CssSelector("input[value='Bearbeiten']"));
+            }
         }
     }
 }
