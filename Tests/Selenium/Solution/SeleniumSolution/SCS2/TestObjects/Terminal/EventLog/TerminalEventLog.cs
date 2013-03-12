3d60832e3e4e6cc521f8524dbabe705c3f2dc7fd
Marc Siegmund <TKCDEV\siegmund>
2013-03-12 20:23:17 +0000

    added event-Log object and basic test (elements present)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40241

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/EventLog/TerminalEventLog.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.EventLog
+{
+    public class TerminalEventLog : WebObject
+    {
+        public TerminalEventLog(IWebDriverAdapter driver)
+            : base(driver)
+        {
+        }
+
+        /// <summary>
+        /// Mulit selectable list for choosing log event entries by severity
+        /// </summary>
+        public SelectElement SeverityList
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("select#SelectedSeverities")).Selector(); }
+        }
+
+        public SelectElement TimeSpan
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("select#SelectedTimeSpan")).Selector(); }
+        }
+
+        public SelectElement EventTypes
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("select#SelectedEventTypes")).Selector(); }
+        }
+
+        public IWebElement EventTable
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("tbody tr#rowContent td#content table")); }
+        }
+
+        public ReadOnlyCollection<IWebElementAdapter> EventList
+        {
+            get { return Driver.FindAdaptedElements(By.CssSelector("td#content table tbody tr.Summary")); }
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/EventLog/BasicEventLogTest.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.EventLog;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Terminal.EventLog
+{
+    [TestClass]
+    public class BasicEventLogTest
+    {
+        private static TerminalEventLog _terminalEventLog;
+        private static IWebDriverAdapter _driver;
+        private static ScsPlatinTestDirector _tb;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            //before first test-method starts
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup(); //default QA-L with certificate login
+            _terminalEventLog = new TerminalEventLog(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _driver.Url = _tb.BaseUrl + "/EP2TerminalLog?TerminalId=30093271";
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //after last test-method finished
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void CheckEventTypes()
+        {
+            Assert.IsTrue(_terminalEventLog.EventList.Count == 0, "at least List with");
+            CollectionAssert.Contains(_terminalEventLog.EventList, "at least List with");
+        }
+
+        [TestMethod]
+        public void CheckSeverityList()
+        {
+            Assert.IsTrue(_terminalEventLog.SeverityList.Options.Count == 4, "number of severities options");
+            
+        }
+
+        [TestMethod]
+        public void CheckTimeSpan()
+        {
+            Assert.IsTrue(_terminalEventLog.SeverityList.Options.Count == 4, "number of time span options");
+        }
+    }
+}
