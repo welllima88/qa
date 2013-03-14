fdf73a7fc3c6eefb2d426bffaaba654690b52775
Marc Siegmund <TKCDEV\siegmund>
2013-03-14 18:47:11 +0000

    corrected Test for basic checks
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40307

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/EventLog/BasicEventLogTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/EventLog/BasicEventLogTest.cs
@@ -39,23 +39,37 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Terminal.EventLog
         }
 
         [TestMethod]
+        public void CheckEventTable()
+        {
+            Assert.IsTrue(_terminalEventLog.EventTable.Displayed, "at least Table with events displayed");
+            // CollectionAssert.Contains(_terminalEventLog.EventList, "at least List with");
+        }
+
+        [TestMethod]
+        public void CheckEventList()
+        {
+            Assert.IsNotNull(_terminalEventLog.EventList, "at least Table with events displayed");
+            // CollectionAssert.Contains(_terminalEventLog.EventList, "at least List with");
+        }
+
+        [TestMethod]
         public void CheckEventTypes()
         {
-            Assert.IsTrue(_terminalEventLog.EventList.Count == 0, "at least List with");
-            CollectionAssert.Contains(_terminalEventLog.EventList, "at least List with");
+            Assert.IsTrue(_terminalEventLog.EventTypes.Options.Count > 0, "at least List with");
+            Assert.IsTrue(_terminalEventLog.EventTypes.Options.Contains("1000 - Startup"));
         }
 
         [TestMethod]
         public void CheckSeverityList()
         {
             Assert.IsTrue(_terminalEventLog.SeverityList.Options.Count == 4, "number of severities options");
-            
         }
 
         [TestMethod]
         public void CheckTimeSpan()
         {
-            Assert.IsTrue(_terminalEventLog.SeverityList.Options.Count == 4, "number of time span options");
+            Assert.IsTrue(_terminalEventLog.TimeSpan.Options.Count == 4, "number of time span options");
+            // CollectionAssert.Contains(_terminalEventLog.TimeSpan.Options.Contains("24h"));
         }
     }
 }
