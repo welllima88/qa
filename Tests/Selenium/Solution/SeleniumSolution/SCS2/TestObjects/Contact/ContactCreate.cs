068297485e79b3ffccb2746ebeee967ad15a26b5
Marc Siegmund <TKCDEV\siegmund>
2013-03-14 13:25:21 +0000

    set new locators due to the gui changes (mvc)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40281

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Contact/ContactCreate.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Contact/ContactCreate.cs
@@ -1,3 +1,4 @@
+using System;
 using OpenQA.Selenium;
 using SIX.SCS.QA.Selenium.Extension;
 using SIX.SCS.QA.Selenium.Extension.TestObjects;
@@ -16,21 +17,21 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpFirstName")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_FirstName")).TypeText(
                     value);
             }
         }
 
         public string Name
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpName")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_LastName")).TypeText(value); }
         }
 
         public string Salutation
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpSalutation")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Title")).TypeText(
                     value);
             }
         }
@@ -39,8 +40,8 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindSuggesterElement(By.CssSelector(
-                    "#ctl00_bodyContentPlaceHolder_sugCountrytbx")).Suggestor(value);
+                Driver.FindAdaptedElement(By.CssSelector(
+                    "select#PersonData_Contact_CountryId")).Selector().SelectByText(value);
             }
         }
 
@@ -48,34 +49,34 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindSuggesterElement(
+                Driver.FindAdaptedElement(
                     By.CssSelector(
-                        "#ctl00_bodyContentPlaceHolder_sugLanguagetbx")).Suggestor(value);
+                        "selector#PersonData_Contact_LanguageId")).Selector().SelectByText(value);
             }
         }
 
         public string Telephone
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpPhone")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Telephone")).TypeText(value); }
         }
 
         public string Mobile
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpMobile")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Mobile")).TypeText(
                     value);
             }
         }
 
         public string Fax
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpFax")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Fax")).TypeText(value); }
         }
 
         public string Email
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpMail")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Email")).TypeText(value); }
         }
 
 
@@ -83,31 +84,31 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpStreet")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Street")).TypeText(
                     value);
             }
         }
 
         public string Po
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpPOBox")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_PO")).TypeText(value); }
         }
 
         public string Zip
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpZIP")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_ZIP")).TypeText(value); }
         }
 
         public string City
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpCity")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_City")).TypeText(value); }
         }
 
         public string Region
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpRegion")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Region")).TypeText(
                     value);
             }
         }
@@ -116,7 +117,7 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#ctl00_bodyContentPlaceHolder_inpAddressAddition")).
+                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_AddressAddition")).
                     TypeText(
                         value);
             }
@@ -129,12 +130,12 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
 
         public IWebElement AdressFromLocationButton
         {
-            get { return Driver.FindElement(By.CssSelector("#ctl00_bodyContentPlaceHolder_btnFromLocation")); }
+            get { throw new NotSupportedException(); }
         }
 
         public IWebElement CancelButton
         {
-            get { return Driver.FindElement(By.CssSelector("#ctl00_bodyContentPlaceHolder_btnCancel")); }
+            get { throw new NotSupportedException(); }
         }
 
         #endregion
