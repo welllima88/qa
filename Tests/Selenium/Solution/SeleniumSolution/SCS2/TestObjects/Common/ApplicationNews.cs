bfcf62b5290d034aa1c497a7e1385218660c5786
Marc Siegmund <TKCDEV\siegmund>
2013-03-19 16:52:04 +0000

    added application news test object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40389

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Common/ApplicationNews.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.ObjectModel;
+using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common
+{
+    public class ApplicationNews : WebObject
+    {
+        public ApplicationNews(IWebDriverAdapter driver) : base(driver)
+        {
+            Prefix = "div.container ";
+        }
+
+        public ReadOnlyCollection<IWebElement> NewsElements
+        {
+            get { return Driver.FindElements(By.CssSelector(Prefix + "ul")); }
+        }
+
+        public IWebElement NewsTitle
+        {
+            get { return Driver.FindElement(By.CssSelector(Prefix + "span#ctl00_bodyContentPlaceHolder_lblNewsTitle h2")); }
+        }
+    }
+}
