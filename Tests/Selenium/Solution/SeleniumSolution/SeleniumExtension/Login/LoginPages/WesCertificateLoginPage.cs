7eec15439fa744d7831675a9668be0418e901029
Marc Siegmund <TKCDEV\siegmund>
2013-03-19 15:17:10 +0000

    used inheritance of web object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40375

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Login/LoginPages/WesCertificateLoginPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Extension.Login.LoginPages
+{
+    public class WesCertificateLoginPage:WebObject
+    {
+        public WesCertificateLoginPage(IWebDriverAdapter driver):base(driver)
+        {            
+        }
+
+        public IWebElement LoginButton
+        {
+            get { return Driver.FindElement(By.CssSelector("form[name='LoginForm'] input.inputbutton[value='Login']")); }
+        }
+
+        public IWebElement HeadLine
+        {
+            get { return Driver.FindElement(By.CssSelector("div.divmainheadlineinner")); }
+        }
+
+        public IWebElement Continue
+        {
+            get { return Driver.FindElement(By.CssSelector("form[name='LoginForm'] input.inputbutton[value='Weiter']")); }
+        }
+
+        public IWebElement MessageInfo
+        {
+            get { return Driver.FindElement(By.CssSelector("form[name='LoginForm'] div#divmsginfo")); }
+        }
+
+        public IWebElement InputLabel
+        {
+            get { return Driver.FindElement(By.CssSelector("form[name='LoginForm'] div#divinputlabel")); }
+        }
+
+
+        public IWebElement Mandant(string mandant)
+        {
+            return
+                Driver.FindElement(
+                    By.CssSelector("form[name='LoginForm'] input[type='radio'][name='client'][value='" + mandant + "']"));
+        }
+    }
+}
