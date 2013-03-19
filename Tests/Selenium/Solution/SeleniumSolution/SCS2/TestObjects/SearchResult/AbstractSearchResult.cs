3722123d8ac8e6740415fe702a3036aee4ab9095
Marc Siegmund <TKCDEV\siegmund>
2013-03-19 15:37:44 +0000

    AbstractSeach-test object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40379

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/SearchResult/AbstractSearchResult.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.SearchResult
+{
+    public abstract class AbstractSearchResult : WebObject 
+    {
+        protected AbstractSearchResult(IWebDriverAdapter driver)
+            : base(driver)
+        {
+            // Prefix=""
+        }
+        public IWebElement LoadMore
+        {
+            get { return Driver.FindElement(By.CssSelector(Prefix + "p#searchSubmit")); }
+        }
+        public IWebElement FilterTextField
+        {
+            get { return Driver.FindElement(By.CssSelector(Prefix + "p#searchSubmit")); }
+        }
+    }
+}
