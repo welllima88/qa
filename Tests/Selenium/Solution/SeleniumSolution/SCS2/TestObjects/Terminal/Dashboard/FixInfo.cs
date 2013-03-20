bbe35764db464fc87cdf23a264bc21913ef013ba
Marc Siegmund <TKCDEV\siegmund>
2013-03-20 10:02:08 +0000

    created test object for terminal dashboard with fix info
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40402

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/FixInfo.cs
@@ -0,0 +1,18 @@
+﻿using SIX.SCS.QA.Selenium.Extension;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
+{
+    public class FixInfo : PortletViewBase
+    {
+        public readonly CustomerInfo Customer;
+        public readonly LocationInfo Location;
+        public readonly TerminalInfo Terminal;
+
+        public FixInfo(IWebDriverAdapter webDriver) : base(webDriver)
+        {
+            Customer = new CustomerInfo(webDriver);
+            Location = new LocationInfo(webDriver);
+            Terminal = new TerminalInfo(webDriver);
+        }
+    }
+}
