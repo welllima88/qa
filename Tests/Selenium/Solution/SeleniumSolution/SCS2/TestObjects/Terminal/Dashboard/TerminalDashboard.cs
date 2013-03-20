eb0f26a3bc98d46147a4cf971cbbf7d25e70b194
Marc Siegmund <TKCDEV\siegmund>
2013-03-20 10:02:18 +0000

    created test object for terminal dashboard with fix info
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40403

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/TerminalDashboard.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/Dashboard/TerminalDashboard.cs
@@ -7,19 +7,19 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.Dashboard
     public class TerminalDashboard : WebObject
     {
         public Brands Brands;
-        public CommonTerminalInfo CommonTerminalInfo;
+        public FixInfo FixInfo;
         public Contacts Contacts;
+        public SaleContract SaleContract;
         public TechnicalView TechnicalView;
-        public TerminalSaleContract TerminalSaleContract;
         public TerminalTexts TerminalTexts;
         public TroubleTickets TroubleTickets;
 
         public TerminalDashboard(IWebDriverAdapter driver) : base(driver)
         {
-            CommonTerminalInfo = new CommonTerminalInfo(Driver);
+            FixInfo = new FixInfo(Driver);
             Contacts = new Contacts(Driver);
             TechnicalView = new TechnicalView(Driver);
-            TerminalSaleContract = new TerminalSaleContract(Driver);
+            SaleContract = new SaleContract(Driver);
             TerminalTexts = new TerminalTexts(Driver);
             TroubleTickets = new TroubleTickets(Driver);
         }
