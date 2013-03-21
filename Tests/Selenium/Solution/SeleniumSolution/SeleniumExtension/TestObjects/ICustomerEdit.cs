87e8fcfd52e1f861591b67be62ae75706057ea2e
Marc Siegmund <TKCDEV\siegmund>
2013-03-21 08:45:17 +0000

    introduced getter for edit customer
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40426

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/ICustomerEdit.cs
@@ -0,0 +1,38 @@
+using System;
+using OpenQA.Selenium;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects
+{
+    public interface ICustomerEdit
+    {
+        string CustomerName { set; get; }
+        string SapNumber { set; get; }
+        string CompanyName { set; get; }
+        string Supplier { get; }
+        string CustomerNumber { get; }
+        string SbsDebitNumber { get; }
+        string SbsAdressNumber { get; }
+        string SbsCurrency { get; }
+        string SbsBillingTenant { get; }
+        string Ep2MerchantId { get; }
+        string CashIntegrator { set; get; }
+        string CategoryCode { set; get; }
+        string Agency { set; get; }
+        string SupportContract { set; get; }
+        string StreetName { set; get; }
+        string Language { set; get; }
+        string Country { set; get; }
+        string Zip { set; get; }
+        string City { set; get; }
+        string AdressAddition { set; get; }
+        string Po { set; get; }
+        string Region { set; get; }
+        string Telephone { set; get; }
+        string Mobile { set; get; }
+        string Fax { set; get; }
+        string Email { set; get; }
+        string Web { set; get; }
+        IWebElement SaveButton { get; }
+        IWebElement CancelButton { get; }
+    }
+}
