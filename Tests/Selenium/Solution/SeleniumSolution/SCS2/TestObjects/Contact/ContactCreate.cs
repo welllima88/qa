99cd5b9ea13c820c6e20841945b5b4009aa9adcf
Marc Siegmund <TKCDEV\siegmund>
2013-03-21 14:41:17 +0000

    put locators for contact in to separate ressource file
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40443

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Contact/ContactCreate.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Contact/ContactCreate.cs
@@ -16,21 +16,22 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_FirstName")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector(Contact.FirstName)).TypeText(
                     value);
             }
+            get { return Driver.FindAdaptedElement(By.CssSelector(Contact.FirstName)).Text; }
         }
 
         public string Name
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_LastName")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Name)).TypeText(value); }
         }
 
         public string Salutation
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Title")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector(Contact.Salutation)).TypeText(
                     value);
             }
         }
@@ -40,7 +41,7 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
             set
             {
                 Driver.FindAdaptedElement(By.CssSelector(
-                    "select#PersonData_Contact_CountryId")).Selector().SelectByText(value);
+                    Contact.Country)).Selector().SelectByText(value);
             }
         }
 
@@ -50,32 +51,32 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
             {
                 Driver.FindAdaptedElement(
                     By.CssSelector(
-                        "selector#PersonData_Contact_LanguageId")).Selector().SelectByText(value);
+                        Contact.Language)).Selector().SelectByText(value);
             }
         }
 
         public string Telephone
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Telephone")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Telephone)).TypeText(value); }
         }
 
         public string Mobile
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Mobile")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector(Contact.Mobile)).TypeText(
                     value);
             }
         }
 
         public string Fax
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Fax")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Fax)).TypeText(value); }
         }
 
         public string Email
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Email")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Email)).TypeText(value); }
         }
 
 
@@ -83,31 +84,31 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Street")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector(Contact.Street)).TypeText(
                     value);
             }
         }
 
         public string Po
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_PO")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Po)).TypeText(value); }
         }
 
         public string Zip
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_ZIP")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.Zip)).TypeText(value); }
         }
 
         public string City
         {
-            set { Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_City")).TypeText(value); }
+            set { Driver.FindAdaptedElement(By.CssSelector(Contact.City)).TypeText(value); }
         }
 
         public string Region
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_Region")).TypeText(
+                Driver.FindAdaptedElement(By.CssSelector(Contact.Region)).TypeText(
                     value);
             }
         }
@@ -116,7 +117,7 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact
         {
             set
             {
-                Driver.FindAdaptedElement(By.CssSelector("input#PersonData_Contact_AddressAddition")).
+                Driver.FindAdaptedElement(By.CssSelector(Contact.AddressAddition)).
                     TypeText(
                         value);
             }
