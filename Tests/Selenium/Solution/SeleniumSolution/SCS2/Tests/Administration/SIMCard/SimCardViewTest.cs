fb28fb0f40673d0933c7b5618432e9960a8ae17f
Marc Siegmund <TKCDEV\siegmund>
2013-03-27 17:40:46 +0000

    added generic checks for sim card view
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40628

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SIMCard/SimCardViewTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SIMCard/SimCardViewTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SIX.SCS.QA.Selenium.Extension;
 using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Administration.SimCard;
 using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
@@ -49,6 +50,14 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Administration.SIMCard
         [TestMethod]
         public void CheckSimCardView()
         {
+            StringAssert.Matches(_simCardView.SimCardElement.MobileNumber, new Regex(@"\d{10}"));
+            StringAssert.Matches(_simCardView.SimCardElement.SimCardNumber, new Regex(@"\d{18}"));
+            StringAssert.Matches(_simCardView.SimCardElement.Pin, new Regex(@"\d{4}"));
+            StringAssert.Matches(_simCardView.SimCardElement.Puk, new Regex(@"\d{8}"));
+            StringAssert.Matches(_simCardView.SimCardElement.TerminalId, new Regex(@"\d+"));
+            Assert.IsNotNull(_simCardView.SimCardElement.Status);
+            Assert.IsNotNull(_simCardView.SimCardElement.Region);
+            Assert.IsNotNull(_simCardView.SimCardElement.Usage);
         }
 
         [TestMethod]
