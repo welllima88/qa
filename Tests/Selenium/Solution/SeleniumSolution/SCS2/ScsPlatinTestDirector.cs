23f453095c5703a5742904710080acc06c1f8637
Marc Siegmund <TKCDEV\siegmund>
2013-04-05 16:11:30 +0000

    added logic for initialize different browser and environment
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40843

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/ScsPlatinTestDirector.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/ScsPlatinTestDirector.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using SIX.SCS.QA.Selenium.Extension;
+﻿using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.Login;
 using SIX.SCS.QA.Selenium.Extension.Login.LoginMethod;
 using SIX.SCS.QA.Selenium.Tests.SCSPlatin.Properties;
 using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
@@ -8,19 +8,42 @@ namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin
 {
     public class ScsPlatinTestDirector : TestDirector
     {
+        private readonly string auth;
+        private readonly string browser;
+        private readonly string environment;
+
+        public ScsPlatinTestDirector(string browser = "FF", string environment = "QAL", string auth = "UAC")
+        {
+            this.environment = environment;
+            this.browser = browser;
+            this.auth = auth;
+        }
+
         protected override IWebDriverAdapter DefaultBrowserSetup()
         {
-            return FireFoxWebDriverAdapter();
+            if (browser == "FF")
+            {
+                return FireFoxWebDriverAdapter();
+            }
+            return InternetExplorerWebDriverAdapter();
         }
 
         public override IWebDriverAdapter DefaultTestSetup()
         {
-            var certificateLogin = new CertificateLogin(Scs2Res.mandant_qa_L, WebDriver);
-            var uacLogin = new UacLogin(Scs2Res.uac_username, Scs2Res.uac_passwort, WebDriver);
+            ILogin login = null;
+            if (auth == "UAC")
+            {
+                login = new UacLogin(Scs2Res.uac_username, Scs2Res.uac_passwort, WebDriver);
+            }
+            else
+            {
+                login = new CertificateLogin(Scs2Res.mandant_qa_L, WebDriver);
+            }
+
             var lobby = new Lobby(WebDriver);
 
             // TestSetup(Scs2Res.WES_QA_L, certificateLogin, lobby);
-            TestSetup(Scs2Res.UAC_DEV, uacLogin, lobby);
+            TestSetup(Scs2Res.UAC_DEV, login, lobby);
             return WebDriver;
         }
     }
