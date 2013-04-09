e6d73f6715ded591e68a33f7d3c6246ad8f8f644
Marc Siegmund <TKCDEV\siegmund>
2013-04-09 14:22:10 +0000

    major changes to global methods and behavior (with almost no impact on the rest of the solution) optmized for better/clean separation and configuration removed some unnecessary methods and fields
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C40915

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestDirector.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestDirector.cs
@@ -13,57 +13,53 @@ namespace SIX.SCS.QA.Selenium.Extension
     public class TestDirector
     {
         private const double TimeOutSeconds = 30;
+        private readonly string _auth;
+        private readonly string _environment;
         private ILogin _login;
         private ILoginCheck _loginCheck;
         private ILogout _logout;
         private ILogoutCheck _logoutCheck;
-        // private static readonly FirefoxBinary FirefoxBinary = new FirefoxBinary(@"D:\_tfs\ScsDev\QA\Tests\Selenium\Firefox\firefox.exe");
 
-        /// <summary>
-        /// Common constructor for default test-launch with one browser instance of Firefox and 10 seconds timeout. 
-        /// </summary>
+        public TestDirector(string environment, string auth)
+        {
+            _environment = environment;
+            _auth = auth;
+        }
+
         public TestDirector()
         {
-            WebDriver = DefaultBrowserSetup();
+            _environment = DriverRes.DEV;
+            _auth = DriverRes.UAC;
         }
 
-        /// <summary>
-        /// Uses an existing and already opened browser for testing (not implemented yet)
-        /// </summary>
-        public TestDirector(IWebDriverAdapter webDriverAdapter)
-        {
-            WebDriver = webDriverAdapter;
-        }
-
-        public IWebDriverAdapter WebDriver { get; protected set; }
+        public IWebDriverAdapter WebDriver { get; private set; }
 
         public string BaseUrl { get; private set; }
 
-        private static IWebDriverAdapter FireFoxWebDriverAdapter(string profileName)
+        private void FireFoxWebDriverAdapter(string profileName)
         {
             FirefoxProfile firefoxProfile = new FirefoxProfileManager().GetProfile(profileName);
 
-            return new WebDriverAdapter(new FirefoxDriver(firefoxProfile));
+            WebDriver = new WebDriverAdapter(new FirefoxDriver(firefoxProfile));
         }
 
-        protected static IWebDriverAdapter FireFoxWebDriverAdapter()
+        protected void FireFoxWebDriverAdapter()
         {
             FirefoxProfile firefoxProfile =
                 new FirefoxProfileManager().GetProfile(DriverRes.FirefoxProfile_Certificate);
 
-            return new WebDriverAdapter(new FirefoxDriver(firefoxProfile));
+            WebDriver = new WebDriverAdapter(new FirefoxDriver(firefoxProfile));
         }
+
         /// <summary>
         /// Important note:
         /// but this is delegated to virtual method, and IE-webtesting is not supported for special test case, this will cause to derive a new class from test-director, which startups IE web driver, because it is rarely used, this solution is perfect at the momemt
         /// </summary>
         /// <returns></returns>
-        protected static IWebDriverAdapter InternetExplorerWebDriverAdapter()
+        protected void InternetExplorerWebDriverAdapter()
         {
             //todo set relative path
-            return
-                new WebDriverAdapter(
-                    new InternetExplorerDriver(@"D:\_tfs\ScsDev\QA\Tests\Selenium\IEDriver"));
+            WebDriver = new WebDriverAdapter(new InternetExplorerDriver(@"D:\_tfs\ScsDev\QA\Tests\Selenium\IEDriver"));
         }
 
         /// <summary>
@@ -78,26 +74,22 @@ namespace SIX.SCS.QA.Selenium.Extension
         /// <param name="logout"></param>
         /// <param name="loginCheck"></param>
         /// <param name="logoutCheck"></param>
-        protected void TestSetup(string baseUrl, ILogin login, ILogout logout, ILoginCheck loginCheck = null,
-                                 ILogoutCheck logoutCheck = null)
+        protected void StartUpTest(string baseUrl, ILogin login, ILogout logout, ILoginCheck loginCheck = null,
+                             ILogoutCheck logoutCheck = null)
         {
+            
+            WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(TimeOutSeconds));
+            WebDriver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(TimeOutSeconds));
+
             _login = login;
             _loginCheck = loginCheck;
             _logout = logout;
             _logoutCheck = logoutCheck;
 
-            WebDriver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(TimeOutSeconds));
-            WebDriver.Manage().Timeouts().SetScriptTimeout(TimeSpan.FromSeconds(TimeOutSeconds));
-
-            BaseUrl = WebDriver.Url = baseUrl; // essential to avoid constructor actions  ;)
+            BaseUrl = WebDriver.Url = baseUrl; // essential to avoid constructor actions
 
             _login.Login();
             if (_loginCheck != null) _loginCheck.CheckLogInSucess();
-        }
-
-        protected virtual IWebDriverAdapter DefaultBrowserSetup()
-        {
-            throw new NotImplementedException("sub class has to implement this method");
         }
 
         public virtual IWebDriverAdapter DefaultTestSetup()
