d7ccdc2fe9b4ce1989c33a464123a9e7eb7e204b
Marc Siegmund <TKCDEV\siegmund>
2013-05-16 14:07:35 +0000

    TerminalTraceLogConfig test objects for view and edit created
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C41589

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/TraceLogConfig/TerminalTraceLogConfigEdit.cs
@@ -0,0 +1,75 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.TraceLogConfig
+{
+    public class TerminalTraceLogConfigEdit : WebObject
+    {
+        public TerminalTraceLogConfigEdit(IWebDriverAdapter driver)
+            : base(driver)
+        {
+        }
+
+        public IWebElementAdapter ActivatedUntil
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#activateTill")); }
+        }
+
+        public IWebElementAdapter Parameter
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_Parameter")); }
+        }
+
+        public SelectElement Medium
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationDevice")).Selector(); }
+        }
+
+        public IWebElementAdapter DestinationAddress
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationAddressIp")); }
+        }
+
+        public IWebElementAdapter DestinationPort
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationAddressPort")); }
+        }
+
+        public IWebElementAdapter TelephoneIsdn
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_PhoneNumberIsdn")); }
+        }
+
+        public IWebElementAdapter TelephonePstn
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_PhoneNumberPstn")); }
+        }
+
+        public IWebElementAdapter DestinationUrl
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_InternetAddressUrl")); }
+        }
+
+        public IWebElementAdapter DestinationUrlPort
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_InternetAddressPort")); }
+        }
+
+        public IWebElementAdapter Save
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("button[value='speichern']")); }
+        }
+
+        public IWebElementAdapter Cancel
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("button[name='cancel']")); }
+        }
+
+        public IWebElementAdapter Deactivate
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("button[name='deactivate']")); }
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Terminal/TraceLogConfig/TerminalTraceLogConfigView.cs
@@ -0,0 +1,64 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Terminal.TraceLogConfig
+{
+    public class TerminalTraceLogConfigView : WebObject
+    {
+        public TerminalTraceLogConfigView(IWebDriverAdapter driver)
+            : base(driver)
+        {
+        }
+
+        public string ActivatedUntil
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#activateTill")).Text; }
+        }
+
+        public string Parameter
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_Parameter")).Text; }
+        }
+
+        public string Medium
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationDevice")).Text; }
+        }
+
+        public string DestinationAddress
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationAddressIp")).Text; }
+        }
+
+        public string DestinationPort
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_DestinationAddressPort")).Text; }
+        }
+
+        public string TelephoneIsdn
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_PhoneNumberIsdn")).Text; }
+        }
+
+        public string TelephonePstn
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_PhoneNumberPstn")).Text; }
+        }
+
+        public string DestinationUrl
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_InternetAddressUrl")).Text; }
+        }
+
+        public string DestinationUrlPort
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ConfigData_InternetAddressPort")).Text; }
+        }
+
+        public IWebElementAdapter Edit
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("button[name='deactivate']")); }
+        }
+    }
+}
