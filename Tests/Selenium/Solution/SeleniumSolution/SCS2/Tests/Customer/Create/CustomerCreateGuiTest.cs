96f0042e6ab9f47088f7e639dd92ab6a4b0e636a
Marc Siegmund <TKCDEV\siegmund>
2013-05-21 10:10:51 +0000

    added test for cancel and gui elements
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C41664

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Customer/Create/CustomerCreateGuiTest.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Menu;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Customer.Create
+{
+    [TestClass]
+    public class CustomerCreateGuiTest
+    {
+        private static IWebDriverAdapter _driver;
+        private static NavigationBar _navigationBar;
+        private static TestDirector _tb;
+        private static CustomerMenu _customerMenu;
+        private static LobbyMenu _lobbyMenu;
+        private static MenusTest _menusTests;
+        private static Lobby _lobby;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            //before first test-method starts
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup(); //default QA-L with certificate login and 10 seconds response timeout
+            _lobbyMenu = new LobbyMenu(_driver);
+            _customerMenu = new CustomerMenu(_driver);
+            _navigationBar = new NavigationBar(_driver);
+            _menusTests = new MenusTest();
+            _lobby = new Lobby(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _driver.Url = _tb.BaseUrl + "/Default.aspx";
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //after last test-method finished
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void CreateCustomerCheckNavBar()
+        {
+            _customerMenu.CustomerCreate.Click();
+            Assert.AreEqual("Neuer Kunde", _navigationBar.Current.Text);
+        }
+
+        [TestMethod]
+        public void CreateCustomerCheckHeadline()
+        {
+            StringAssert.Contains("Lobby", _lobby.Headline.Text);
+            _customerMenu.CustomerCreate.Click();
+            Assert.AreEqual("Neuer Kunde", _lobby.Headline.Text);
+        }
+
+        [TestMethod]
+        public void CreateCustomerMenu()
+        {
+            _menusTests.LobbyMenu(_lobbyMenu);
+            _customerMenu.CustomerCreate.Click();
+        }
+    }
+}
