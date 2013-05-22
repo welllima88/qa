7ccd85e5d37f50e65c635cec5b7426fc95907f8e
Marc Siegmund <TKCDEV\siegmund>
2013-05-22 15:26:32 +0000

    test cases for navigation bar and meta natvigation bar
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C41731

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/General Checks/MetaNavigationBarTest.cs
@@ -0,0 +1,92 @@
+﻿/* RegExp
+ * http://openbook.galileocomputing.de/csharp/kap17.htm
+ * 
+ * Strings als Parameter:
+ * http://openbook.galileocomputing.de/csharp/kap19.htm
+ * "Kundenname" statt k.Name Feld-Bezeichnung ist viel flexibler für zukünftige Struktur
+ *  
+ * http://openbook.galileocomputing.de/csharp/kap31.htm#t25
+ * 
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+
+namespace SeleniumTests
+{
+    /// <summary>
+    /// be careful with menu expander because they prevent some actions and need special handling 
+    /// </summary>
+    [TestClass]
+    public class MetaNavigationBarTest
+    {
+        private static IWebDriverAdapter _driver;
+        private static TestDirector _tb;
+        private static Lobby _lobby;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup();
+            _lobby = new Lobby(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void CheckLobbyMenu()
+        {
+        }
+
+        [TestMethod]
+        public void User()
+        {
+            Assert.IsTrue(_lobby.MetaNavBar.User.Displayed);
+        }
+
+        [TestMethod]
+        public void Logout()
+        {
+            Assert.IsTrue(_lobby.MetaNavBar.Logout.Displayed);
+        }
+
+        [TestMethod]
+        public void Languages()
+        {
+            Assert.IsTrue(_lobby.MetaNavBar.Languages.Displayed);
+        }
+
+        [TestMethod]
+        public void LanguageGerman()
+        {
+            _lobby.MetaNavBar.Languages.Click();
+            Assert.IsTrue(_lobby.MetaNavBar.Language("Deutsch").Displayed);
+            Assert.IsTrue(_lobby.MetaNavBar.Language("English").Enabled);
+            _lobby.MetaNavBar.Languages.Click();
+        }
+
+        [TestMethod]
+        public void LanguageEnglish()
+        {
+            _lobby.MetaNavBar.Languages.Click();
+            Assert.IsTrue(_lobby.MetaNavBar.Language("English").Displayed); //Enabled, if displayed is making problems
+            _lobby.MetaNavBar.Languages.Click();
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/General Checks/NavigationBarTest.cs
@@ -0,0 +1,98 @@
+﻿/* RegExp
+ * http://openbook.galileocomputing.de/csharp/kap17.htm
+ * 
+ * Strings als Parameter:
+ * http://openbook.galileocomputing.de/csharp/kap19.htm
+ * "Kundenname" statt k.Name Feld-Bezeichnung ist viel flexibler für zukünftige Struktur
+ *  
+ * http://openbook.galileocomputing.de/csharp/kap31.htm#t25
+ * 
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+
+namespace SeleniumTests
+{
+    /// <summary>
+    /// be careful with menu expander because they prevent some actions and need special handling 
+    /// </summary>
+    [TestClass]
+    public class NavigationBarTest
+    {
+        private static IWebDriverAdapter _driver;
+        private static TestDirector _tb;
+        private static Lobby _lobby;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup();
+            _lobby = new Lobby(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void CheckLobbyMenu()
+        {
+        }
+
+
+        [TestMethod]
+        public void Lobby()
+        {
+            Assert.IsTrue(_lobby.NavigationBar.Lobby.Displayed);
+        }
+
+        [TestMethod]
+        public void Administration()
+        {
+            Assert.IsTrue(_lobby.NavigationBar.Administration.Displayed);
+        }
+
+        [TestMethod]
+        public void Reporting()
+        {
+            Assert.IsTrue(_lobby.NavigationBar.Reporting.Displayed);
+        }
+
+        [TestMethod]
+        public void MoreMenu()
+        {
+            Assert.IsTrue(_lobby.NavigationBar.MoreMenu.Displayed);
+        }
+
+        [TestMethod]
+        public void Help()
+        {
+            _lobby.NavigationBar.MoreMenu.Click(); //open more Menu
+            Assert.IsTrue(_lobby.NavigationBar.Help.Displayed);
+            _lobby.NavigationBar.MoreMenu.Click(); //close more Menu (also necessary for correct logoff)
+        }
+
+        [TestMethod]
+        public void SystemInfo()
+        {
+            _lobby.NavigationBar.MoreMenu.Click(); //open more Menu
+            Assert.IsTrue(_lobby.NavigationBar.SystemInfo.Enabled);
+            _lobby.NavigationBar.MoreMenu.Click(); //close more Menu (also necessary for correct logoff)
+        }
+    }
+}
