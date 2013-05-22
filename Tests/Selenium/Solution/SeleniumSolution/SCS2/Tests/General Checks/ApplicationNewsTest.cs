174e11bdd3ce61197e5717a4b273d918a438fba6
Marc Siegmund <TKCDEV\siegmund>
2013-05-22 14:53:52 +0000

    added tests for application info, application news and footer (basic lobby elements)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C41729

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/General Checks/ApplicationNewsTest.cs
@@ -0,0 +1,64 @@
+﻿/* RegExp
+ * http://openbook.galileocomputing.de/csharp/kap17.htm
+ * 
+ * Strings als Parameter:
+ * http://openbook.galileocomputing.de/csharp/kap19.htm
+ * "Kundenname" statt k.Name Feld-Bezeichnung ist viel flexibler für zukünftige Struktur
+ *  
+ * http://openbook.galileocomputing.de/csharp/kap31.htm#t25
+ * 
+ */
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+
+namespace SeleniumTests
+{
+    /// <summary>
+    /// be careful with menu expander because they prevent some actions and need special handling 
+    /// </summary>
+    [TestClass]
+    public class ApplicationNewsTest
+    {
+        private static IWebDriverAdapter _driver;
+        private static TestDirector _tb;
+        private static Lobby _lobby;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup();
+            _lobby = new Lobby(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void CheckLobbyMenu()
+        {
+        }
+
+
+        [TestMethod]
+        public void ApplicationNews()
+        {
+            Assert.IsTrue(_lobby.LobbyNews.NewsTitle.Displayed);
+        }
+    }
+}
