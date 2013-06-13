32b2f217ae4b9126fb325f7a32c399a43d92ad00
Marc Siegmund <TKCDEV\siegmund>
2013-06-13 12:15:47 +0000

    changed article edit (data object and test) to view mode and added properties (according to gui and feature changes)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42198

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Administration/Article/ArticleView.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Administration.Article
+{
+    internal class ArticleView : WebObject, IArticle
+    {
+        public ArticleView(IWebDriverAdapter driver) : base(driver)
+        {
+        }
+
+        public IWebElement CancelButton
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("input.button[name='cancel']")); }
+        }
+
+        public IWebElement SaveButton
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("input.button[value='Speichern']")); }
+        }
+
+        #region IArticle Members
+
+        public string Name
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ArticleName")).Text; }
+        }
+
+        public bool Status
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ArticleState")).Selected; }
+        }
+
+        public string NetType
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#NetType")).Selector().SelectedOption.Text; }
+        }
+
+        public string SbsArticleNumber
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ArticleSbsNumber")).Text; }
+        }
+
+
+        public string CardReaderType
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#CardReaderType")).Text; }
+        }
+
+        public string ContactlessIndicator
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#ContactlessInd")).Text; }
+        }
+
+        public string PosInterface
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#PosInterface")).Text; }
+        }
+
+        public string Color
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#Color")).Text; }
+        }
+
+        public string SoftwareId
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#SoftwareId")).Text; }
+        }
+
+        public string TrxSubmLim
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#TrxSubmLim")).Text; }
+        }
+
+        public string TrxTransLim
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#TrxTransLim")).Text; }
+        }
+
+        public List<string> Supplier
+        {
+            get
+            {
+                return
+                    Driver.WebElementsAsStringList(
+                        Driver.FindElements(By.CssSelector("td#content form div table.dataTable tbody tr td")));
+            }
+        }
+
+        public string RecallInterval
+        {
+            get { return Driver.FindAdaptedElement(By.CssSelector("#RecallInterval")).Text; }
+        }
+
+        #endregion
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/Article/ArticleViewTest.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Administration.Article;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Administration.Article
+{
+    [TestClass]
+    public class ArticleViewTest
+    {
+        private static IArticle _articleView;
+        private static TestDirector _tb;
+
+        private static IWebDriverAdapter _driver;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            //before first test-method starts
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup(); //default QA-L with certificate login and 10 seconds response timeout
+            _articleView = new ArticleView(_driver);
+
+            _driver.Url = _tb.BaseUrl + "/Article/Edit?Id=CF03DF69-5AE1-422B-8C37-DCE143157D98";
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //_customerView.
+
+            //after last test-method finished
+            _tb.ShutDownTest();
+        }
+
+
+        [TestMethod]
+        public void ArticlelName()
+        {
+            Assert.AreEqual("yomani XR S C O PINPAD, TCP/IP (IP)", _articleView.Name);
+        }
+
+        [TestMethod]
+        public void SbsArticleNumber()
+        {
+            Assert.AreEqual("", _articleView.SbsArticleNumber);
+        }
+
+        [TestMethod]
+        public void Status()
+        {
+            Assert.IsTrue(_articleView.Status);
+        }
+
+        [TestMethod]
+        public void SupplierList()
+        {
+            CollectionAssert.AllItemsAreNotNull(_articleView.Supplier);
+        }
+
+        [TestMethod]
+        public void SupplierListHasAtLeastOneElement()
+        {
+            Assert.IsTrue(_articleView.Supplier.Count > 0, "0 - :) or less elements");
+        }
+
+        [TestMethod]
+        public void SupplierListHasSixSupplierElement()
+        {
+            CollectionAssert.Contains(_articleView.Supplier, "SIX Payment Services AG");
+        }
+
+        [TestMethod]
+        public void Color()
+        {
+            Assert.AreEqual("white", _articleView.Color);
+        }
+
+        [TestMethod]
+        public void NetTypeEditWithoutSave()
+        {
+            Assert.AreEqual("TCPIPEP2IP", _articleView.NetType);
+        }
+
+        [TestMethod]
+        public void SoftwareId()
+        {
+            Assert.AreEqual("1095", _articleView.SoftwareId);
+        }
+
+        [TestMethod]
+        public void TrxSubmLim()
+        {
+            Assert.AreEqual("1", _articleView.TrxSubmLim);
+        }
+
+        [TestMethod]
+        public void TrxTransLim()
+        {
+            Assert.AreEqual("1", _articleView.TrxTransLim);
+        }
+
+        [TestMethod]
+        public void ContactlessIndicator()
+        {
+            Assert.AreEqual("1", _articleView.ContactlessIndicator);
+        }
+
+        [TestMethod]
+        public void CardReaderType()
+        {
+            Assert.AreEqual("3", _articleView.CardReaderType);
+        }
+
+        [TestMethod]
+        public void PosInterface()
+        {
+            Assert.AreEqual("VEZ", _articleView.PosInterface);
+        }
+
+        [TestMethod]
+        public void RecallInterval()
+        {
+            Assert.AreEqual("86400", _articleView.RecallInterval);
+        }
+    }
+}
