31e70db5e4c64682edc5828dd60f7701d0f9b524
Marc Siegmund <TKCDEV\siegmund>
2013-07-03 14:11:11 +0000

    set Test to ignore, because used for exploring and debugging
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42482

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/BuildTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests
+{
+    [TestClass]
+    [Ignore]
+    public class BuildTest
+    {
+        [TestCategory("BuildTestFailed")]
+        [TestMethod]
+        public void BuildTestFailed()
+        {
+            Assert.Fail("Always fails this test :-(");
+        }
+
+        [TestCategory("BuildTestPassed")]
+        [TestMethod]
+        public void BuildTestPassed()
+        {
+            Assert.IsTrue(true, "Always pass this test :-)");
+        }
+    }
+}
