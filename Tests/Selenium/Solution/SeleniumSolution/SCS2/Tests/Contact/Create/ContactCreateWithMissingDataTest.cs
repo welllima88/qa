e2dbc4a72ee1994e8f357467044dc99e8b07cd48
Marc Siegmund <TKCDEV\siegmund>
2013-07-09 12:01:29 +0000

    contact create missing input tests
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42670

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Contact/Create/ContactCreateWithMissingDataTest.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Menu;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Contact.Create
+{
+    [TestClass]
+    public class ContactCreateWithMissingDataTest
+    {
+        private static ContactCreate _contactCreate;
+        private static ContactView _contactView;
+        private static IWebDriverAdapter _driver;
+        private static NavigationBar _navigationBar;
+        private static RecentElements _recentElements;
+        private static TestDirector _tb;
+        private static FormAlert _formAlert;
+        private static CustomerMenu _customerMenu;
+        private static ContactMenu _contactMenu;
+        private static MenusTest _menusTests;
+        private static LobbyView _lobby;
+
+        private long _dt;
+        private List<string> _formAlerts;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            //before first test-method starts
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup(); //default QA-L with certificate login and 10 seconds response timeout
+
+            _customerMenu = new CustomerMenu(_driver);
+            _contactCreate = new ContactCreate(_driver);
+            _contactView = new ContactView(_driver);
+            _contactMenu = new ContactMenu(_driver);
+            _recentElements = new RecentElements(_driver);
+            _navigationBar = new NavigationBar(_driver);
+            _formAlert = new FormAlert(_driver);
+            _menusTests = new MenusTest();
+            _lobby = new LobbyView(_driver);
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            _driver.Url = _tb.BaseUrl + "/Pages/Customer/CustomerEdit.aspx?CustomerId=401152";
+            _dt = DateTime.Now.Ticks; //timestamp for each test
+
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.Salutation = "Herr";
+            _contactCreate.FirstName = "Marc";
+            _contactCreate.Name = "Siegmund SYR AUTO";
+            _contactCreate.Language = "de";
+            _contactCreate.Telephone = "+41 58 399 6237 - 1";
+            _contactCreate.Mobile = "+41 58 399 6237 - 2";
+            _contactCreate.Fax = "+41 58 399 6237 - 3";
+            _contactCreate.Email = "contact@six-group.com";
+            _contactCreate.Street = "Kontakt-Weg 1";
+            _contactCreate.Po = "POC";
+            _contactCreate.Zip = "55555";
+            _contactCreate.City = "Berlin";
+            _contactCreate.Region = "Mitte";
+            _contactCreate.Country = "DE";
+            _contactCreate.AddressAddition = "Zusatz1";
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //after last test-method finished
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void WithoutFirstName()
+        {
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.FirstName = "";
+
+            _contactCreate.SaveButton.Click();
+            _formAlerts = _formAlert.FormAlertList;
+
+            Assert.IsTrue(_formAlerts.Count == 1);
+            Assert.IsTrue(_formAlerts.Contains("Vorname: Dies ist ein Pflichtfeld!"));
+        }
+
+        [TestMethod]
+        public void WithoutLastName()
+        {
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.Name = "";
+
+            _contactCreate.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+            Assert.IsTrue(_formAlerts.Count == 1);
+            Assert.IsTrue(_formAlerts.Contains("Name: Zu kurze Eingabe! Dies ist ein Pflichtfeld!"));
+        }
+
+        [TestMethod]
+        public void WithoutStreetAndNumber()
+        {
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.Street = "";
+
+            _contactCreate.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+            Assert.IsTrue(_formAlerts.Count == 1);
+            Assert.IsTrue(_formAlerts.Contains("Strasse / Nr: Dies ist ein Pflichtfeld!"));
+        }
+
+        [TestMethod]
+        public void WithoutCityFailed()
+        {
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.City = "";
+
+            _contactCreate.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+            Assert.IsTrue(_formAlerts.Count == 1);
+            Assert.IsTrue(_formAlerts.Contains("Ort: Dies ist ein Pflichtfeld!"));
+        }
+
+        [TestMethod]
+        public void WithoutZipFailed()
+        {
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.Zip = "";
+
+            _contactCreate.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+            Assert.IsTrue(_formAlerts.Count == 1);
+            Assert.IsTrue(_formAlerts.Contains("PLZ: Dies ist ein Pflichtfeld!"));
+        }
+    }
+}
