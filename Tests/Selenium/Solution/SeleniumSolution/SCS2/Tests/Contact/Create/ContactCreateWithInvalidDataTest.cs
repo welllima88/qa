3683c693ac5d3582e2d697c9faa942305cd03868
Marc Siegmund <TKCDEV\siegmund>
2013-07-09 11:15:48 +0000

    added test  for contact create with (completely) invalid data
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42664

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Contact/Create/ContactCreateWithInvalidDataTest.cs
@@ -0,0 +1,147 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Contact;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Contact.Create
+{
+    [TestClass]
+    public class ContactCreateWithInvalidDataTest
+    {
+        private static ContactCreate _contactCreate;
+        private static IWebDriverAdapter _driver;
+        private static TestDirector _tb;
+        private static FormAlert _formAlert;
+        private static CustomerMenu _customerMenu;
+
+        private static List<string> _formAlerts;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            //before first test-method starts
+            _tb = new ScsPlatinTestDirector();
+            _driver = _tb.DefaultTestSetup(); //default QA-L with certificate login and 10 seconds response timeout
+
+            _customerMenu = new CustomerMenu(_driver);
+            _contactCreate = new ContactCreate(_driver);
+            _formAlert = new FormAlert(_driver);
+
+            _driver.Url = _tb.BaseUrl + "/Pages/Customer/CustomerEdit.aspx?CustomerId=401152";
+
+            _customerMenu.ContactCreate.Click();
+
+            _contactCreate.Salutation = "Herr^@}$°";
+            _contactCreate.FirstName = "Marc^@}$°";
+            _contactCreate.Name = "SYR AUTO ^@}$°";
+            _contactCreate.Telephone = "+41 58 399 ^@}$° - 1";
+            _contactCreate.Mobile = "+41 58 399 ^@}$° - 2";
+            _contactCreate.Fax = "+41 58 399 ^@}$° - 3";
+            _contactCreate.Email = "con^@}$°tact@six-group.com";
+            _contactCreate.Street = "Kontakt-^@}$° 1";
+            _contactCreate.Po = "P^@}$°";
+            _contactCreate.Zip = "^@}$°";
+            _contactCreate.City = "^City@}$°";
+            _contactCreate.Region = "Re^@}$°";
+            _contactCreate.AddressAddition = "Add^@}$°";
+
+            _contactCreate.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+        }
+
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            //after last test-method finished
+            _tb.ShutDownTest();
+        }
+
+        [TestMethod]
+        public void SalutationInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Anreade: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void FirstNameInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Vorname: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void LastNameInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Nachname: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void AdditionInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Zusatz: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void TelephoneInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Telefon: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void MobileInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Mobiltelefon: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void FaxInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Fax: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void StreetAndNoInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Strasse / Nr: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void ZipInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("PLZ: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void CityInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Ort: Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void RegionInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Region (Kurzzeichen): Ungültige Zeichen gefunden!"));
+        }
+
+        [TestMethod]
+        public void EmailInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Email: Dies ist keine gültige E-Email Adresse!"));
+        }
+
+        [TestMethod]
+        public void WebInvalid()
+        {
+            Assert.IsTrue(_formAlerts.Contains("Web: Dies ist keine gültige Web Adresse!"));
+        }
+
+        [TestMethod]
+        public void NumberOfFormAlerts()
+        {
+            Assert.IsTrue(_formAlerts.Count == 13);
+        }
+    }
+}
