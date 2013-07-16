6bf93db27b2ebbc0d61f9956beb638105c54ea1a
Marc Siegmund <TKCDEV\siegmund>
2013-07-16 08:10:50 +0000

    handling of driver and environments is improved and made also much faster used testlauncher and test director
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42872

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestLauncher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin
+{
+    [TestClass]
+    public static class TestLauncher
+    {
+        public static TestDirector TestDirector;
+
+        public static void Navigate(string urlSuffix)
+        {
+            TestDirector.WebDriver.Url = TestDirector.TestEnvironment.BaseUrl + urlSuffix;
+        }
+
+        public static long TestGenId()
+        {
+            return DateTime.Now.Ticks;
+        }
+
+        [AssemblyInitialize]
+        public static void LaunchTestDirector(TestContext testContext)
+        {
+            TestDirector = new ScsPlatinTestDirector();
+            TestDirector.PrepareBrowser();
+        }
+
+        [AssemblyCleanup]
+        public static void StopTestDirector()
+        {
+            TestDirector.ShutDownBrowser();
+        }
+    }
+}
