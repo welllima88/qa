06d9e636c7e959cae75bbf5c2cb17097588f6013
Marc Siegmund <TKCDEV\siegmund>
2013-07-16 08:55:49 +0000

    added test launcher for old scs with test director call and use of environment class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42881

--- Tests/Selenium/Solution/SeleniumSolution/SCS/ScsClassicTestDirector.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS/ScsClassicTestDirector.cs
@@ -1,38 +1,14 @@
 ﻿using SIX.SCS.QA.Selenium.Extension;
-using SIX.SCS.QA.Selenium.Extension.Authentication;
-using SIX.SCS.QA.Selenium.Extension.Authentication.Method;
-using SIX.SCS.QA.Selenium.Tests.SCSClassics.Properties;
-using SIX.SCS.QA.Selenium.Tests.SCSClassics.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.Settings;
+using SIX.SCS.QA.Selenium.Tests.SCSClassics.Settings;
 
 namespace SIX.SCS.QA.Selenium.Tests.SCSClassics
 {
     public class ScsClassicTestDirector : TestDirector
     {
-        public override IWebDriverAdapter DefaultTestSetup()
+        public override TestEnvironment TestEnvironment
         {
-            IAuthentication authentication;
-            string baseAdress;
-
-            FireFoxSetUpWithCertifacte(out authentication, out baseAdress);
-
-            var lobby = new Scs(WebDriver);
-            StartUpTest(baseAdress, authentication, lobby);
-
-            return WebDriver;
-        }
-
-        private void FireFoxSetUpWithCertifacte(out IAuthentication authentication, out string baseAdress)
-        {
-            FireFoxWebDriverAdapter(CertifacteProfile);
-            authentication = new CertificateAuthentication(ScsRes.mandant_qa_K, WebDriver);
-            baseAdress = ScsRes.WES_QA_K;
-        }
-
-        private void FireFoxSetUpWithSecurId(out IAuthentication authentication, out string baseAdress)
-        {
-            FireFoxWebDriverAdapter(PlainProfile);
-            authentication = new SecurIdAuthentication("tksyr", "", ScsRes.mandant_prod, "", WebDriver);
-            baseAdress = ScsRes.WES_Production;
+            get { return new QaL(); }
         }
     }
 }
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS/TestLauncher.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSClassics
+{
+    [TestClass]
+    public static class TestLauncher
+    {
+        public static TestDirector TestDirector;
+
+        [AssemblyInitialize]
+        public static void LaunchTestDirector(TestContext testContext)
+        {
+            TestDirector = new ScsClassicTestDirector();
+            TestDirector.PrepareBrowser();
+        }
+
+        [AssemblyCleanup]
+        public static void StopTestDirector()
+        {
+            TestDirector.ShutDownBrowser();
+        }
+    }
+}
