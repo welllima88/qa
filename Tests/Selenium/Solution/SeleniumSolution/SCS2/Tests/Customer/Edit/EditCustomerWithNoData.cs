726fe383ed6c5b58a8d7858358ae146c6abedb85
Marc Siegmund <TKCDEV\siegmund>
2013-07-17 11:25:47 +0000

    created test for customer edit and no data entered (form alerts)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C42931

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Customer/Edit/EditCustomerWithNoData.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Customer;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Customer.Edit
+{
+    [TestClass]
+    public class EditCustomerWithNoData
+    {
+        private static CustomerEdit _customerEdit;
+        private static FormAlert _formAlert;
+        private static CustomerMenu _customerMenu;
+
+        private static List<string> _formAlerts;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _customerMenu = new CustomerMenu();
+            _customerEdit = new CustomerEdit();
+            _formAlert = new FormAlert();
+
+            TestLauncher.Navigate("/Pages/Customer/CustomerEdit.aspx?CustomerId=405125");
+            _customerMenu.CustomerEdit.Click();
+
+            _customerEdit.CustomerName = "";
+            _customerEdit.CompanyName = "";
+            _customerEdit.StreetName = "";
+            _customerEdit.Zip = "";
+            _customerEdit.City = "";
+            _customerEdit.Po = "";
+            _customerEdit.AdressAddition = "";
+            _customerEdit.Region = "";
+            _customerEdit.SapNumber = "";
+
+            _customerEdit.Email = "";
+            _customerEdit.Telephone = "";
+            _customerEdit.Mobile = "";
+            _customerEdit.Fax = "";
+            _customerEdit.Web = "";
+
+            _customerEdit.SaveButton.Click();
+
+            _formAlerts = _formAlert.FormAlertList;
+        }
+
+        [TestMethod]
+        public void EditCustomerWithoutCompanyNameFailed()
+        {
+            _customerEdit.CompanyName = "";
+            _formAlerts = _formAlert.FormAlertList;
+
+            Assert.IsTrue(_formAlerts.Count == 1);
+            CollectionAssert.Contains(_formAlerts, "Firmenname: Zu kurze Eingabe! Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void NumberOfFormAlerts()
+        {
+            Assert.IsTrue(_formAlerts.Count == 5);
+        }
+
+        [TestMethod]
+        public void CustomerNameError()
+        {
+            CollectionAssert.Contains(_formAlerts, "Kundenname: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void CompanyNameError()
+        {
+            CollectionAssert.Contains(_formAlerts, "Firmenname: Dies ist ein Pflichtfeld! Zu kurze Eingabe!");
+        }
+
+        [TestMethod]
+        public void StreetAndNumberError()
+        {
+            CollectionAssert.Contains(_formAlerts, "Strasse / Nr: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void Zip()
+        {
+            CollectionAssert.Contains(_formAlerts, "PLZ: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void City()
+        {
+            CollectionAssert.Contains(_formAlerts, "Ort: Dies ist ein Pflichtfeld!");
+        }
+    }
+}
