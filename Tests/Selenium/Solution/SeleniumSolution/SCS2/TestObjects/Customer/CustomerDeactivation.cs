2ce8bac5649c9ec2aa0d0ef4794906d1cc7699c1
Marc Siegmund <TKCDEV\siegmund>
2013-07-19 09:21:22 +0000

    created test object for customer deactivate / quit
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43014

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Customer/CustomerDeactivation.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Customer
+{
+    public class CustomerDeactivation : WebObject
+    {
+        public IWebElementAdapter Cancel
+        {
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(By.CssSelector("#ctl00_bodyContentPlaceHolder_lblCustomerDeleteInfo"));
+            }
+        }
+
+        public IWebElementAdapter Message
+        {
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(By.CssSelector("#ctl00_bodyContentPlaceHolder_lblCustomerDeleteInfo"));
+            }
+        }
+
+        public IWebElementAdapter DeactivateConfirm
+        {
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(By.CssSelector("#ctl00_bodyContentPlaceHolder_processButton"));
+            }
+        }
+    }
+}
