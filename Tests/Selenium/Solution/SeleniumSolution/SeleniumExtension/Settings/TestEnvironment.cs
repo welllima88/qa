b150e51ee60ce79d7a12c3bd6e6c6105f39d813a
Marc Siegmund <TKCDEV\siegmund>
2013-07-25 12:59:07 +0000

    made test environment fields static to construct them for sure just once
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43142

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Settings/TestEnvironment.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Settings/TestEnvironment.cs
@@ -2,12 +2,12 @@ using SIX.SCS.QA.Selenium.Extension.Authentication;
 
 namespace SIX.SCS.QA.Selenium.Extension.Settings
 {
-    public class TestEnvironment : ITestEnvironment
+    public class TestEnvironment
     {
-        public IApplication Application;
-        public IAuthentication Authentication;
-        public string BaseUrl;
-        public string BrowserProfileName;
-        public SeleniumConfig SeleniumConfig;
+        public static IApplication Application;
+        public static IAuthentication Authentication;
+        public static string BaseUrl;
+        public static string BrowserProfileName;
+        public static SeleniumConfig SeleniumConfig;
     }
 }
