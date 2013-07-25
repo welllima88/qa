df9572723fa61f4b60e45921e3f434f8d2284912
Marc Siegmund <TKCDEV\siegmund>
2013-07-25 09:02:07 +0000

    added test environment for local (wksiegmund)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43126

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Settings/Local.cs
@@ -0,0 +1,23 @@
+﻿using SIX.SCS.QA.Selenium.Extension.Authentication.Method;
+using SIX.SCS.QA.Selenium.Extension.Properties;
+using SIX.SCS.QA.Selenium.Extension.Settings;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.Properties;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Common;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Settings
+{
+    public class Local : TestEnvironment
+    {
+        public Local()
+        {
+            BaseUrl = Scs2Res.UAC_LOCAL;
+            BrowserProfileName = DriverRes.FirefoxProfile_Plain;
+            Application = new LobbyView();
+            Authentication = new UacAuthentication(Scs2Res.uac_username, Scs2Res.uac_passwort);
+            SeleniumConfig = new SeleniumConfig
+                {
+                    Timeouts = new Timeouts {ImplicitlyWait = 5, SetScriptTimeout = 10, SetPageLoadTimeout = 30}
+                };
+        }
+    }
+}
