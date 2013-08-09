d5cc7cbe27b7a2d3e061d1514eef5b3abc0f2024
Marc Siegmund <TKCDEV\siegmund>
2013-08-09 17:59:58 +0000

    used different approacht to make test wait for user input
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43583

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/SecurIdDialogBox.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/SecurIdDialogBox.cs
@@ -1,49 +1,12 @@
-﻿using System;
-using System.Threading;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
 
 namespace SIX.SCS.QA.Selenium.Extension.Authentication.Method
 {
-    public partial class SecurIdDialogBox : Form, IAuthentication
+    public class SecurIdDialogBox : IAuthentication
     {
-        private IAuthentication _authentication;
-        private bool _loginActionPerformed;
-
-        public SecurIdDialogBox()
-        {
-            InitializeComponent();
-        }
-
-        // todo doesn't work to prompt for user input (because of testing framework??), alternatively wait for "onButtonPressed" of Webpage?
         public void Login()
         {
-            var wp = new Thread(WaitForLoginButtonPressed);
-            wp.Name = "WaitForLoginThread";
-            wp.Start();
-            wp.Join();
-        }
-
-        public void WaitForLoginButtonPressed()
-        {
-            while (!_loginActionPerformed)
-            {
-                // wait active for loginButton was pressed
-                Thread.Sleep(88);
-            }
-            PerformLogin();
-        }
-
-        private void PerformLogin()
-        {
-            _authentication = new SecurIdAuthentication(userName.Text, password.Text, mandant.Text, securId.Text);
-            _authentication.Login();
-        }
-
-        private void OnLoginButtonClicked(object sender, EventArgs e)
-        {
-            // prevent second click/call and 'tell' that login shall be performed
-            loginButton.Enabled = false;
-            _loginActionPerformed = true;
+            MessageBox.Show("please login in on to web page and confirm after successful authentication");
         }
     }
 }
