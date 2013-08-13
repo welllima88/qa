1849c02daefa3bff2ad0acad48f6778d5dd4eb71
Marc Siegmund <TKCDEV\siegmund>
2013-08-13 13:06:29 +0000

    made use of ILocation and IAdress
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43632

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/ICustomer.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/ICustomer.cs
@@ -1,35 +1,16 @@
 ﻿namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions
 {
-    public interface ICustomer
+    public interface ICustomer : IAdress, ILocation
     {
-        string Guid { get; }
         string CustomerName { set; get; }
-        string SapNumber { set; get; }
-        string CompanyName { set; get; }
         string Supplier { get; set; }
         string CustomerNumber { get; }
-        string SbsDebitNumber { get; }
-        string SbsAdressNumber { get; }
         string SbsCurrency { get; set; }
         string SbsBillingTenant { get; set; }
-        string Ep2MerchantId { get; set; }
         string CashIntegrator { set; get; }
         string CategoryCode { set; get; }
         string Agency { set; get; }
         string SupportContract { set; get; }
-        string StreetNo { set; get; }
-        string Language { set; get; }
-        string Country { set; get; }
-        string Zip { set; get; }
-        string City { set; get; }
-        string AdressAddition { set; get; }
-        string Po { set; get; }
-        string Region { set; get; }
-        string Telephone { set; get; }
-        string Mobile { set; get; }
-        string Fax { set; get; }
-        string Email { set; get; }
-        string Web { set; get; }
         string Segment { set; get; }
     }
 }
