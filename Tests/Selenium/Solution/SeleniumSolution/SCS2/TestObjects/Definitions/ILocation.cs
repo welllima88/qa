67588e6c00389f9ab0de4820a0fa4174f78d6e15
Marc Siegmund <TKCDEV\siegmund>
2013-08-13 13:06:40 +0000

    use IAdress
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43633

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/ILocation.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/ILocation.cs
@@ -2,26 +2,19 @@
 
 namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions
 {
-    public interface ILocation
+    public interface ILocation:IAdress
     {
         string CompanyName { get; }
         string Guid { get; }
-        string StreetNo { get; }
-        string Zip { get; }
-        string City { get; }
         String SbsDebitNumber { get; }
         String SbsAdressNumber { get; }
         string Language { get; }
-        string Country { get; }
         string Ep2MerchantId { get; }
         string SapNumber { get; }
-        string Po { get; }
-        string AdressAddition { get; }
         string Email { get; }
         string Web { get; }
         string Telephone { get; }
         string Mobile { get; }
-        string Fax { get; }
-        string Region { get; }
+        string Fax { get; }        
     }
 }
