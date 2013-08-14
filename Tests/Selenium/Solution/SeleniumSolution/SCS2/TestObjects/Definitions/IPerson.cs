b88807ebc19a796158982ce47975695dffc807dc
Marc Siegmund <TKCDEV\siegmund>
2013-08-14 13:35:42 +0000

    delete file
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43675

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/IPerson.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions
-{
-    public interface IPerson
-    {
-        string FirstName { set; get; }
-        string Name { set; get; }
-        string Salutation { set; get; }
-    }
-}
