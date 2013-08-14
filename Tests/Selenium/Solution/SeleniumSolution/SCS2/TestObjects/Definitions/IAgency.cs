62d664e9035e8cf5702059050afc8b895e966cf5
Marc Siegmund <TKCDEV\siegmund>
2013-08-14 09:15:24 +0000

    added agency interface, testdata excel
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C43659

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/TestObjects/Definitions/IAgency.cs
@@ -0,0 +1,9 @@
+﻿namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Definitions
+{
+    internal interface IAgency : IAdress
+    {
+        string Name { get; set; }
+        string DisplayName { get; set; }
+        ISupplier Supplier { get; set; }
+    }
+}
