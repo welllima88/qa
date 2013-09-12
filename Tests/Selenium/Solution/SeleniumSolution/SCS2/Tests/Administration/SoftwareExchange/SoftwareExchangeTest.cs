2bd67f13e5721a44643db6fb1dbc15e80294db1c
Marc Siegmund <TKCDEV\siegmund>
2013-09-12 15:55:02 +0000

    added test for agency test
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C44335

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SoftwareExchange/SoftwareExchangeTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Tests.SCSPlatin.TestObjects.Administration.SoftwareExchange;
+
+namespace SIX.SCS.QA.Selenium.Tests.SCSPlatin.Tests.Administration.SoftwareExchange
+{
+    [TestClass]
+    public class SoftwareExchangeTest
+    {
+        private static SoftwareExchangeCreate _softwareExchangeCreate;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            TestLauncher.Navigate("/MasterSoftwareExchange/");
+            _softwareExchangeCreate = new SoftwareExchangeCreate();
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+        }
+
+        [TestMethod]
+        public void CreateSimCardAndSave()
+        {
+        }
+    }
+}
