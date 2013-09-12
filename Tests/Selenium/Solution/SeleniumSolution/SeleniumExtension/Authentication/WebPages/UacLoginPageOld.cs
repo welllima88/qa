148a6db907f95d517cb7bff21bf672d407cb6155
Marc Siegmund <TKCDEV\siegmund>
2013-09-12 16:26:26 +0000

    added old uac login page (used for old scs login)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C44341

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/WebPages/UacLoginPageOld.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+
+namespace SIX.SCS.QA.Selenium.Extension.Authentication.WebPages
+{
+    public class UacLoginPageOld : WebObject
+    {
+        public string UserName
+        {
+            set { WebDriver.FindAdaptedElement(By.CssSelector("input.loginInput[name='spUsername']")).TypeText(value); }
+        }
+
+        public string UserPasswort
+        {
+            set
+            {
+                WebDriver.FindAdaptedElement(By.CssSelector("input.loginInput[name='spPassword']"))
+                         .TypeText(value);
+            }
+        }
+
+        public IWebElement LoginButton
+        {
+            get { return WebDriver.FindElement(By.CssSelector("input.loginInput[alt='Login']")); }
+        }
+    }
+}
