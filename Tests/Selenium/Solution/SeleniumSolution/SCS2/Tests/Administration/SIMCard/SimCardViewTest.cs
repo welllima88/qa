946d51786db328c64701c6a2ab1cb0724699b94e
Marc Siegmund <TKCDEV\siegmund>
2013-10-10 15:52:30 +0000

    added tests
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45141

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SIMCard/SimCardViewTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SIMCard/SimCardViewTest.cs
@@ -1,7 +1,6 @@
 ﻿using System.Text.RegularExpressions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Administration.SimCard;
-using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
 
 namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Administration.SIMCard
@@ -9,16 +8,6 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Administration.SIMCard
     [TestClass]
     public class SimCardCreateTest
     {
-        private static SimCardView SimCardView;
-        private static LobbyView LobbyView;
-
-        [ClassInitialize]
-        public static void ClassInit(TestContext testContext)
-        {
-            SimCardView = new SimCardView();
-            LobbyView = new LobbyView();
-        }
-
         [TestInitialize]
         public void TestInit()
         {
@@ -26,15 +15,50 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Administration.SIMCard
         }
 
         [TestMethod]
-        public void CheckSimCardView()
+        public void MobileNumber()
         {
             StringAssert.Matches(SimCardView.SimCardElement.MobileNumber, new Regex(@"\d{10}"));
+        }
+
+        [TestMethod]
+        public void SimCardNumber()
+        {
             StringAssert.Matches(SimCardView.SimCardElement.SimCardNumber, new Regex(@"\d{18}"));
+        }
+
+        [TestMethod]
+        public void Pin()
+        {
             StringAssert.Matches(SimCardView.SimCardElement.Pin, new Regex(@"\d{4}"));
+        }
+
+        [TestMethod]
+        public void Puk()
+        {
             StringAssert.Matches(SimCardView.SimCardElement.Puk, new Regex(@"\d{8}"));
+        }
+
+        [TestMethod]
+        public void TerminalId()
+        {
             StringAssert.Matches(SimCardView.SimCardElement.TerminalId, new Regex(@"\d+"));
+        }
+
+        [TestMethod]
+        public void Status()
+        {
             Assert.IsNotNull(SimCardView.SimCardElement.Status);
+        }
+
+        [TestMethod]
+        public void Region()
+        {
             Assert.IsNotNull(SimCardView.SimCardElement.Region);
+        }
+
+        [TestMethod]
+        public void Usage()
+        {
             Assert.IsNotNull(SimCardView.SimCardElement.Usage);
         }
 
