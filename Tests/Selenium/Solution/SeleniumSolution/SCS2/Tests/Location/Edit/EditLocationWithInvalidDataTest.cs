039e323e2a84b46912a03f2393acfa2d9ebd8a31
Marc Siegmund <TKCDEV\siegmund>
2013-10-15 09:34:33 +0000

    added test for invalid location edit
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45177

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Location/Edit/EditLocationWithInvalidDataTest.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Edit
+{
+    [TestClass]
+    public class EditLocationWithInvalidDataTest
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestLauncher.Navigate("/Location?LocationId=4b77d4f2-d5db-4823-9d52-63c1534de457");
+            LocationMenu.LocationEdit.Click();
+
+            LocationEdit.CompanyName = "SYR Sele Firma A$°";
+            LocationEdit.StreetNo = "Hardturmstr. 201$°";
+            LocationEdit.Zip = "802$°";
+            LocationEdit.City = "Zürich$°";
+            LocationEdit.Po = "PFO1$°";
+            LocationEdit.AdressAddition = "Etage 3$°";
+            LocationEdit.Region = "Reg 3[]$°";
+            LocationEdit.SapNumber = "444$°";
+
+            LocationEdit.Language = "Deutsch [de]";
+            LocationEdit.Country = "Schweiz [CH]";
+            LocationEdit.Email = "marc.siegmund@six-grou$°p.com()";
+            LocationEdit.Telephone = "0031 58 399 6237$";
+            LocationEdit.Mobile = "0032 58 399 6237$";
+            LocationEdit.Fax = "0033 58 399 6237$";
+            LocationEdit.Web = "www.six-^°@}$.com/de-inte[]rn$°";
+
+            LocationEdit.SaveButton.Click();
+        }
+
+        [TestMethod]
+        public void CompanyName()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Firmenname: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void AdressAddition()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Zusatz: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Telephone()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Telefon: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Mobile()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Mobiltelefon: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void EditLocationWithInvalidDataFailed()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Fax: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void StreetNo()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Strasse / Nr: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Zip()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "PLZ: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Po()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Postfach: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void City()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Ort: Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Region()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Region (Kurzzeichen): Ungültige Zeichen gefunden!");
+        }
+
+        [TestMethod]
+        public void Email()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Email: Dies ist keine gültige E-Mail Adresse!");
+        }
+
+        [TestMethod]
+        public void Web()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Web: Dies ist keine gültige Web Adresse!");
+        }
+
+        [TestMethod]
+        public void NumberOfAlerts()
+        {
+            Assert.AreEqual(12, FormAlert.FormAlertList.Count, "Wrong number of form alerts");
+        }
+    }
+}
