0622a691c09b49128fe79eea316c46d0b87057c0
Marc Siegmund <TKCDEV\siegmund>
2013-10-15 16:28:47 +0000

    added test for location edit with missing data
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45195

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Location/Edit/LocationEditWithMissingDataTest.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Edit
+{
+    [TestClass]
+    public class LocationEditWithMissingDataTest
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestDirector.Navigate("Location?LocationId=4b77d4f2-d5db-4823-9d52-63c1534de457");
+            LocationMenu.LocationEdit.Click();
+
+            LocationEdit.StreetNo = "Hardturmstr. 201";
+            LocationEdit.Zip = "8021";
+            LocationEdit.City = "Zürich";
+            LocationEdit.Po = "PFO1";
+            LocationEdit.AdressAddition = "Etage 3";
+            LocationEdit.SapNumber = "4440";
+
+            LocationEdit.Language = "Deutsch [de]";
+            LocationEdit.Country = "Schweiz [CH]";
+            LocationEdit.Email = "marc.siegmund@six-group.com";
+            LocationEdit.Telephone = "0031 58 399 6237";
+            LocationEdit.Mobile = "0032 58 399 6237";
+            LocationEdit.Fax = "0033 58 399 6237";
+            LocationEdit.Web = "www.six-group.com/de-intern";
+
+            LocationEdit.CompanyName = "SYR Sele Firma A";
+        }
+
+        [TestMethod]
+        public void EditLocationWithoutCompanyNameFailed()
+        {
+            LocationEdit.CompanyName = "";
+            LocationEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count, "Wrong number of form alerts");
+            Assert.IsTrue(FormAlert.FormAlertList.Contains("Firmenname: Zu kurze Eingabe! Dies ist ein Pflichtfeld!"),
+                          "Wrong text in form alerts");
+            LocationEdit.CancelButton.Click();
+        }
+
+        [TestMethod]
+        public void EditLocationWithoutStreetAndNumberFailed()
+        {
+            LocationEdit.StreetNo = "";
+
+            LocationEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Strasse / Nr: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditLocationWithoutCityFailed()
+        {
+            LocationEdit.City = "";
+
+            LocationEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Ort: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditLocationWithoutZipFailed()
+        {
+            LocationEdit.Zip = "";
+
+            LocationEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "PLZ: Dies ist ein Pflichtfeld!");
+        }
+    }
+}
