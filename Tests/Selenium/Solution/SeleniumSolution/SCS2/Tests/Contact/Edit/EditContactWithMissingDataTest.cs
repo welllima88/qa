3b92bea56c9e7babcf8c6ef52b00ad0a60ac120b
Marc Siegmund <TKCDEV\siegmund>
2013-10-23 14:25:40 +0000

    added test for contact edit with missing data
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45328

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Contact/Edit/EditContactWithMissingDataTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Person;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Contact.Edit
+{
+    [TestClass]
+    public class EditContactWithMissingDataTest
+    {
+        private readonly string _contactId;
+
+        public EditContactWithMissingDataTest(string contactId)
+        {
+            _contactId = contactId;
+        }
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestDirector.Navigate("/Pages/Person/ContactPersonEdit.aspx?PageMode=view&PersonId=" + _contactId);
+
+            ContactMenu.ContactEdit.Click();
+
+            ContactPersonEdit.Salutation = "Herr";
+            ContactPersonEdit.FirstName = "Marc";
+            ContactPersonEdit.Name = "Siegmund";
+            ContactPersonEdit.Language = "Italienisch [it]";
+            ContactPersonEdit.Telephone = "0123456789";
+            ContactPersonEdit.Mobile = "1234567890";
+            ContactPersonEdit.Fax = "2345678901";
+            ContactPersonEdit.Email = "tksyr_contact@six-group.com";
+            ContactPersonEdit.Po = "Pof";
+            ContactPersonEdit.StreetNo = "Strasse 1";
+            ContactPersonEdit.Zip = "2351";
+            ContactPersonEdit.City = "City";
+            ContactPersonEdit.Region = "Reg";
+            ContactPersonEdit.Country = "Frankreich [FR]";
+            ContactPersonEdit.AdressAddition = "Addr-Add";
+        }
+
+        [TestMethod]
+        public void EditContactWithoutFirstNameFailed()
+        {
+            ContactPersonEdit.FirstName = "";
+            ContactPersonEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Firmenname: Zu kurze Eingabe! Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditContactWithoutNameFailed()
+        {
+            ContactPersonEdit.Name = "";
+            ContactPersonEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Firmenname: Zu kurze Eingabe! Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditContactWithoutStreetAndNumberFailed()
+        {
+            ContactPersonEdit.StreetNo = "";
+
+            ContactPersonEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Strasse / Nr: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditContactWithoutCityFailed()
+        {
+            ContactPersonEdit.City = "";
+
+            ContactPersonEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Ort: Dies ist ein Pflichtfeld!");
+        }
+
+        [TestMethod]
+        public void EditContactWithoutZipFailed()
+        {
+            ContactPersonEdit.Zip = "";
+            ContactPersonEdit.SaveButton.Click();
+
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+            CollectionAssert.Contains(FormAlert.FormAlertList, "PLZ: Dies ist ein Pflichtfeld!");
+        }
+    }
+}
