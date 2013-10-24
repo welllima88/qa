7d8f3200868284420d925b07b1d0022f5a5d71fc
Marc Siegmund <TKCDEV\siegmund>
2013-10-24 13:19:08 +0000

    added test for location edit without give reason by user
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45360

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Location/Edit/LocationEditWithoutReasonTest.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Edit
+{
+    [TestClass]
+    public class LocationEditWithoutReasonTest
+    {
+        [ClassInitialize]
+        public void TestInit()
+        {
+            TestDirector.Navigate("Location?LocationId=071b90c2-c8c5-42c5-87c5-d816b26b0a1a");
+            LocationView.EditButton.Click();
+            
+            LocationEdit.CompanyName = "SYR" + TestLauncher.GenerateTestId();
+            
+            LocationCreate.SaveButton.Click();
+        }
+
+        [TestMethod]
+        public void FormAlertCheck()
+        {
+            CollectionAssert.Contains(FormAlert.FormAlertList, "Änderungsgrund - In diesem Feld wird eine Eingabe benötigt!");
+        }
+
+        [TestMethod]
+        public void FormAlertNumber()
+        {
+            Assert.AreEqual(1, FormAlert.FormAlertList.Count);
+        }
+    }
+}
