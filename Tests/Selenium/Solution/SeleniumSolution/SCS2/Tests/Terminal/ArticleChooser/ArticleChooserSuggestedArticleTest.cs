87bc84d7b47272fb74e5c079a11523faeae729a6
Marc Siegmund <TKCDEV\siegmund>
2013-10-29 12:13:41 +0000

    added article lookup and suggested article test
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45478

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/ArticleChooser/ArticleChooserSuggestedArticleTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Terminal.ArticleChooser
+{
+    [TestClass]
+    public class ArticleChooserSuggestedArticleTest
+    {
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestDirector.Navigate(
+                "TerminalArticleSelector/LocationTerminalNew/?LOCATIONID=e7fc5338-dff2-4556-866d-27b96d225a97");
+        }
+
+        [TestMethod]
+        public void SuggestedListHasEntries()
+        {
+            Assert.IsTrue(TerminalChooser.SuggestedArtikelList.Count > 2);
+        }
+
+        [TestMethod]
+        public void SuggestedListHasConcreteEntry()
+        {
+            CollectionAssert.Contains(TerminalChooser.SuggestedArtikelList, "  xentissimo MOBILE WLAN, TCP/IP TCPIPEP2  15401");
+        }
+    }
+}
