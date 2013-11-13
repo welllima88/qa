7456f48bfdfcc2dd30a73b9012168a9866af5a3a
Marc Siegmund <TKCDEV\siegmund>
2013-11-13 16:43:00 +0000

    updated framework and project settings due to resharper problem
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45861

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/SearchResult/CustomerResult.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/SearchResult/CustomerResult.cs
@@ -1,10 +1,38 @@
+using System;
+using OpenQA.Selenium;
+
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects.SearchResult
 {
-    public class CustomerResult : SearchResult
+    public class CustomerResult : WebObject
     {
-        public CustomerResult()
+        private readonly string _customerId;
+
+        public CustomerResult(string customerId)
         {
-            Prefix = "div#customerDiv.container ";
+            _customerId = customerId;
+        }
+
+        public static string CustomerId
+        {
+            get { return WebDriver.FindElement(By.CssSelector("tbody#customerResult span#customerId")).Text; }
+        }
+
+        public static IWebElement ShowDescent(string customerId)
+        {
+            return ShowDescents().FindElement(By.CssSelector("img[node_searchhint='" + customerId +
+                                                             "'][alt='child_expanderIcon']"));
+        }
+
+        public static IWebElement ShowDescents()
+        {
+            return
+                WebDriver.FindElement(By.CssSelector("tbody#customerResult"));
+        }
+
+        public static IWebElement Result(string s)
+        {
+            throw new NotImplementedException(" almost finished");
+            //return ShowDescents().FindElement(By.CssSelector(""));
         }
     }
 }
