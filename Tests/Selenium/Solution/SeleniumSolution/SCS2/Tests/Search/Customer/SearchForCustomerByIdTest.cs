8a1a31e67ada2fbe50b3b9dda572fb7d31b88fc7
Marc Siegmund <TKCDEV\siegmund>
2013-11-13 16:44:01 +0000

    search for CustomerByIdTest updated project and framework settings edited
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C45862

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Search/Customer/SearchForCustomerByIdTest.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.SearchResult;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Search.Customer
+{
+    /// <summary>
+    ///     be careful with menu expander because they prevent some actions and need special handling
+    /// </summary>
+    [TestClass]
+    public class SearchForCustomerByIdTest
+    {
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            TestDirector.Navigate();
+            QuickSearch.SearchField = "401858";
+            QuickSearch.SearchButton.Click();
+        }
+
+        [TestMethod]
+        [TestCategory("Search"), TestCategory("Customer")]
+        public void CustomerResultNotEmpty()
+        {
+        }
+
+        [TestMethod]
+        [TestCategory("Search"), TestCategory("Customer")]
+        public void SearchStringInHeadlineIsDisplayed()
+        {
+            StringAssert.Contains(SearchResult.Headline, "401858");
+        }
+
+        [TestMethod]
+        [TestCategory("Search"), TestCategory("Customer")]
+        public void CustomerIdInResultIsDisplayed()
+        {
+            StringAssert.Contains(CustomerResult.Result("401858").Text, "401858");
+        }
+
+        [TestMethod]
+        [TestCategory("Search"), TestCategory("Customer")]
+        public void ShowLocationsAndTerminals()
+        {
+            CustomerResult.ShowDescent("401858").Click();
+            Assert.IsFalse(CustomerResult.ShowDescent("").Displayed);            
+        }
+    }
+}
