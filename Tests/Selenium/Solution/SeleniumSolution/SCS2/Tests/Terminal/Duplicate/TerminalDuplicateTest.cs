1965731e7b58fcb402dc6b7cebc79280eb5647e3
Marc Siegmund <TKCDEV\siegmund>
2013-11-20 13:03:03 +0000

    added test object and test for terminal duplicate
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C46055

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/Duplicate/TerminalDuplicateTest.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Duplicate;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Terminal.Duplicate
+{
+    [TestClass]
+    public class TerminalDuplicateTest
+    {
+        private long _dt;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            TestDirector.Navigate("TerminalDuplicate?TerminalId=21013049");
+            _dt = TestLauncher.GenerateTestId();
+        }
+
+        [TestMethod]
+        public void DuplicateTerminalWithCopyPasswordWithAcquirerNotificationToLocation()
+        {
+            TerminalDuplicate.CopyPassword = true;
+            TerminalDuplicate.AcquirerNotification = true;
+            TerminalDuplicate.Location = "";
+            TerminalDuplicate.InstallationNotification =
+                "DuplicateTerminalWithCopyPasswordWithAcquirerNotificationToLocation";
+        }
+
+        [TestMethod]
+        public void DuplicateTerminalWithNoCopyPasswordWithNoAcquirerNotificationToCustomerLocation()
+        {
+            TerminalDuplicate.CopyPassword = false;
+            TerminalDuplicate.AcquirerNotification = false;
+            TerminalDuplicate.Location = "";
+            TerminalDuplicate.InstallationNotification =
+                "DuplicateTerminalWithCopyPasswordWithAcquirerNotificationToLocation";
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Terminal/Duplicate/TerminalDuplicate.cs
@@ -0,0 +1,31 @@
+using System;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Duplicate
+{
+    public static class TerminalDuplicate
+    {
+        public static bool CopyPassword
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public static bool AcquirerNotification
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public static string Location
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+
+        public static string InstallationNotification
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
