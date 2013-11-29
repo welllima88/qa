3d0d9a5f145453d558ae523af01d0c0ce280c562
Marc Siegmund <TKCDEV\siegmund>
2013-11-29 09:48:49 +0000

    added check and renamed
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C46276

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/Duplicate/TerminalDuplicateWithDifferentPasswords.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Terminal/Duplicate/TerminalDuplicateWithDifferentPasswords.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SIX.SCS.QA.Selenium.Extension;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Massmuation;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Dashboard;
@@ -7,17 +8,18 @@ using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Duplicate;
 namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Terminal.Duplicate
 {
     [TestClass]
-    public class TerminalDuplicateWithDefaultTest
+    public class TerminalDuplicateWithDifferentPasswords
     {
-        private const int NumberOfTerminals = 10;
+        private const int NumberOfDuplications = 10;
+        private const string TerminalId = "21013049";
 
         [ClassInitialize]
         public static void TestInit(TestContext testContext)
         {
-            TestDirector.Navigate("TerminalDashboard/?TerminalId=21013049");
+            TestDirector.Navigate("TerminalDashboard/?TerminalId=" + TerminalId);
             SalesContractPortlet.TerminalDuplicate.Click();
-            TerminalDuplicate.NumberOfTerminals = NumberOfTerminals;
-            TerminalDuplicate.InstallationMessage = "TerminalDuplicateWithDefaultTest";
+            TerminalDuplicate.NumberOfTerminals = NumberOfDuplications;
+            TerminalDuplicate.CopyPassword = false;
             TerminalDuplicate.DulpicateButton.Click();
         }
 
@@ -25,6 +27,7 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Terminal.Duplicate
         public void PasswordsAreDifferent()
         {
             CollectionAssert.AllItemsAreUnique(TerminalMassValidation.Passwords);
+            Assert.AreEqual(NumberOfDuplications, TerminalMassValidation.Passwords.Distinct().Count());
         }
 
         [TestMethod]
@@ -32,14 +35,14 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Terminal.Duplicate
         {
             foreach (string referenceTerminalId in TerminalMassValidation.ReferenceTerminalIds)
             {
-                Assert.AreEqual("21013049", referenceTerminalId);
+                Assert.AreEqual(TerminalId, referenceTerminalId);
             }
         }
 
         [TestMethod]
         public void NumberOfTerminalsToDuplicate()
         {
-            Assert.AreEqual(TerminalMassValidation.Count, NumberOfTerminals);
+            Assert.AreEqual(TerminalMassValidation.Count, NumberOfDuplications);
         }
     }
 }
