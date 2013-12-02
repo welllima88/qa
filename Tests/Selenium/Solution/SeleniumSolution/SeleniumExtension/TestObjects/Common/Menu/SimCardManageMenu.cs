5a7a6faf42c5b8474fc9d671042a2b55690302ab
Marc Siegmund <TKCDEV\siegmund>
2013-12-02 16:22:57 +0000

    removed class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C46330

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Menu/SimCardManageMenu.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
-
-namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu
-{
-    public class SimCardManageMenu : WebObject
-    {
-    }
-}
