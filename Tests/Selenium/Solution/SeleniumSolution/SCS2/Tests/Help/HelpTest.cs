d8d6d78d8daa2098438a07cc8d3acd8c6725113e
Marc Siegmund <TKCDEV\siegmund>
2013-12-19 17:39:14 +0000

    back to the roots - removed from smoke test the help hell test, which dont give a useful result - implemented an order component for caipi
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C46770

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Help/HelpTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Help/HelpTest.cs
@@ -21,21 +21,21 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Help
         }
 
         [TestMethod]
-        [TestCategory("LobbyCheck"), TestCategory("Help")]
+        [TestCategory("Help")]
         public void HelpIsAvialable()
         {
             Assert.AreEqual("Startseite", HelpNavigation.Help.Text);
         }
 
         [TestMethod]
-        [TestCategory("LobbyCheck"), TestCategory("Help")]
+        [TestCategory("Help")]
         public void ScsHelpLinkIsAvailable()
         {
             Assert.AreEqual("SCS", HelpNavigation.Scs.Text);
         }
 
         [TestMethod]
-        [TestCategory("LobbyCheck"), TestCategory("Help")]
+        [TestCategory("Help")]
         public void Scs2HelpLinkIsAvailable()
         {
             Assert.AreEqual("SCS2", HelpNavigation.Scs2.Text);
