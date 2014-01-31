ca9a10375ae7efe5286a12fce635b5680d801513
Marc Siegmund <TKCDEV\siegmund>
2014-01-31 12:19:01 +0000

    lobby header test
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C47542

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Lobby/LobbyHeader.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Lobby/LobbyHeader.cs
@@ -15,9 +15,6 @@ using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
 
 namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Lobby
 {
-    /// <summary>
-    ///     be careful with menu expander because they prevent some actions and need special handling
-    /// </summary>
     [TestClass]
     public class LobbyHeaderTest
     {
