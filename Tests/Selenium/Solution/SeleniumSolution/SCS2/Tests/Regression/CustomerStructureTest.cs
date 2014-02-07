27f9e5846957ef747413b26e00b38a2cdef3c96f
Marc Siegmund <TKCDEV\siegmund>
2014-02-07 16:22:06 +0000

    added dummy test which includes a simple test scenario (still experimental)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C47861

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Regression/CustomerStructureTest.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Contact.Create;
+using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Customer.Create;
+using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Create;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Customer;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
+{
+    [TestClass]
+    public class CustomerStructureTest
+    {
+        private string _customerNumber;
+        private string _locationGuid;
+
+        [TestInitialize]
+        public void TestInit()
+        {
+            CustomerCreateTest.SetCustomerData();
+
+            _customerNumber = CustomerView.CustomerNumber;
+
+            CustomerMenu.ContactCreate.Click();
+            ContactCreateTest.SetContactData();
+
+            LocationCreateAndSaveTest.SetLocationData();
+            _locationGuid = LocationView.Guid;
+
+            LocationMenu.ContactCreate.Click();
+            ContactCreateTest.SetContactData();
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+            CustomerMenu.CustomerDeactivate.Click();
+        }
+
+        [TestMethod]
+        public void CustomerIsCreated()
+        {
+            Assert.AreEqual(_customerNumber, CustomerView.CustomerNumber);
+        }
+
+        [TestMethod]
+        public void LocationIsCreated()
+        {
+            Assert.AreEqual(_customerNumber, CustomerView.CustomerNumber);
+        }
+
+        [TestMethod]
+        public void ContactToCustomerIsCreated()
+        {
+            Assert.AreEqual(_customerNumber, CustomerView.CustomerNumber);
+        }
+
+        [TestMethod]
+        public void ContactToLocationIsCreated()
+        {
+            Assert.AreEqual(_customerNumber, CustomerView.CustomerNumber);
+        }
+    }
+}
