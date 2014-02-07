75a655c072e16a9d208526444e3a64e401f2c638
Marc Siegmund <TKCDEV\siegmund>
2014-02-07 08:33:10 +0000

    some gui tests during location create
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C47815

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Location/Create/LocationGuiTest.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.Selenium;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Customer;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+
+namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Create
+{
+    internal class LocationGuiTest
+    {
+        [TestInitialize]
+        public void OpenCustomer()
+        {
+            TestDirector.Navigate("Pages/Customer/CustomerEdit.aspx?CustomerId=404871");
+        }
+
+        [TestMethod]
+        public void CountryOfCustomerIsUsedForAgencyOfLocation()
+        {
+            string country = CustomerView.Country;
+            CustomerMenu.LocationCreate.Click();
+            Assert.AreEqual(country, LocationCreate.Country);
+        }
+
+        [TestMethod]
+        public void LanguageOfCustomerIsUsedForAgencyOfLocation()
+        {
+            string language = CustomerView.Language;
+            CustomerMenu.LocationCreate.Click();
+            Assert.AreEqual(language, LocationCreate.Language);
+        }
+
+        [TestMethod]
+        public void CreateLocationCheckNavBar()
+        {
+            Assert.AreEqual("Kunde", NavigationBar.Current.Text);
+            CustomerMenu.LocationCreate.Click();
+            Assert.AreEqual("Kunde", NavigationBar.Current.Text);
+        }
+
+        [TestMethod]
+        public void CreateLocationCheckHeadline()
+        {
+            CustomerMenu.LocationCreate.Click();
+            Assert.AreEqual("Neuer Standort", LobbyView.Headline.Text);
+        }
+    }
+}
