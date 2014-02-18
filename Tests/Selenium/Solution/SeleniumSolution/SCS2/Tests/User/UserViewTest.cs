60d5ef7bb2217d1d43a1903624d08932f653fc49
Marc Siegmund <TKCDEV\siegmund>
2014-02-18 14:53:31 +0000

    added categories to put in tests to daily smoke test
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48192

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/User/UserViewTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/User/UserViewTest.cs
@@ -22,60 +22,70 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.User
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void UserName()
         {
             Assert.AreEqual("six_scs_auto", UserView.UserName);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void UserPassword()
         {
             Assert.AreEqual("", UserView.Password);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void UserSalutation()
         {
             Assert.AreEqual("Herr", UserView.Salutation);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void FirstName()
         {
             Assert.AreEqual("Automatisierer", UserView.FirstName);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void LastName()
         {
             Assert.AreEqual("SCS", UserView.LastName);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void Email()
         {
             Assert.AreEqual("marc.siegmund@six-group.com", UserView.Email);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void Language()
         {
             Assert.AreEqual("Deutsch [de]", UserView.Language);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void SecureId()
         {
             Assert.AreEqual("tksyr", UserView.SecureId);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void WesMandant()
         {
             StringAssert.Contains(UserView.WesMandant, "TKCPOS");
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void LastLogin()
         {
             new NotImplementedException("to bed done because of date");
@@ -83,30 +93,35 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.User
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void Created()
         {
             StringAssert.Contains(UserView.Created, "25.07.2012");
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void Comment()
         {
             Assert.AreEqual("DO NOT DELETE", UserView.Comment);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void EditButton()
         {
             Assert.IsTrue(UserView.EditButton.Displayed);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void ListUsersButton()
         {
             Assert.IsTrue(UserView.ListUsersButton.Displayed);
         }
 
         [TestMethod]
+        [TestCategory("SmokeTest")]
         public void ReactivateButton()
         {
             Assert.IsTrue(UserView.ReactivateButton.Displayed);
