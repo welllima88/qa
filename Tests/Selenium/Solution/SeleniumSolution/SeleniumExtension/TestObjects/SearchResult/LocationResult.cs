a6232767e754f3913812e3d646099255a4c5a822
Marc Siegmund <TKCDEV\siegmund>
2014-02-27 08:52:55 +0000

    added result element for location search
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48444

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/SearchResult/LocationResult.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/SearchResult/LocationResult.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects.SearchResult
 {
     public class LocationResult : SearchResults
@@ -5,6 +7,11 @@ namespace SIX.SCS.QA.Selenium.Extension.TestObjects.SearchResult
         public LocationResult()
         {
             Prefix = "div#locationDiv.container ";
+        }
+
+        public static IWebElement Result()
+        {
+            return WebDriver.FindAdaptedElement(By.CssSelector("tbody#locationResult a#locationLink"));
         }
     }
 }
