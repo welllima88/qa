185be8165f9dd07045d8cffad62a69202f4a7dcf
Marc Siegmund <TKCDEV\siegmund>
2014-03-06 17:17:41 +0000

    added test object for terminal list
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48682

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Terminal/TerminalList.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal
+{
+    public class TerminalList:WebObject
+    {
+        public static IWebElement First()
+        {
+           return WebDriver.FindAdaptedElement(By.CssSelector("tbody tr td#content table.dataTable tbody tr td a[href*='/TerminalDashboard?TerminalId=']"));
+        }
+    }
+}
