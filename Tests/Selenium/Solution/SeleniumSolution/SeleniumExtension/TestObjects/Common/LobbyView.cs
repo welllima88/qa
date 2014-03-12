c2e4c3eefb8d2dfbff1dcfc1a163de2f7635816f
Marc Siegmund <TKCDEV\siegmund>
2014-03-12 17:11:12 +0000

    removed fields
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48827

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/LobbyView.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/LobbyView.cs
@@ -3,22 +3,11 @@ using OpenQA.Selenium;
 using SIX.SCS.QA.Selenium.Extension.Authentication;
 using SIX.SCS.QA.Selenium.Extension.Selenium;
 using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
-using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
 
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common
 {
     public class LobbyView : WebObject, IApplication
     {
-        public static ApplicationInfo ApplicationInfo;
-        public static Footer Footer;
-        public static LobbyMenu Menu;
-        public static MetaNavBar MetaNavBar;
-        public static NavigationBar NavigationBar;
-        public static ProcessTickets ProcessTickets;
-        public static RecentElements RecentElements;
-        public static RecentMassmutations RecentMassmutations;
-        public static QuickSearch QuickSearch;
-
         public static IWebElement CompanyLogo
         {
             get { return WebDriver.FindElement(By.CssSelector(CommonRes.Lobby_CompanyLogo)); }
