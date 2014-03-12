e4555e4124cc807a84d01159a95baf919bf743b0
Marc Siegmund <TKCDEV\siegmund>
2014-03-12 15:59:22 +0000

    added search workflow as more abstraction of test objects
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48812

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/Search.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Customer;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.SearchResult;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Dashboard;
+
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public class Search
+    {
+        public static void CustomerCanBeFoundByCustomerId(string customerId)
+        {
+            QuickSearch.Find(customerId);
+            CustomerResult.Result().Click();
+
+            Assert.AreEqual(customerId, CustomerView.CustomerNumber);
+        }
+
+        public static void LocationCanBeFoundByLocationName(string locationName)
+        {
+            QuickSearch.Find(locationName);
+            LocationResult.Result().Click();
+
+            Assert.AreEqual(locationName, LocationView.CompanyName);
+        }
+
+        public static void CustomerCanBeFoundByCustomerName(string customerName)
+        {
+            QuickSearch.Find(customerName);
+            CustomerResult.Result().Click();
+
+            Assert.AreEqual(customerName, CustomerView.CustomerName);
+        }
+
+        public static void TerminalCanBeFoundById(string terminalId)
+        {
+            QuickSearch.Find(terminalId);
+            TerminalResult.First().Click();
+
+            Assert.AreEqual(terminalId, TerminalInfo.TerminalId);
+        }
+    }
+}
