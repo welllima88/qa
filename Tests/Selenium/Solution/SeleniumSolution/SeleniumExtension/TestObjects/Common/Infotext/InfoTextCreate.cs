8f6305d398c37aff20e08f04ad3d8bccabc6e7be
Marc Siegmund <TKCDEV\siegmund>
2014-03-14 15:18:42 +0000

    added test object for infotext gui
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48899

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Infotext/InfoTextCreate.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Infotext
+{
+    public class InfoTextCreate : WebObject
+    {
+        public static string Text
+        {
+            set { WebDriver.FindAdaptedElement(By.CssSelector("textarea#InfoText_Text")).TextArea().TypeText(value); }
+        }
+
+        public static IWebElement SaveButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("input#save")); }
+        }
+
+        public static IWebElement CancelButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("input#cancel")); }
+        }
+    }
+}
