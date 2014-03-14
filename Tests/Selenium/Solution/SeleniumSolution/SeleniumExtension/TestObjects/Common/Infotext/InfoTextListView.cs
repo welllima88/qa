c00610bdb0a9021591082af03ba27d4c6ead4c67
Marc Siegmund <TKCDEV\siegmund>
2014-03-14 15:59:22 +0000

    added infotext list as test object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C48904

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Infotext/InfoTextListView.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Infotext
+{
+    public class InfoTextListView : WebObject
+    {
+        public static IWebElement CreateInfotextButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content div button[name='create'][href*='/InfoText/']")); }
+        }
+    }
+}
