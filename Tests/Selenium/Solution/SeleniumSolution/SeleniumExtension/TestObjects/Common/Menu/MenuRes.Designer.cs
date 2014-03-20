489106e0286358b592ff9a7aab364141f1a02615
Marc Siegmund <TKCDEV\siegmund>
2014-03-20 14:49:56 +0000

    updated locator for menu
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49004

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Menu/MenuRes.Designer.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Menu/MenuRes.Designer.cs
@@ -295,7 +295,7 @@ namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu {
         }
         
         /// <summary>
-        ///   Looks up a localized string similar to td#leftHandMenu a[href*=&apos;/Pages/InfoText/LocationInfoTextNew.aspx?CUSTOMERID=&apos;].
+        ///   Looks up a localized string similar to td#leftHandMenu a[href*=&apos;/InfoText/NewLocationInfoText&apos;].
         /// </summary>
         public static string LocationMenu_InfotextCreate {
             get {
