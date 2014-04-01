cdb1b3470d0ee89198e20bdd77f3919d1d87d6a2
Marc Siegmund <TKCDEV\siegmund>
2014-04-01 12:51:43 +0000

    added mpd test object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49248

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Mpd/MpdCreate.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd
+{
+    public class MpdCreate : WebObject
+    {
+        public static string Description
+        {
+            set { WebDriver.FindAdaptedElement(By.CssSelector("input#Description")).TextBox().TypeText(value); }
+        }
+
+        public static string Adress
+        {
+            set { WebDriver.FindAdaptedElement(By.CssSelector("input#InetAddr")).TextBox().TypeText(value); }
+        }
+
+        public static string Port
+        {
+            set { WebDriver.FindAdaptedElement(By.CssSelector("input#InetPort")).TextBox().TypeText(value); }
+        }
+
+        public static IWebElement SaveButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("input#save")); }
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Mpd/MpdListView.cs
@@ -0,0 +1,13 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd
+{
+    public class MpdListView : WebObject
+    {
+        public static IWebElement CreateButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("button#create")); }
+        }
+    }
+}
