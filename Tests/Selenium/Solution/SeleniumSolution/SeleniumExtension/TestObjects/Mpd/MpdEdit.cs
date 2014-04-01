d07284207ad899817b2b9a712433a3c673fc2ce3
Marc Siegmund <TKCDEV\siegmund>
2014-04-01 15:34:56 +0000

    added test objects for mpd edit and view
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49255

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Mpd/MpdEdit.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd
+{
+    public class MpdEdit : WebObject
+    {
+        public static string Description
+        {
+            set { MpdCreate.Description = value; }
+        }
+
+        public static string Adress
+        {
+            set { MpdCreate.Adress = value; }
+        }
+
+        public static string Port
+        {
+            set { MpdCreate.Port = value; }
+        }
+
+        public static IWebElement SaveButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("input#save")); }
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Mpd/MpdView.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd
+{
+    public class MpdView : WebObject
+    {
+        public static string Id
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("div#MpdId")).Text; }
+        }
+
+        public static string Description
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("div#Description")).Text; }
+        }
+
+        public static string Adress
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("div#InetAddr")).Text; }
+        }
+
+        public static string Port
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("div#InetPort")).Text; }
+        }
+    }
+}
