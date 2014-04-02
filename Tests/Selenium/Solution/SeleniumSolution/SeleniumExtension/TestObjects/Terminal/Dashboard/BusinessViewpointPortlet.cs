b46aca4a4c9b8deb361a91cfc58a8bc9f19b25cd
Marc Siegmund <TKCDEV\siegmund>
2014-04-02 16:21:32 +0000

    added object for one portlet
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49290

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Terminal/Dashboard/BusinessViewpointPortlet.cs
@@ -0,0 +1,61 @@
+using OpenQA.Selenium;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Dashboard
+{
+    public class BusinessViewpointPortlet : PortletViewBase
+    {
+        public IWebElement ReceiptHeader
+        {
+            get
+            {
+                return
+                    WebDriver.FindElement(
+                        By.CssSelector("div#frame_TerminalBusinessViewpointPortlet div#BVP_ReceiptHeader"));
+            }
+        }
+
+        public static string Status
+        {
+            get
+            {
+                return
+                    WebDriver.FindElement(
+                        By.CssSelector("div#frame_TerminalBusinessViewpointPortlet span#BVP_State")).Text;
+            }
+        }
+
+        public static string TerminalType
+        {
+            get
+            {
+                return
+                    WebDriver.FindElement(
+                        By.CssSelector(
+                            "div#frame_TerminalBusinessViewpointPortlet span#BVP_Appearance")).Text;
+            }
+        }
+
+        public static string Color
+        {
+            get
+            {
+                return
+                    WebDriver.FindElement(
+                        By.CssSelector(
+                            "div#frame_TerminalBusinessViewpointPortlet div#BVP_TerminalColor div.treeGridValueCol span")).Text;
+            }
+        }
+
+        public static string TerminalLanguage
+        {
+            get
+            {
+                return
+                    WebDriver.FindElement(
+                        By.CssSelector(
+                            "div#frame_TerminalBusinessViewpointPortlet div#BVP_TerminalAppearance_LanguageProperty div.treeGridValueCol span"))
+                             .Text;
+            }
+        }
+    }
+}
