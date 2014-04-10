58c76a4bb43f4d879bae8b0ec34b22a0eb52da0a
Marc Siegmund <TKCDEV\siegmund>
2014-04-10 12:42:42 +0000

    added entity/data class as transferring object for test data
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49480

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/CustomerData.cs
@@ -0,0 +1,35 @@
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    public class CustomerData : ILocation, IEntity
+    {
+        public string CustomerName { get; set; }
+        public string Supplier { get; set; }
+        public string CustomerNumber { get; set; }
+        public string SbsCurrency { get; set; }
+        public string SbsBillingTenant { get; set; }
+        public string CashIntegrator { get; set; }
+        public string CategoryCode { get; set; }
+        public string Agency { get; set; }
+        public string SupportContract { get; set; }
+        public string Segment { get; set; }
+        public string CompanyName { get; set; }
+        public string Guid { get; set; }
+        public string SbsDebitNumber { get; set; }
+        public string SbsAdressNumber { get; set; }
+        public string Ep2MerchantId { get; set; }
+        public string SapNumber { get; set; }
+        public string Language { get; set; }
+        public string Telephone { get; set; }
+        public string Mobile { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Web { get; set; }
+        public string StreetNo { get; set; }
+        public string Country { get; set; }
+        public string Zip { get; set; }
+        public string City { get; set; }
+        public string AdressAddition { get; set; }
+        public string Po { get; set; }
+        public string Region { get; set; }
+    }
+}
