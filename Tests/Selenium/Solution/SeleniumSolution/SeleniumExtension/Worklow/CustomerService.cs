4bfee8e59b4bbb2c3e264fe91b1b8df2c72b3720
Marc Siegmund <TKCDEV\siegmund>
2014-04-11 14:58:43 +0000

    refactored customer service
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49537

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/CustomerService.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/CustomerService.cs
@@ -18,36 +18,36 @@ namespace SIX.SCS.QA.Selenium.Extension.Worklow
             CustomerCreate.SbsBillingTenant = c.SbsBillingTenant;
             CustomerCreate.CustomerName = c.CustomerName;
 
-            CustomerCreate.StreetNo = c.StreetNo;
-            CustomerCreate.Zip = c.Zip;
-            CustomerCreate.City = c.City;
-            CustomerCreate.Po = c.Po;
-            CustomerCreate.AdressAddition = c.AdressAddition;
-            CustomerCreate.Region = c.Region;
-            CustomerCreate.SapNumber = c.SapNumber;
-            CustomerCreate.Ep2MerchantId = c.Ep2MerchantId; // may be dont set anything?
+            CustomerCreate.StreetNo = c.Location.Adress.StreetNo;
+            CustomerCreate.Zip = c.Location.Adress.Zip;
+            CustomerCreate.City = c.Location.Adress.City;
+            CustomerCreate.Po = c.Location.Adress.Po;
+            CustomerCreate.AdressAddition = c.Location.Adress.AdressAddition;
+            CustomerCreate.Region = c.Location.Adress.Region;
+            CustomerCreate.SapNumber = c.Location.SapNumber;
+            CustomerCreate.Ep2MerchantId = c.Location.Ep2MerchantId; // may be don't set anything?
             CustomerCreate.Agency = c.Agency;
-            CustomerCreate.Language = c.Language;
-            CustomerCreate.Country = c.Country;
-            CustomerCreate.Email = c.Email;
-            CustomerCreate.Telephone = c.Telephone;
-            CustomerCreate.Mobile = c.Mobile;
-            CustomerCreate.Fax = c.Fax;
-            CustomerCreate.Web = c.Web;
+            CustomerCreate.Language = c.Location.Contact.Language;
+            CustomerCreate.Country = c.Location.Adress.Country;
+            CustomerCreate.Email = c.Location.Contact.Email;
+            CustomerCreate.Telephone = c.Location.Contact.Telephone;
+            CustomerCreate.Mobile = c.Location.Contact.Mobile;
+            CustomerCreate.Fax = c.Location.Contact.Fax;
+            CustomerCreate.Web = c.Location.Contact.Web;
             CustomerCreate.CategoryCode = c.CategoryCode;
 
             CustomerCreate.SaveButton.Click();
 
             c.CustomerNumber = CustomerView.CustomerNumber;
-            c.SbsDebitNumber = CustomerView.SbsDebitNumber;
-            c.SbsAdressNumber = CustomerView.SbsAdressNumber;
+            c.Location.SbsDebitNumber = CustomerView.SbsDebitNumber;
+            c.Location.SbsAdressNumber = CustomerView.SbsAdressNumber;
         }
 
         public static void Edit(CustomerData c)
         {
             c.CustomerNumber = CustomerView.CustomerNumber;
-            c.SbsDebitNumber = CustomerView.SbsDebitNumber;
-            c.SbsAdressNumber = CustomerView.SbsAdressNumber;
+            c.Location.SbsDebitNumber = CustomerView.SbsDebitNumber;
+            c.Location.SbsAdressNumber = CustomerView.SbsAdressNumber;
             c.SbsCurrency = CustomerView.SbsCurrency;
             c.SbsBillingTenant = CustomerView.SbsBillingTenant;
             c.Supplier = CustomerView.Supplier;
@@ -55,23 +55,22 @@ namespace SIX.SCS.QA.Selenium.Extension.Worklow
             CustomerMenu.CustomerEdit.Click();
 
             CustomerEdit.CustomerName = c.CustomerName;
-            // CustomerEdit.CompanyName = c.CompanyName;
-            CustomerEdit.StreetNo = c.StreetNo;
-            CustomerEdit.Zip = c.Zip;
-            CustomerEdit.City = c.City;
-            CustomerEdit.Po = c.Po;
-            CustomerEdit.AdressAddition = c.AdressAddition;
-            CustomerEdit.Region = c.Region;
-            CustomerEdit.SapNumber = c.SapNumber;
+            CustomerEdit.StreetNo = c.Location.Adress.StreetNo;
+            CustomerEdit.Zip = c.Location.Adress.Zip;
+            CustomerEdit.City = c.Location.Adress.City;
+            CustomerEdit.Po = c.Location.Adress.Po;
+            CustomerEdit.AdressAddition = c.Location.Adress.AdressAddition;
+            CustomerEdit.Region = c.Location.Adress.Region;
+            CustomerEdit.SapNumber = c.Location.SapNumber;
 
             CustomerEdit.Agency = c.Agency;
-            CustomerEdit.Language = c.Language;
-            CustomerEdit.Country = c.Country;
-            CustomerEdit.Email = c.Email;
-            CustomerEdit.Telephone = c.Telephone;
-            CustomerEdit.Mobile = c.Mobile;
-            CustomerEdit.Fax = c.Fax;
-            CustomerEdit.Web = c.Web;
+            CustomerEdit.Language = c.Location.Contact.Language;
+            CustomerEdit.Country = c.Location.Adress.Country;
+            CustomerEdit.Email = c.Location.Contact.Email;
+            CustomerEdit.Telephone = c.Location.Contact.Telephone;
+            CustomerEdit.Mobile = c.Location.Contact.Mobile;
+            CustomerEdit.Fax = c.Location.Contact.Fax;
+            CustomerEdit.Web = c.Location.Contact.Web;
 
             ChangeForm.Reason = "MerchantAddressChange";
             ChangeForm.Remark = "SYR " + Factory.GenerateTestId() + " customer change remark";
@@ -86,10 +85,10 @@ namespace SIX.SCS.QA.Selenium.Extension.Worklow
             Assert.AreEqual(c.Supplier, CustomerView.Supplier);
             Assert.AreEqual(c.SbsBillingTenant, CustomerView.SbsBillingTenant);
             Assert.AreEqual(c.SbsCurrency, CustomerView.SbsCurrency);
-            Assert.AreEqual(c.SbsDebitNumber, CustomerView.SbsDebitNumber);
+            Assert.AreEqual(c.Location.SbsDebitNumber, CustomerView.SbsDebitNumber);
             // Todo: not reliable check: 
             // Assert.AreEqual(c.SbsAdressNumber, CustomerView.SbsAdressNumber);
-            Assert.AreEqual(c.SapNumber, CustomerView.SapNumber);
+            Assert.AreEqual(c.Location.SapNumber, CustomerView.SapNumber);
             Assert.AreEqual(c.Supplier, CustomerView.Supplier);
             Assert.AreEqual(c.SbsBillingTenant, CustomerView.SbsBillingTenant);
             StringAssert.Contains(CustomerView.SbsCurrency, c.SbsCurrency);
@@ -98,20 +97,20 @@ namespace SIX.SCS.QA.Selenium.Extension.Worklow
             StringAssert.Matches(CustomerView.SbsAdressNumber, TestRegExpPatterns.SbsAdressNoOpt);
             StringAssert.Matches(CustomerView.Ep2MerchantId, TestRegExpPatterns.Ep2MerchantId);
 
-            Assert.AreEqual(c.Po, CustomerView.Po);
-            Assert.AreEqual(c.AdressAddition, CustomerView.AdressAddition);
-            Assert.AreEqual(c.Region, CustomerView.Region);
-            Assert.AreEqual(c.StreetNo, CustomerView.StreetNo);
-            Assert.AreEqual(c.Zip, CustomerView.Zip);
-            Assert.AreEqual(c.City, CustomerView.City);
+            Assert.AreEqual(c.Location.Adress.Po, CustomerView.Po);
+            Assert.AreEqual(c.Location.Adress.AdressAddition, CustomerView.AdressAddition);
+            Assert.AreEqual(c.Location.Adress.Region, CustomerView.Region);
+            Assert.AreEqual(c.Location.Adress.StreetNo, CustomerView.StreetNo);
+            Assert.AreEqual(c.Location.Adress.Zip, CustomerView.Zip);
+            Assert.AreEqual(c.Location.Adress.City, CustomerView.City);
             StringAssert.Contains(CustomerView.Agency, c.Agency);
-            Assert.AreEqual(c.Language, CustomerView.Language);
-            Assert.AreEqual(c.Country, CustomerView.Country);
-            Assert.AreEqual(c.Email, CustomerView.Email);
-            Assert.AreEqual(c.Telephone, CustomerView.Telephone);
-            Assert.AreEqual(c.Mobile, CustomerView.Mobile);
-            Assert.AreEqual(c.Fax, CustomerView.Fax);
-            Assert.AreEqual(c.Web, CustomerView.Web);
+            Assert.AreEqual(c.Location.Contact.Language, CustomerView.Language);
+            Assert.AreEqual(c.Location.Adress.Country, CustomerView.Country);
+            Assert.AreEqual(c.Location.Contact.Email, CustomerView.Email);
+            Assert.AreEqual(c.Location.Contact.Telephone, CustomerView.Telephone);
+            Assert.AreEqual(c.Location.Contact.Mobile, CustomerView.Mobile);
+            Assert.AreEqual(c.Location.Contact.Fax, CustomerView.Fax);
+            Assert.AreEqual(c.Location.Contact.Web, CustomerView.Web);
         }
     }
 }
