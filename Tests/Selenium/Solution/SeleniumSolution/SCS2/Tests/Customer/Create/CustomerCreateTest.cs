459d2858769437421878b8832908db63bd760303
Marc Siegmund <TKCDEV\siegmund>
2014-04-11 15:02:17 +0000

    removed check method (part of service now) and refactored
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49540

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Customer/Create/CustomerCreateTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Customer/Create/CustomerCreateTest.cs
@@ -16,7 +16,7 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Customer.Create
         public static void ClassInit(TestContext testContext)
         {
             TestDirector.Navigate();
-            _c = Factory.Customer.SixCustomerNew();
+            _c = Factory.Customer.Create();
             CustomerService.Create(_c);
         }
 
@@ -110,42 +110,42 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Customer.Create
         [TestCategory("Customer"), TestCategory("Create")]
         public void Po()
         {
-            Assert.AreEqual(_c.Po, CustomerView.Po);
+            Assert.AreEqual(_c.Location.Adress.Po, CustomerView.Po);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void AdressAddition()
         {
-            Assert.AreEqual(_c.AdressAddition, CustomerView.AdressAddition);
+            Assert.AreEqual(_c.Location.Adress.AdressAddition, CustomerView.AdressAddition);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Region()
         {
-            Assert.AreEqual(_c.Region, CustomerView.Region);
+            Assert.AreEqual(_c.Location.Adress.Region, CustomerView.Region);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void StreetNo()
         {
-            Assert.AreEqual(_c.StreetNo, CustomerView.StreetNo);
+            Assert.AreEqual(_c.Location.Adress.StreetNo, CustomerView.StreetNo);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Zip()
         {
-            Assert.AreEqual(_c.Zip, CustomerView.Zip);
+            Assert.AreEqual(_c.Location.Adress.Zip, CustomerView.Zip);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void City()
         {
-            Assert.AreEqual(_c.City, CustomerView.City);
+            Assert.AreEqual(_c.Location.Adress.City, CustomerView.City);
         }
 
         [TestMethod]
@@ -160,7 +160,7 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Customer.Create
         [Ignore]
         public void Language()
         {
-            Assert.AreEqual(_c.Language, CustomerView.Language);
+            Assert.AreEqual(_c.Location.Contact.Language, CustomerView.Language);
         }
 
         [Ignore]
@@ -168,78 +168,48 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Customer.Create
         [TestCategory("Customer"), TestCategory("Create")]
         public void Country()
         {
-            Assert.AreEqual(_c.Country, CustomerView.Country);
+            Assert.AreEqual(_c.Location.Adress.Country, CustomerView.Country);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Email()
         {
-            Assert.AreEqual(_c.Email, CustomerView.Email);
+            Assert.AreEqual(_c.Location.Contact.Email, CustomerView.Email);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Telephone()
         {
-            Assert.AreEqual(_c.Telephone, CustomerView.Telephone);
+            Assert.AreEqual(_c.Location.Contact.Telephone, CustomerView.Telephone);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Mobile()
         {
-            Assert.AreEqual(_c.Mobile, CustomerView.Mobile);
+            Assert.AreEqual(_c.Location.Contact.Mobile, CustomerView.Mobile);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Fax()
         {
-            Assert.AreEqual(_c.Fax, CustomerView.Fax);
+            Assert.AreEqual(_c.Location.Contact.Fax, CustomerView.Fax);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Web()
         {
-            Assert.AreEqual(_c.Web, CustomerView.Web);
+            Assert.AreEqual(_c.Location.Contact.Web, CustomerView.Web);
         }
 
         [TestMethod]
         [TestCategory("Customer"), TestCategory("Create")]
         public void Segment()
         {
-            Assert.AreEqual(_c.Segment, CustomerView.Segment);
-        }
-
-        public static void Check()
-        {
-            StringAssert.Matches(CustomerView.CustomerNumber, TestRegExpPatterns.CustomerNo);
-            Assert.AreEqual(_c.CustomerName, CustomerView.CustomerName);
-            Assert.AreEqual(_c.Supplier, CustomerView.Supplier);
-            Assert.AreEqual(_c.SbsBillingTenant, CustomerView.SbsBillingTenant);
-            StringAssert.Contains(CustomerView.SbsCurrency.ToUpper(), _c.SbsCurrency);
-            Assert.AreEqual(_c.SapNumber, CustomerView.SapNumber);
-            Assert.AreEqual(_c.SupportContract, CustomerView.SupportContract);
-            Assert.AreEqual(_c.CashIntegrator, CustomerView.CashIntegrator);
-            StringAssert.Matches(CustomerView.SbsDebitNumber, TestRegExpPatterns.SbsDebitorNo);
-            StringAssert.Matches(CustomerView.SbsAdressNumber, TestRegExpPatterns.SbsAdressNoOpt);
-            StringAssert.Matches(CustomerView.Ep2MerchantId, TestRegExpPatterns.Ep2MerchantId);
-            Assert.AreEqual(_c.Po, CustomerView.Po);
-            Assert.AreEqual(_c.AdressAddition, CustomerView.AdressAddition);
-            Assert.AreEqual(_c.Region, CustomerView.Region);
-            Assert.AreEqual(_c.StreetNo, CustomerView.StreetNo);
-            Assert.AreEqual(_c.Zip, CustomerView.Zip);
-            Assert.AreEqual(_c.City, CustomerView.City);
-            Assert.AreEqual(_c.Agency, CustomerView.Agency);
-            Assert.AreEqual(_c.Language, CustomerView.Language);
-            Assert.AreEqual(_c.Country, CustomerView.Country);
-            Assert.AreEqual(_c.Email, CustomerView.Email);
-            Assert.AreEqual(_c.Telephone, CustomerView.Telephone);
-            Assert.AreEqual(_c.Mobile, CustomerView.Mobile);
-            Assert.AreEqual(_c.Fax, CustomerView.Fax);
-            Assert.AreEqual(_c.Web, CustomerView.Web);
             Assert.AreEqual(_c.Segment, CustomerView.Segment);
         }
     }
