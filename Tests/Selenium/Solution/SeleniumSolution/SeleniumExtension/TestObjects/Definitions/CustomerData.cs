452a578c740d8977c297a72f112e69473db0f7a1
Marc Siegmund <TKCDEV\siegmund>
2014-04-15 14:10:46 +0000

    converted to aggregation instead of inheritance (more friendly) and removed fields
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49610

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/CustomerData.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/CustomerData.cs
@@ -1,7 +1,8 @@
 namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
 {
-    public class CustomerData : ILocation, IEntity
+    public class CustomerData : IEntity
     {
+        public LocationData Location { get; set; }
         public string CustomerName { get; set; }
         public string Supplier { get; set; }
         public string CustomerNumber { get; set; }
@@ -11,25 +12,6 @@ namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
         public string CategoryCode { get; set; }
         public string Agency { get; set; }
         public string SupportContract { get; set; }
-        public string Segment { get; set; }
-        public string CompanyName { get; set; }
-        public string Guid { get; set; }
-        public string SbsDebitNumber { get; set; }
-        public string SbsAdressNumber { get; set; }
-        public string Ep2MerchantId { get; set; }
-        public string SapNumber { get; set; }
-        public string Language { get; set; }
-        public string Telephone { get; set; }
-        public string Mobile { get; set; }
-        public string Fax { get; set; }
-        public string Email { get; set; }
-        public string Web { get; set; }
-        public string StreetNo { get; set; }
-        public string Country { get; set; }
-        public string Zip { get; set; }
-        public string City { get; set; }
-        public string AdressAddition { get; set; }
-        public string Po { get; set; }
-        public string Region { get; set; }
+        public string Segment { get; set; }        
     }
 }
