d84f22284e4d766affbf5aecfc98e512b6e293ab
Marc Siegmund <TKCDEV\siegmund>
2014-04-15 18:26:08 +0000

    used aggregation instead of inheritance made member pucblic
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49626

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/AgencyData.cs
@@ -0,0 +1,10 @@
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    public class AgencyData
+    {
+        public AdressData Adress { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+        public string Supplier { get; set; }
+    }
+}
--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/IAgency.cs
+++
@@ -1,9 +0,0 @@
-namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
-{
-    internal interface IAgency : IAdress
-    {
-        string Name { get; set; }
-        string DisplayName { get; set; }
-        string Supplier { get; set; }
-    }
-}
