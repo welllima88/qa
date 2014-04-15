ed674b3dc8adc39f6efe9a028eea15c1d63cd511
Marc Siegmund <TKCDEV\siegmund>
2014-04-15 18:24:40 +0000

    created class for contact person data objects
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49625

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/ContactPersonData.cs
@@ -0,0 +1,14 @@
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    /// <summary>
+    ///     the contact person has adress and contact fields
+    /// </summary>
+    public class ContactPersonData
+    {
+        public AdressData Adress { get; set; }
+        public ContactData Contact { get; set; }
+        public string FirstName { set; get; }
+        public string Name { set; get; }
+        public string Salutation { set; get; }
+    }
+}
