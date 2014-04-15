16a040f67f6688a2350ba5fdf2b4f3aa6ab6eaaa
Marc Siegmund <TKCDEV\siegmund>
2014-04-15 18:22:09 +0000

    converted interface to class with pubs
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49621

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/LocationData.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    public class LocationData
+    {
+        public ContactData Contact { get; set; }
+        public AdressData Adress { get; set; }
+        public string CompanyName { get; set; }
+        public string Guid { get; set; }
+        public String SbsDebitNumber { get; set; }
+        public String SbsAdressNumber { get; set; }
+        public string Ep2MerchantId { get; set; }
+        public string SapNumber { get; set; }
+    }
+}
