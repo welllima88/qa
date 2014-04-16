ca144e741800ef742b477502ba4c52ecc2483a9a
Marc Siegmund <TKCDEV\siegmund>
2014-04-16 17:48:37 +0000

    added service for info texts
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49668

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/InfoTextService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.TestData;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Infotext;
+
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public class InfoTextService
+    {
+        /// <summary>
+        /// This implies, that the create infotext button is displayed
+        /// </summary>
+        /// <param name="infotext">Substring to create an Infotext</param>
+        public static void Create(string infotext)
+        {
+            infotext =
+                InfoTextCreate.Text = String.Format("SYR {0} Infotext {1}", infotext, Factory.GenerateTestId());
+            InfoTextCreate.SaveButton.Click();
+            Assert.IsTrue(InfoTextListView.List.Contains(infotext));
+        }
+    }
+}
