df681ab18a2ab643f79477f6997622262116b348
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 09:11:58 +0000

    added contact factory which is used by contact-persons, customers, locations
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49688

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/ContactFactory.cs
@@ -0,0 +1,46 @@
+﻿using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestData
+{
+    public class ContactFactory
+    {
+        public ContactData Create()
+        {
+            return new ContactData
+            {
+                Language = "Deutsch [de]",
+                Email = "marc.siegmund@six-group.de",
+                Telephone = "0031 58 399 6237",
+                Mobile = "0032 58 399 6237",
+                Fax = "0033 58 399 6237",
+                Web = "www.six-group.com/de-intern",
+            };
+        }
+
+        public ContactData Edit()
+        {
+            return new ContactData
+            {
+                Language = "Englisch [en]",
+                Email = "marc.siegmund@six-group.uk",
+                Telephone = "+31 - 58 399 6237",
+                Mobile = "+32 - 58 399 6237",
+                Fax = "+33 - 58 399 6237",
+                Web = "www.six-group.com/en-intern"
+            };
+        }
+
+        public ContactData Invalid()
+        {
+            return new ContactData
+            {
+                Language = "Deutsch [de]",
+                Email = "marc.siegmund@six-grou$°p.com",
+                Telephone = "0031 58 399 6237$",
+                Mobile = "0032 58 399 6237$",
+                Fax = "0033 58 399 6237$",
+                Web = "www.six-^°@}$.com/de-inte[]rn$°"
+            };
+        }
+    }
+}
