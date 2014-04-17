6c866c8eb9d2c229de59582152627d07d892bda8
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 14:22:53 +0000

    added test object for creating a user
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49725

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/User/UserCreate.cs
@@ -0,0 +1,53 @@
+using OpenQA.Selenium;
+using SIX.SCS.QA.Selenium.Extension.Selenium.WebElements;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.User
+{
+    public class UserCreate : WebObject
+    {
+        public static string UserName
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_Username")).TextBox().TypeText(value); }
+        }
+
+        public static string Salutation
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_Salutation")).TextBox().TypeText(value); }
+        }
+
+        public static string FirstName
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_FirstName")).TextBox().TypeText(value); }
+        }
+
+        public static string Name
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_LastName")).TextBox().TypeText(value); }
+        }
+
+        public static string Email
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_Email")).TextBox().TypeText(value); }
+        }
+
+        public static string Language
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_Language")).TextBox().TypeText(value); }
+        }
+
+        public static string SecurId
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_SecureId")).TextBox().TypeText(value); }
+        }
+
+        public static string WesMandant
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_WesMandant")).TextBox().TypeText(value); }
+        }
+
+        public static string Comment
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("User_Comment")).TextBox().TypeText(value); }
+        }
+    }
+}
