9d9ba80f037502370f8ca658815a1d4ff8035ae2
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 12:09:04 +0000

    added location factory
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49703

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/LocationFactory.cs
@@ -0,0 +1,42 @@
+﻿using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestData
+{
+    public class LocationFactory
+    {
+        public LocationData Create()
+        {
+            return new LocationData
+            {
+                Adress = Factory.Adress.Create(),
+                Contact = Factory.Contact.Create(),
+                CompanyName = "SYR Location Create" + Factory.GenerateTestId(),
+                SapNumber = "4444",
+                Ep2MerchantId = "",
+            };
+        }
+
+        public LocationData Edit()
+        {
+            return new LocationData
+            {
+                Adress = Factory.Adress.Edit(),
+                Contact = Factory.Contact.Edit(),
+                CompanyName = "SYR Location Edit" + Factory.GenerateTestId(),
+                SapNumber = "1234",
+            };
+        }
+
+        public LocationData Invalid()
+        {
+            return new LocationData
+            {
+                Adress = Factory.Adress.Invalid(),
+                Contact = Factory.Contact.Invalid(),
+                SapNumber = "444$°",
+                CompanyName = "444$°?",
+                Ep2MerchantId = "12D%FF_3-3",
+            };
+        }
+    }
+}
