afbf7c66e29f206235ca97ee7d0d1eb25b5f0394
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 08:51:36 +0000

    added adress factory which is used for location/customers and contacts
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49687

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/AdressFactory.cs
@@ -0,0 +1,49 @@
+﻿using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestData
+{
+    public class AdressFactory
+    {
+        public AdressData Create()
+        {
+            return new AdressData
+            {
+                StreetNo = "Hardturmstr. 201",
+                Zip = "8021",
+                City = "Zürich",
+                Po = "Po-O1",
+                AdressAddition = "SYR",
+                Region = "Reg 55",
+                Country = "Schweiz [CH]",
+            };
+        }
+
+        public AdressData Edit()
+        {
+            return new AdressData
+            {
+                StreetNo = "Theater Platz 1a-3c",
+                AdressAddition = "SYR/Stage 3",
+                Po = "PFO1-44",
+                Zip = "01067",
+                City = "Dresden",
+                Region = "Mitte",
+                Country = "Deutschland [DE]",
+            };
+        }
+
+        public AdressData Invalid()
+        {
+            return new AdressData
+            {
+                StreetNo = "Hardturmstr. 201$°",
+                Zip = "802$°",
+                City = "Zürich$°",
+                Po = "PFO1$°",
+                AdressAddition = "Etage 3$°",
+                Region = "Reg 3[]$°",
+                Country = "Schweiz [CH]",
+            };
+        }
+    }
+}
