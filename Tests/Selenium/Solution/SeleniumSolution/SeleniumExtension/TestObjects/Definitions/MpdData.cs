fa69f3e1baea003194657f67e7a430422b81b0f6
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 08:50:42 +0000

    added mpd data object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49686

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/MpdData.cs
@@ -0,0 +1,9 @@
+﻿namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    public class MpdData
+    {
+        public string Adress { get; set; }
+        public string Port { get; set; }
+        public string Description { get; set; }
+    }
+}
