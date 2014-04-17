b632e863237fcc6d37f2366e91e9fef939a7dbec
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 08:50:29 +0000

    added service for managing mpds
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49685

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/MpdService.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd;
+
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public class MpdService
+    {
+        public static void Create(MpdData m)
+        {
+            CustomerMenu.AllMpds.Click();
+            MpdListView.CreateButton.Click();
+
+            MpdCreate.Description = m.Description;
+            MpdCreate.Adress = m.Adress;
+            MpdCreate.Port = m.Port;
+
+            MpdCreate.SaveButton.Click();
+        }
+
+        public static void Check(MpdData m)
+        {
+            Assert.AreEqual(m.Description, MpdView.Description);
+            Assert.AreEqual(m.Adress, MpdView.Adress);
+            Assert.AreEqual(m.Port, MpdView.Port);
+        }
+    }
+}
