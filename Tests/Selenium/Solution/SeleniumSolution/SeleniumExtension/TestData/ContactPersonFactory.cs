b781883080669f97b1c90f26a6b6bc711a297caf
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 09:28:02 +0000

    added person factory
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49689

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/ContactPersonFactory.cs
@@ -0,0 +1,31 @@
+﻿using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+
+namespace SIX.SCS.QA.Selenium.Extension.TestData
+{
+    public class ContactPersonFactory
+    {
+        public ContactPersonData Create()
+        {
+            return new ContactPersonData
+            {
+                FirstName = "SYR Contact" + Factory.GenerateTestId(),
+                Name = "SYR - Create",
+                Salutation = "Herr",
+                Adress = Factory.Adress.Create(),
+                Contact = Factory.Contact.Create(),
+            };
+        }
+
+        public ContactPersonData Edit()
+        {
+            return new ContactPersonData
+            {
+                FirstName = "SYR Contact" + Factory.GenerateTestId(),
+                Name = "SYR - Edit",
+                Salutation = "Frau",
+                Adress = Factory.Adress.Edit(),
+                Contact = Factory.Contact.Edit(),
+            };
+        }
+    }
+}
