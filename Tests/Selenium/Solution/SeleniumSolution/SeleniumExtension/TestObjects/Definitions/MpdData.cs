bf981731e07653490c4e176c56a695302ba3ba64
Marc Siegmund <TKCDEV\siegmund>
2014-04-17 09:42:16 +0000

    added id to mpd data object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49691

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/MpdData.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/MpdData.cs
@@ -5,5 +5,6 @@
         public string Adress { get; set; }
         public string Port { get; set; }
         public string Description { get; set; }
+        public string Id { get; set; }
     }
 }
