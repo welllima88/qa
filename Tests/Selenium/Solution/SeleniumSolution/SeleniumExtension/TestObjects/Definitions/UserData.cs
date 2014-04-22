98f63b052bec40062f74be7ed178f448153c94b0
Marc Siegmund <TKCDEV\siegmund>
2014-04-22 15:40:38 +0000

    added password property for future use e.g. login as different user (for testing it is less appropiate)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49779

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Definitions/UserData.cs
@@ -0,0 +1,16 @@
+namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions
+{
+    public class UserData
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Salutation { get; set; }
+        public string Comment { get; set; }
+        public string SecurId { get; set; }
+        public string FirstName { get; set; }
+        public string Name { get; set; }
+        public string Language { get; set; }
+        public string WesMandant { get; set; }
+        public string Password { get; set; }
+    }
+}
