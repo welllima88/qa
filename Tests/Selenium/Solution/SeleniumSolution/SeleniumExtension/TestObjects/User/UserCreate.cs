2bee76cfab2255bec731eacac3812c2e7d2ceafd
Marc Siegmund <TKCDEV\siegmund>
2014-04-22 10:00:38 +0000

    updated html ids
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49753

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/User/UserCreate.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/User/UserCreate.cs
@@ -7,47 +7,47 @@ namespace SIX.SCS.QA.Selenium.Extension.TestObjects.User
     {
         public static string UserName
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_Username")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_Username")).TextBox().TypeText(value); }
         }
 
         public static string Salutation
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_Salutation")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_SalutationId")).Selector().SelectByText(value); }
         }
 
         public static string FirstName
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_FirstName")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_Firstname")).TextBox().TypeText(value); }
         }
 
         public static string Name
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_LastName")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_Name")).TextBox().TypeText(value); }
         }
 
         public static string Email
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_Email")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("email")).TextBox().TypeText(value); }
         }
 
         public static string Language
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_Language")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_LanguageId")).Selector().SelectByText(value); }
         }
 
         public static string SecurId
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_SecureId")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_SecureId")).TextBox().TypeText(value); }
         }
 
         public static string WesMandant
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_WesMandant")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("UserData_WESMandantId")).Selector().SelectByText(value); }
         }
 
         public static string Comment
         {
-            set { WebDriver.FindAdaptedElement(By.Id("User_Comment")).TextBox().TypeText(value); }
+            set { WebDriver.FindAdaptedElement(By.Id("description")).TextBox().TypeText(value); }
         }
         public static IWebElement SaveButton
         {
