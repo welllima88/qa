2d3156b06d7f8ec5c83026f261978e4caea615fb
Marc Siegmund <TKCDEV\siegmund>
2014-04-22 08:21:48 +0000

    added user service for creating a user
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49741

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/UserService.cs
@@ -0,0 +1,44 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
+using SIX.SCS.QA.Selenium.Extension.TestObjects.User;
+
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public class UserService
+    {
+        public static void Create(UserData u)
+        {
+            CustomerMenu.Users.Click();
+            UserList.UserCreate.Click();
+
+            UserCreate.UserName = u.UserName;
+            UserCreate.Salutation = u.Salutation;
+            UserCreate.FirstName = u.FirstName;
+            UserCreate.Name = u.Name;
+            UserCreate.Language = u.Language;
+            UserCreate.Email = u.Email;
+            UserCreate.SecurId = u.SecurId;
+            UserCreate.Comment = u.Comment;
+            UserCreate.WesMandant = u.WesMandant;
+
+            UserCreate.SaveButton.Click();
+        }
+
+        public static void Check(UserData u)
+        {
+            Assert.AreEqual(u.UserName, UserView.UserName);
+            StringAssert.Matches(UserView.Password, TestRegExpPatterns.UserPassword);
+            Assert.AreEqual(u.Salutation, UserView.Salutation);
+            Assert.AreEqual(u.FirstName, UserView.FirstName);
+            Assert.AreEqual(u.Name, UserView.Name);
+            Assert.AreEqual(u.Language, UserView.Language);
+            Assert.AreEqual(u.Email, UserView.Email);
+            Assert.AreEqual(u.SecurId, UserView.SecurId);
+            Assert.AreEqual(u.Comment, UserView.Comment);
+            Assert.AreEqual(u.WesMandant, UserView.WesMandant);
+
+            UserCreate.SaveButton.Click();
+        }
+    }
+}
