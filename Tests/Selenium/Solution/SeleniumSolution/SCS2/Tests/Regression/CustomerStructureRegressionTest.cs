ca897e8538a600546b8587f2460acc0cb3f5e7c9
Marc Siegmund <TKCDEV\siegmund>
2014-04-25 09:23:36 +0000

    - extended tests for mpd, user-logins, additional checks - improved regression test by using factories, services, and checks even after opened the last elements
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49867

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Regression/CustomerStructureRegressionTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Regression/CustomerStructureRegressionTest.cs
@@ -1,10 +1,4 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Contact.Create;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Contact.Edit;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Infotext;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Create;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Location.Edit;
-using SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Mpd;
 using SIX.SCS.QA.Selenium.Extension.TestData;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Brand;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
@@ -13,7 +7,6 @@ using SIX.SCS.QA.Selenium.Extension.TestObjects.Common.Menu;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Customer;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Location;
-using SIX.SCS.QA.Selenium.Extension.TestObjects.Mpd;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Person;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Dashboard;
@@ -25,13 +18,14 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
     [TestClass]
     public class CustomerStructureRegressionTest
     {
-        private static string _locationGuid;
-        private static string _locationName;
-        private static string _contactCustomerName;
-        private static string _contactLocationName;
         private static string _terminalIdLocation;
         private static string _terminalIdCustomer;
-        private static CustomerData _c;
+        private static CustomerData _customer;
+        private static LocationData _location;
+        private static ContactPersonData _personOnCustomer;
+        private static ContactPersonData _personOnLocation;
+        private static MpdData _mpd;
+        private static UserData _user;
 
         /* [ClassCleanup]
         public static void CleanupCustomerStructure()
@@ -69,23 +63,33 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
 
         private static void EditLocation()
         {
-            LocationEditAndSaveTest.Edit();
-            _locationName = LocationView.CompanyName;
-            LocationEditAndSaveTest.Check();
+            _location = Factory.Location.Edit();
+            LocationService.Edit(_location);
+            LocationService.Check(_location);
+
+            OpenLatestElement();
+            LocationService.Check(_location);
         }
 
         private static void EditCustomer()
         {
-            CustomerData c = Factory.Customer.SixCustomerEdit();
-            CustomerService.Edit(c);
-            CustomerService.Check(c);
+            _customer = Factory.Customer.Edit();
+            CustomerService.Edit(_customer);
+            CustomerService.Check(_customer);
+
+            OpenLatestElement();
+            CustomerService.Check(_customer);
         }
 
         private static void CreateContactOnLocation()
         {
             LocationMenu.ContactCreate.Click();
-            ContactCreateToCustomerTest.Create();
-            _contactLocationName = ContactPersonView.FirstName;
+            _personOnLocation = Factory.ContactPerson.Create();
+            ContactService.Create(_personOnLocation);
+            ContactService.Check(_personOnLocation);
+
+            OpenLatestElement();
+            ContactService.Check(_personOnLocation);
         }
 
         private static void CreateTerminalOnLocation()
@@ -103,18 +107,24 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
             TerminalMenu.Terminal.Click();
             _terminalIdLocation = TerminalInfo.TerminalId;
 
+            OpenLatestElement();
+            Assert.AreEqual(_terminalIdLocation, TerminalInfo.TerminalId);
             Assert.AreEqual("Aktiviert - Aktiviert", BusinessViewpointPortlet.Status);
             Assert.AreEqual("xentissimo MOBILE WLAN, TCP/IP", BusinessViewpointPortlet.TerminalType);
             Assert.AreEqual("grau", BusinessViewpointPortlet.Color);
-            Assert.AreEqual("Englisch [en]", BusinessViewpointPortlet.TerminalLanguage);
+            Assert.AreEqual(_location.Contact.Language, BusinessViewpointPortlet.TerminalLanguage);
+
+            Assert.AreEqual(_location.CompanyName, LocationInfo.CompanyName);
         }
 
         private static void CreateLocationOnCustomer()
         {
-            LocationCreateTest.Create();
-            _locationGuid = LocationView.Guid;
-            _locationName = LocationView.CompanyName;
-            LocationCreateTest.Check();
+            _location = Factory.Location.Create();
+            LocationService.Create(_location);
+            LocationService.Check(_location);
+
+            OpenLatestElement();
+            LocationService.Check(_location);
         }
 
         private static void CreateTerminalOnCustomer()
@@ -126,46 +136,55 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
             TerminalConfigCreate.Infotext = "SYR Terminal AUTO" + Factory.GenerateTestId();
             TerminalConfigCreate.SaveButton.Click();
 
+            Assert.IsTrue(BrandSelect.ConfirmButton().Displayed);
             TerminalMenu.Terminal.Click();
             _terminalIdCustomer = TerminalInfo.TerminalId;
 
+            OpenLatestElement();
+            Assert.AreEqual(_terminalIdCustomer, TerminalInfo.TerminalId);
             Assert.AreEqual("Aktiviert - Aktiviert", BusinessViewpointPortlet.Status);
             Assert.AreEqual("yomani AUTONOM, TCP/IP ep2 (DNS)", BusinessViewpointPortlet.TerminalType);
             Assert.AreEqual("weiss", BusinessViewpointPortlet.Color);
-            Assert.AreEqual("Deutsch [de]", BusinessViewpointPortlet.TerminalLanguage);
+
+            Assert.AreEqual(_customer.Location.Contact.Language, BusinessViewpointPortlet.TerminalLanguage);
+
+            Assert.AreEqual(_customer.CustomerNumber, CustomerInfo.Number);
+            Assert.AreEqual(_customer.CustomerName, CustomerInfo.Name);
         }
 
         private static void CreateContactOnCustomer()
         {
             CustomerMenu.ContactCreate.Click();
-            ContactCreateToCustomerTest.Create();
-            ContactCreateToCustomerTest.Check();
-            _contactCustomerName = ContactPersonView.FirstName;
+            _personOnCustomer = Factory.ContactPerson.Create();
+            ContactService.Create(_personOnCustomer);
+            ContactService.Check(_personOnCustomer);
+            OpenLatestElement();
+            ContactService.Check(_personOnCustomer);
         }
 
         [TestMethod]
         [TestCategory("RegressionA")]
         public void ContactToCustomerIsCreated()
         {
-            SearchService.CustomerCanBeFoundByCustomerId(_c.CustomerNumber);
+            SearchService.CustomerCanBeFoundByCustomerNumber(_customer.CustomerNumber);
 
             CustomerMenu.Contacts.Click();
 
             ContactList.First().Click();
 
-            Assert.AreEqual(_contactCustomerName, ContactPersonView.FirstName);
+            ContactService.Check(_personOnCustomer);
         }
 
         [TestMethod]
         [TestCategory("RegressionA")]
         public void ContactToLocationIsCreated()
         {
-            SearchService.LocationCanBeFoundByLocationName(_locationName);
+            SearchService.LocationCanBeFoundByLocationName(_location.CompanyName);
 
             LocationMenu.Contacts.Click();
 
             ContactList.First().Click();
-            Assert.AreEqual(_contactLocationName, ContactPersonView.FirstName);
+            ContactService.Check(_personOnLocation);
         }
 
         [TestMethod]
@@ -194,7 +213,7 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
         [TestCategory("RegressionA")]
         public void TerminalToCustomerIsCreated()
         {
-            SearchService.CustomerCanBeFoundByCustomerId(_c.CustomerNumber);
+            SearchService.CustomerCanBeFoundByCustomerNumber(_customer.CustomerNumber);
 
             CustomerMenu.AllTerminals.Click();
             TerminalList.First().Click();
@@ -207,77 +226,110 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
         [TestCategory("RegressionA")]
         public void CustomerIsCreated()
         {
-            SearchService.CustomerCanBeFoundByCustomerId(_c.CustomerNumber);
+            SearchService.CustomerCanBeFoundByCustomerName(_customer.CustomerName);
+            CustomerService.Check(_customer);
+        }
 
-            // CustomerCheck();
+        [TestMethod]
+        [TestCategory("RegressionA")]
+        public void UserIsCreated()
+        {
+            SearchService.UserCanBeFoundByCustomerName(_user.UserName);
+            UserService.Check(_user);
         }
 
         [TestMethod]
         [TestCategory("RegressionA")]
         public void LocationIsCreated()
         {
-            SearchService.LocationCanBeFoundByLocationName(_locationName);
+            SearchService.LocationCanBeFoundByLocationName(_location.CompanyName);
 
-            // LocationCheck();
+            LocationService.Check(_location);
         }
 
         [ClassInitialize]
         public static void ExecuteRegressiontest(TestContext testContext)
         {
-            _c = Factory.Customer.SixCustomerNew();
-            CustomerService.Create(_c);
-            OpenLatestElement();
-            CustomerService.Check(_c);
+            CreateCustomer();
             CreateContactOnCustomer();
-
-            SearchService.CustomerCanBeFoundByCustomerId(_c.CustomerNumber);
             CreateTerminalOnCustomer();
-            OpenLatestElement();
 
-            SearchService.CustomerCanBeFoundByCustomerName(_c.CustomerName);
-            Assert.AreEqual(_c.CustomerNumber, CustomerView.CustomerNumber);
+            OpenCustomer();
             EditCustomer();
-            OpenLatestElement();
+
             CreateInfotextOnCustomer();
-            // CreateMpdOnCustomer();
+            CreateMpdOnCustomer();
 
             CreateLocationOnCustomer();
-            OpenLatestElement();
 
             CreateTerminalOnLocation();
-            OpenLatestElement();
             CreateInfotextOnTerminal();
 
             // TODO:
-            // CreateBrandsOnTerminal();
+            CreateBrandsOnTerminal();
 
-            SearchService.LocationCanBeFoundByLocationName(_locationName);
-            Assert.AreEqual(_locationGuid, LocationView.Guid);
-
+            OpenLocation();
             EditLocation();
-            OpenLatestElement();
             CreateInfotextOnLocation();
 
             CreateContactOnLocation();
-            OpenLatestElement();
+
             EditContactOnLocation();
+
+            OpenCustomer();
+            CreateUser();
+        }
+
+        private static void OpenLocation()
+        {
+            SearchService.LocationCanBeFoundByLocationName(_location.CompanyName);
+            Assert.AreEqual(_location.Guid, LocationView.Guid);
+        }
+
+        private static void CreateUser()
+        {
+            _user = Factory.User.Create();
+            UserService.Create(_user);
+
+            UserService.Check(_user);
+            OpenLatestElement();
+            UserService.Check(_user);
+        }
+
+        private static void OpenCustomer()
+        {
+            SearchService.CustomerCanBeFoundByCustomerNumber(_customer.CustomerNumber);
+            Assert.AreEqual(_customer.CustomerNumber, CustomerView.CustomerNumber);
+        }
+
+        private static void CreateCustomer()
+        {
+            _customer = Factory.Customer.Create();
+            CustomerService.Create(_customer);
+            CustomerService.Check(_customer);
+            OpenLatestElement();
+            CustomerService.Check(_customer);
         }
 
         private static void CreateMpdOnCustomer()
         {
-            CustomerMenu.AllMpds.Click();
+            _mpd = Factory.Mpd.Create();
+            MpdService.Create(_mpd);
+            MpdService.Check(_mpd);
 
-            MpdListView.CreateButton.Click();
-            MpdCreateTest.Create();
-            MpdCreateTest.Check();
+            OpenLatestElement();
+            MpdService.Check(_mpd);
         }
 
         private static void EditContactOnLocation()
         {
             ContactMenu.ContactEdit.Click();
-            ContactEditTest.Edit();
-            ContactEditTest.Check();
-            _contactLocationName = ContactPersonView.FirstName;
+            _personOnLocation = Factory.ContactPerson.Edit();
+            ContactService.Edit(_personOnLocation);
+            ContactService.Check(_personOnLocation);
+
+            OpenLatestElement();
+            ContactService.Check(_personOnLocation);
         }
 
         private static void CreateInfotextOnLocation()
@@ -285,22 +337,21 @@ namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium.Tests.Regression
             LocationMenu.Infotexts.Click();
             InfoTextListView.CreateButton.Click();
 
-            InfotextCreateTest.Create("Location");
+            InfoTextService.Create("Location");
         }
 
         private static void CreateInfotextOnCustomer()
         {
-            // TODO: CustomerMenu.Infotexts.Click();
             CustomerMenu.Infotexts.Click();
             InfoTextListView.CreateButton.Click();
 
-            InfotextCreateTest.Create("Customer");
+            InfoTextService.Create("Customer");
         }
 
         private static void CreateInfotextOnTerminal()
         {
             TerminalTextsPortlet.CreateInfotext.Click();
-            InfotextCreateTest.Create("Terminal");
+            InfoTextService.Create("Terminal");
         }
 
         private static void OpenLatestElement()
