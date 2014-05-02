94137fd7ceec3357a51cccc859671b12d38b47f3
Marc Siegmund <TKCDEV\siegmund>
2014-05-02 12:39:18 +0000

    added languages as class
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C49968

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Languages.cs
@@ -0,0 +1,15 @@
+﻿namespace SIX.SCS.QA.Selenium.Extension.TestObjects.Common
+{
+    public class Languages
+    {
+        public static string German
+        {
+            get { return "Deutsch"; }
+        }
+
+        public static string English
+        {
+            get { return "English"; }
+        }
+    }
+}
