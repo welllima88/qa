7c3605325e87338a9c543143417bfdd16cbfcc06
Marc Siegmund <TKCDEV\siegmund>
2014-05-13 14:55:07 +0000

    added annotation for language check wich could be done by workaround (refresh)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C50325

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/UserService.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/UserService.cs
@@ -5,7 +5,7 @@ using SIX.SCS.QA.Selenium.Extension.TestObjects.User;
 
 namespace SIX.SCS.QA.Selenium.Extension.Worklow
 {
-    public class UserService
+    public static class UserService
     {
         public static void Create(UserData u)
         {
@@ -34,6 +34,7 @@ namespace SIX.SCS.QA.Selenium.Extension.Worklow
             Assert.AreEqual(u.FirstName, UserView.FirstName);
             Assert.AreEqual(u.Name, UserView.Name);
             // Removed check due to inconsistent user interface ("de" vs "Deutsch [de]")
+            // TODO: may be refresh and check values
             // Assert.AreEqual(u.Language, UserView.Language); 
             Assert.AreEqual(u.Email, UserView.Email);
             Assert.AreEqual(u.SecurId, UserView.SecurId);
