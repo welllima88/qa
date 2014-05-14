e276664218bade5cac7054947d1728b3b3558609
Marc Siegmund <TKCDEV\siegmund>
2014-05-14 12:02:38 +0000

    added Lobby Service
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C50365

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/LobbyService.cs
@@ -0,0 +1,13 @@
+﻿using SIX.SCS.QA.Selenium.Extension.TestObjects.Common;
+
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public static class LobbyService
+    {
+        public static void Language(string language)
+        {
+            MetaNavBar.Languages.Click();
+            MetaNavBar.Language(language).Click();
+        }
+    }
+}
