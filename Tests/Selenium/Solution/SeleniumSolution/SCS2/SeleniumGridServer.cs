cd6677d5974387590b7f35cc572a04e07107f9b0
Marc Siegmund <TKCDEV\siegmund>
2014-05-14 08:15:50 +0000

    addresses for grid servers
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C50346

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/SeleniumGridServer.cs
@@ -0,0 +1,15 @@
+﻿namespace SIX.SCS.QA.SCSPlatin.Tests.Selenium
+{
+    public static class SeleniumGridServer
+    {
+        public static string Build
+        {
+            get { return "http://wkbuild03:4488/wd/hub"; }
+        }
+
+        public static string Local
+        {
+            get { return "http://localhost:4488/wd/hub"; }
+        }
+    }
+}
