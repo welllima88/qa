1febdb68d97937cbfa7d5013f941064ea107dd64
Marc Siegmund <TKCDEV\siegmund>
2014-05-21 13:00:26 +0000

    updated  .net framework for Selenium extension project
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C50569

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/BrandService.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/BrandService.cs
@@ -1,15 +1,18 @@
-using SIX.SCS.QA.Selenium.Extension.TestObjects.Definitions;
 using SIX.SCS.QA.Selenium.Extension.TestObjects.Terminal.Dashboard.Brand;
 
 namespace SIX.SCS.QA.Selenium.Extension.Worklow
 {
     public class BrandService
     {
+        public static void Select(Acquirer acquirerBrands)
+        {
+        }
+
         public static void Create(Acquirer acquirerBrands)
         {
         }
 
-        public static void Check(UserData u)
+        public static void Check(Acquirer u)
         {
         }
     }
