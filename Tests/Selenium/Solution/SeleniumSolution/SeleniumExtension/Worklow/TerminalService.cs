20143eac538a43fbd665534c32311ccf39eb2c8c
Marc Siegmund <TKCDEV\siegmund>
2014-05-21 12:59:45 +0000

    added dummy service for terminal use cases
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C50568

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Worklow/TerminalService.cs
@@ -0,0 +1,26 @@
+namespace SIX.SCS.QA.Selenium.Extension.Worklow
+{
+    public class TerminalService
+    {
+        public static void Create(TerminalData terminalData)
+        {
+        }
+
+        public static void Check(TerminalData terminalData)
+        {
+            // DB Check Terminal and Contracts?
+        }
+
+        public static void Duplicate()
+        {
+        }
+    }
+
+    public class TerminalFactory
+    {
+    }
+
+    public class TerminalData
+    {
+    }
+}
