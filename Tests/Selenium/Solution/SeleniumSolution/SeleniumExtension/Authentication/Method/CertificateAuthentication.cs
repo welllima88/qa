a6bc73cf1cc0561e69fe5d78501b55a4ff6e685b
Marc Siegmund <TKCDEV\siegmund>
2014-06-17 09:05:01 +0000

    made static and removed constructor call
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51221

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/CertificateAuthentication.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/CertificateAuthentication.cs
@@ -6,19 +6,17 @@ namespace Six.Scs.Tests.Selenium.Extension.Authentication.Method
     public class CertificateAuthentication : WebObject, IAuthentication
     {
         private readonly string _mandant;
-        private readonly WesCertificateLoginPage _wes;
 
         public CertificateAuthentication(string mandant)
         {
-            _mandant = mandant;
-            _wes = new WesCertificateLoginPage();
+            _mandant = mandant;            
         }
 
         public void Login()
         {
             // choose tenant and authentication/submit:
-            _wes.Mandant(_mandant).Click();
-            _wes.LoginButton.Click();
+            WesCertificateLoginPage.Mandant(_mandant).Click();
+            WesCertificateLoginPage.LoginButton.Click();
         }
     }
 }
