ccd6f8dc416f938e15b7876094821c5e24678237
Marc Siegmund <TKCDEV\siegmund>
2014-06-17 09:32:14 +0000

    removed calls of constructors to statics
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51232

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/Factory.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestData/Factory.cs
@@ -4,41 +4,6 @@ namespace Six.Scs.Tests.Selenium.Extension.TestData
 {
     public static class Factory
     {
-        public static CustomerFactory Customer
-        {
-            get { return new CustomerFactory(); }
-        }
-
-        public static LocationFactory Location
-        {
-            get { return new LocationFactory(); }
-        }
-
-        public static ContactFactory Contact
-        {
-            get { return new ContactFactory(); }
-        }
-
-        public static AdressFactory Adress
-        {
-            get { return new AdressFactory(); }
-        }
-
-        public static ContactPersonFactory ContactPerson
-        {
-            get { return new ContactPersonFactory(); }
-        }
-
-        public static MpdFactory Mpd
-        {
-            get { return new MpdFactory(); }
-        }
-
-        public static UserFactory User
-        {
-            get { return new UserFactory(); }
-        }
-
         public static string GenerateTestId()
         {
             return DateTime.Now.Ticks.ToString();
