a11947f35a4ea71ea97f103e7aa7b8e9b5830b93
Marc Siegmund <TKCDEV\siegmund>
2014-06-17 09:12:15 +0000

    removed unused accessors
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51226

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Person/ContactPersonView.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Person/ContactPersonView.cs
@@ -19,67 +19,56 @@ namespace Six.Scs.Tests.Selenium.Extension.TestObjects.Person
         public static string FirstName
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.FirstName)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Name
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Name)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Salutation
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Salutation)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Country
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Country)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Language
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Language)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Telephone
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Telephone)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Mobile
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Mobile)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Fax
         {
             get { return WebDriver.FindElement(By.CssSelector(PersonViewRes.Fax)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Email
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_Email")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Web
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_Web")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string StreetNo
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_Street")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public string AdressAddition { get; set; }
@@ -87,25 +76,21 @@ namespace Six.Scs.Tests.Selenium.Extension.TestObjects.Person
         public static string Po
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_PO")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Zip
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_ZIP")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string City
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_City")).Text; }
-            set { throw new NotImplementedException(); }
         }
 
         public static string Region
         {
             get { return WebDriver.FindElement(By.CssSelector("#Address_Region")).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static IWebElement DeleteButton
