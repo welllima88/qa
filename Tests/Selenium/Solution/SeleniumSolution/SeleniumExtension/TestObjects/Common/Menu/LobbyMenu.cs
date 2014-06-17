cc1087b5f45c5957ebbaaa404dbbb8c20679c73a
Marc Siegmund <TKCDEV\siegmund>
2014-06-17 09:23:22 +0000

    removed fields
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51229

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Menu/LobbyMenu.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Common/Menu/LobbyMenu.cs
@@ -6,14 +6,7 @@ namespace Six.Scs.Tests.Selenium.Extension.TestObjects.Common.Menu
     ///     css Locator have problems with menu so use xpath until resolved the reason...
     /// </summary>
     public class LobbyMenu : MenuObject
-    {
-        public static LocationMenu LocationMenu;
-        public static MassMutationMenu MassMutation;
-        public static SimCardMenu SimCard;
-        public static TerminalMenu TerminalMenu;
-        public static TicketMenu Ticket;
-        public CustomerMenu Customer;
-
+    {        
         public static IWebElement Lobby
         {
             get { return WebDriver.FindElement(By.CssSelector(MenuRes.LobbyMenu_Lobby)); }
