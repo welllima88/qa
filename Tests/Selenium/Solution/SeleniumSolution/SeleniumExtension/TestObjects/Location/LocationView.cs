d1901d808a3a2c246f852949c67658a62bac764f
Marc Siegmund <TKCDEV\siegmund>
2014-06-18 12:37:22 +0000

    removed unsupported operations
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51292

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Location/LocationView.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Location/LocationView.cs
@@ -6,8 +6,6 @@ namespace Six.Scs.QA.Selenium.Extension.TestObjects.Location
 {
     public class LocationView : WebObject
     {
-        #region ILocationView Members
-
         public static IWebElement EditButton
         {
             get { return WebDriver.FindElement(By.CssSelector("input#edit")); }
@@ -16,7 +14,6 @@ namespace Six.Scs.QA.Selenium.Extension.TestObjects.Location
         public static string CompanyName
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.CompanyName)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Guid
@@ -27,19 +24,16 @@ namespace Six.Scs.QA.Selenium.Extension.TestObjects.Location
         public static string StreetNo
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.StreetName)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Zip
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Zip)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string City
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.City)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static String SbsDebitNumber
@@ -55,75 +49,61 @@ namespace Six.Scs.QA.Selenium.Extension.TestObjects.Location
         public static string Language
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Language)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Country
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Country)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Ep2MerchantId
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Ep2MerchantId)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string SapNumber
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.SapNumber)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Po
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Po)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string AdressAddition
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.AdressAddition)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Email
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Email)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Web
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Web)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Telephone
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Telephone)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Mobile
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Mobile)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Fax
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Fax)).Text; }
-            set { throw new NotSupportedException(); }
         }
 
         public static string Region
         {
             get { return WebDriver.FindElement(By.CssSelector(LocationRes.Region)).Text; }
-            set { throw new NotSupportedException(); }
         }
-
-        #endregion
     }
 }
