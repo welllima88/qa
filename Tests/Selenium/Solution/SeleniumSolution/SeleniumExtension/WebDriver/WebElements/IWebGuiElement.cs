4a439b39988b7acd798f43b16de867db44c007a2
Marc Siegmund <TKCDEV\siegmund>
2014-06-18 13:00:09 +0000

    removed interface which wasn't used
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51304

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/WebDriver/WebElements/IWebGuiElement.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Six.Scs.QA.Selenium.Extension.WebDriver.WebElements
-{
-    /// <summary>
-    ///     Interface to mark, that the class is WebGUIElement
-    /// </summary>
-    public interface IWebGuiElement
-    {
-    }
-}
