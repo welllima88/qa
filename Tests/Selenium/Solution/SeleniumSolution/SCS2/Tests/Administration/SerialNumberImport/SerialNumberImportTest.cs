3290de779b115e567aab0fbcf3e515cae82fc8ff
Marc Siegmund <TKCDEV\siegmund>
2014-06-25 15:16:11 +0000

    splitted tests
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51506

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SerialNumberImport/SerialNumberImportTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Tests/Administration/SerialNumberImport/SerialNumberImportTest.cs
@@ -69,29 +69,6 @@ namespace Six.Scs.QA.Selenium.Tests.Administration.SerialNumberImport
             CheckSupplier("GMX", "1", "11");
         }
 
-        [TestMethod]
-        [TestCategory("SerialNumberManagement"), TestCategory("PreviewSerialNumbers")]
-        public void PreviewSerialNumbers()
-        {
-            SerialNumberManage.SerialNumbersField =
-                "1234567890A11223344556677889900000\n" +
-                "11234567890B1122334455667788990000\n" +
-                "111234567890C112233445566778899000\n" +
-                "1111234567890D11223344556677889900\n";
-
-            CheckPreview("1", "10", "1234567890");
-            CheckPreview("2", "11", "234567890A1");
-            CheckPreview("5", "1", "5");
-        }
-
-        private void CheckPreview(string start, string length, string expectedPreview)
-        {
-            SerialNumberManage.StartField = start;
-            SerialNumberManage.LengthField = length;
-
-            Assert.AreEqual(expectedPreview, SerialNumberManage.Preview);
-        }
-
         private static void CheckSupplier(string supplier, string start, string length)
         {
             SerialNumberManage.Supplier = supplier;
