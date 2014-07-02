22da0b1ad3db4957f2956e62841adfe4c4ddaa9e
Marc Siegmund <TKCDEV\siegmund>
2014-07-02 12:09:32 +0000

    added locator property removed contructors, changed doc
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51744

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/WebDriver/WebElements/WebObject.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/WebDriver/WebElements/WebObject.cs
@@ -1,3 +1,5 @@
+using OpenQA.Selenium;
+
 namespace Six.Scs.QA.Selenium.Extension.WebDriver.WebElements
 {
     /// <summary>
@@ -8,31 +10,10 @@ namespace Six.Scs.QA.Selenium.Extension.WebDriver.WebElements
         protected string Prefix = "";
         protected string Suffix = "";
 
-        /// <summary>
-        ///     This abstract class of an WebObject defines the essential constructor for initializing the WebDriver reference.
-        ///     It is also used for adding features to all test objects
-        /// </summary>
-        /// <param name="driver"></param>
-        protected WebObject(IWebDriverAdapter driver)
-        {
-            WebDriver = driver;
-        }
-
-        protected WebObject()
-        {
-        }
-
-        // /// <summary>
-        // /// override the default constructor to prevent initializing without a WebDriver reference
-        // /// </summary>
-        // private WebObject()
-        //{
-        //   throw new NotSupportedException("The default constructor is not allowed to be used to avoid missing webdriver reference");
-        //}
+        public By Locator { get; set; }
 
         /// <summary>
-        ///     overrides the acessor for this public WebDriver reference, so that the constructor can set (internally usually by
-        ///     the constructor)
+        ///     provides a public WebDriver reference for global use
         /// </summary>
         public static IWebDriverAdapter WebDriver { get; set; }
     }
