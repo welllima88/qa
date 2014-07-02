e6f5f3e2f9a66e33290fd08dda2834ef6afe1fe9
Marc Siegmund <TKCDEV\siegmund>
2014-07-02 12:42:19 +0000

    renamed
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51754

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Terminal/Dashboard/TerminalFixInfo.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestObjects/Terminal/Dashboard/TerminalFixInfo.cs
@@ -5,15 +5,10 @@ namespace Six.Scs.QA.Selenium.Extension.TestObjects.Terminal.Dashboard
     /// <summary>
     ///     This object bundles the fix view elements of the "Terminal Dashboard"
     /// </summary>
-    public class TerminFixInfo : WebObject
+    public class TerminalFixInfo : WebObject
     {
         public static readonly CustomerInfo Customer;
         public static readonly LocationInfo Location;
         public static readonly TerminalInfo Terminal;
-
-        public TerminFixInfo()
-        {
-            Prefix = "";
-        }
     }
 }
