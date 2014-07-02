51ad02e7bc11a198a985c546517d1eb81b36769a
Marc Siegmund <TKCDEV\siegmund>
2014-07-02 07:48:00 +0000

    added button element as webobject
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51722

---
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/WebDriver/WebElements/Button.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+
+namespace Six.Scs.QA.Selenium.Extension.WebDriver.WebElements
+{
+    public class Button
+    {
+        private readonly IWebElement _webElement;
+
+        public Button(IWebElement webElement)
+        {
+            //if (!(webElement.TagName.Equals("input") && webElement.GetAttribute("type").Equals(Type())))
+            //    throw new NotButtonElementException(webElement.TagName, webElement.GetCssValue("type"));
+            _webElement = webElement;
+        }
+
+        public void Click()
+        {
+            _webElement.Click();
+        }
+    }
+}
