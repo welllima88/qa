d1d3aa21bfb496bc4c4aa435f245d23f3f4ea090
Marc Siegmund <TKCDEV\siegmund>
2014-07-04 09:18:48 +0000

    changed global regular expression to string only (nunit has different signature) and removed some files/folders
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51845

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestRegExpPatterns.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestRegExpPatterns.cs
@@ -10,20 +10,20 @@ namespace Six.Scs.QA.Selenium.Extension
     /// </summary>
     public static class TestRegExpPatterns
     {
-        public static readonly Regex WesHeadline = new Regex(@"^SIX [\s\S]* - WES.*");
-
-        public static readonly Regex CustomerNo = new Regex("[0-9]{1,7}");
-        public static readonly Regex SbsAdressNo = new Regex("[0-9]{1,7}");
-        public static readonly Regex SbsAdressNoOpt = new Regex("[0-9]{1,7}|");
-        public static readonly Regex SbsDebitorNo = new Regex("[0-9]{1,7}");
-        public static readonly Regex Ep2MerchantId = new Regex("[a-zA-Z0-9]{0,7}[0-9]{0,8}");
-        public static readonly Regex Guid = new Regex(@"[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}");
-        public static readonly Regex Date = new Regex("[0-9]{2}.[0-9]{2}.[0-9]{4}");
-        public static readonly Regex Time = new Regex("[0-9]{2}:[0-9]{2}:[0-9]{2}");
-        public static readonly Regex DateTime = new Regex(Date + " " + Time);
-        public static readonly Regex TerminalPassword = new Regex("[0-9]{5,8}");
-        public static readonly Regex UserName = new Regex(@"\w{5}");
-        public static readonly Regex NotEmpty = new Regex(@"\w");
-        public static readonly Regex UserPassword = new Regex(@"[\w]{4,10}|");
+        public static readonly string WesHeadline = @"^SIX [\s\S]* - WES.*";
+                               
+        public static readonly string CustomerNo = "[0-9]{1,7}";
+        public static readonly string SbsAdressNo = "[0-9]{1,7}";
+        public static readonly string SbsAdressNoOpt = "[0-9]{1,7}|";
+        public static readonly string SbsDebitorNo = "[0-9]{1,7}";
+        public static readonly string Ep2MerchantId = "[a-zA-Z0-9]{0,7}[0-9]{0,8}";
+        public static readonly string Guid = @"[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}";
+        public static readonly string Date = "[0-9]{2}.[0-9]{2}.[0-9]{4}";
+        public static readonly string Time = "[0-9]{2}:[0-9]{2}:[0-9]{2}";
+        public static readonly string DateTime = Date + " " + Time;
+        public static readonly string TerminalPassword = "[0-9]{5,8}";
+        public static readonly string UserName = @"\w{5}";
+        public static readonly string NotEmpty = @"\w";
+        public static readonly string UserPassword = @"[\w]{4,10}|";
     }
 }
