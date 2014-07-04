a58dc748b3ac7c5b3eb2ca92355149940c62e3c0
Marc Siegmund <TKCDEV\siegmund>
2014-07-04 09:29:55 +0000

    updated packages
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51849

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/SecurIdAuthentication.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Authentication/Method/SecurIdAuthentication.cs
@@ -1,5 +1,4 @@
 using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Six.Scs.QA.Selenium.Extension.Authentication.WebPages;
 using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
 
