f23ddaf29674cb06e0718b23db1e632263290a79
Marc Siegmund <TKCDEV\siegmund>
2014-07-10 09:19:04 +0000

    added test flow for customer
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51982

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Customer.cs
@@ -0,0 +1,36 @@
+﻿using Six.Scs.QA.Selenium.Extension.TestData.Factory;
+using Six.Scs.QA.Selenium.Extension.TestData.ValueObjects;
+using Six.Scs.QA.Selenium.Extension.Worklow;
+
+namespace Six.Scs.QA.Selenium
+{
+    public class Customer
+    {
+        public static CustomerData Edit(CustomerData customer)
+        {
+            Open(customer);
+            CustomerData _customer = CustomerFactory.Edit();
+            CustomerService.Edit(_customer);
+            CustomerService.Check(_customer);
+
+            LobbyService.OpenLatestElement();
+            CustomerService.Check(_customer);
+            return _customer;
+        }
+
+        public static CustomerData Create()
+        {
+            CustomerData customer = CustomerFactory.Create();
+            CustomerService.Create(customer);
+            CustomerService.Check(customer);
+            LobbyService.OpenLatestElement();
+            CustomerService.Check(customer);
+            return customer;
+        }
+
+        public static void Open(CustomerData customer)
+        {
+            SearchService.CustomerCanBeFoundByCustomerNumber(customer.CustomerNumber);
+        }
+    }
+}
