2809ed9cba5faf2cac566238be7e0d13c905d97b
Marc Siegmund <TKCDEV\siegmund>
2014-07-10 11:29:59 +0000

    made to constants removed using
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C51993

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestRegExpPatterns.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/TestRegExpPatterns.cs
@@ -1,5 +1,3 @@
-using System.Text.RegularExpressions;
-
 namespace Six.Scs.QA.Selenium.Extension
 {
     /// <summary>
@@ -11,19 +9,19 @@ namespace Six.Scs.QA.Selenium.Extension
     public static class TestRegExpPatterns
     {
         public static readonly string WesHeadline = @"^SIX [\s\S]* - WES.*";
-                               
-        public static readonly string CustomerNo = "[0-9]{1,7}";
-        public static readonly string SbsAdressNo = "[0-9]{1,7}";
-        public static readonly string SbsAdressNoOpt = "[0-9]{1,7}|";
-        public static readonly string SbsDebitorNo = "[0-9]{1,7}";
-        public static readonly string Ep2MerchantId = "[a-zA-Z0-9]{0,7}[0-9]{0,8}";
-        public static readonly string Guid = @"[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}";
-        public static readonly string Date = "[0-9]{2}.[0-9]{2}.[0-9]{4}";
-        public static readonly string Time = "[0-9]{2}:[0-9]{2}:[0-9]{2}";
-        public static readonly string DateTime = Date + " " + Time;
+
+        public const string CustomerNo = "[0-9]{1,7}";
+        public const string SbsAdressNo = "[0-9]{1,7}";
+        public const string SbsAdressNoOpt = "[0-9]{1,7}|";
+        public const string SbsDebitorNo = "[0-9]{1,7}";
+        public const string Ep2MerchantId = "[a-zA-Z0-9]{0,7}[0-9]{0,8}";
+        public const string Guid = @"[\w]{8}-[\w]{4}-[\w]{4}-[\w]{4}-[\w]{12}";
+        public const string Date = "[0-9]{2}.[0-9]{2}.[0-9]{4}";
+        public const string Time = "[0-9]{2}:[0-9]{2}:[0-9]{2}";
+        public const string DateTime = Date + " " + Time;
         public static readonly string TerminalPassword = "[0-9]{5,8}";
-        public static readonly string UserName = @"\w{5}";
-        public static readonly string NotEmpty = @"\w";
-        public static readonly string UserPassword = @"[\w]{4,10}|";
+        public const string UserName = @"\w{5}";
+        public const string NotEmpty = @"\w";
+        public const string UserPassword = @"[\w]{4,10}|";
     }
 }
