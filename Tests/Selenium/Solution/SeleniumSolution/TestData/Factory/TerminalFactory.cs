185af6e6ff5aeb787f574373615e356dcf2c80f3
Marc Siegmund <TKCDEV\siegmund>
2014-07-10 15:10:31 +0000

    added terminal data factory (stub)
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52040

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/TerminalFactory.cs
@@ -0,0 +1,17 @@
+using Six.Scs.QA.TestData.ValueObjects;
+
+namespace Six.Scs.QA.TestData.Factory
+{
+    public class TerminalFactory
+    {
+        public static TerminalData Xentissimo()
+        {
+            return new TerminalData();
+        }
+
+        public static TerminalData Yomani()
+        {
+            return new TerminalData();
+        }
+    }
+}
