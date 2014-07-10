844d669d2502057a2823fc2622d7458755de8ad4
Marc Siegmund <TKCDEV\siegmund>
2014-07-10 14:57:59 +0000

    added default application stuff (login) which belongs to environment
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52032

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS/ScsClassicEnvironment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Six.Scs.QA.Selenium.Extension.Authentication.Method;
+using Six.Scs.QA.Selenium.Extension.Authentication.WebPages;
+using Six.Scs.QA.Selenium.Extension.Environment;
+using Six.Scs.QA.Selenium.Extension.Properties;
+
+namespace Six.Scs.QA.Selenium
+{
+    public class ScsClassicEnvironment
+    {
+        public static void Dev()
+        {
+            TestEnvironment.BaseUrl = new Uri("https://mdzhwcweb01/mgmt/");
+            TestEnvironment.BrowserProfileName = ScsRes.FirefoxProfile_Plain;
+            TestEnvironment.Application = new Common.Scs();
+            TestEnvironment.Authentication = new UacAuthentication(new UacLogOnPageOld(), ScsRes.uac_username, ScsRes.uac_passwort);
+            TestEnvironment.SeleniumConfig = new SeleniumConfig
+            {
+                Timeouts = new Timeouts {ImplicitlyWait = 5, SetScriptTimeout = 10, SetPageLoadTimeout = 30}
+            };
+        }
+
+        public static void Prod()
+        {
+            TestEnvironment.BaseUrl = new Uri("https://gate.telekurs.ch/scss/");
+            TestEnvironment.BrowserProfileName = ScsRes.FirefoxProfile_Plain;
+            TestEnvironment.Application = new Common.Scs();
+            TestEnvironment.Authentication = new SecurIdAuthentication("tksyr", @"PW", "tkcpos", "??");
+            TestEnvironment.SeleniumConfig = new SeleniumConfig
+            {
+                Timeouts = new Timeouts {ImplicitlyWait = 5, SetScriptTimeout = 10, SetPageLoadTimeout = 30}
+            };
+        }
+
+        public static void QaK()
+        {
+            TestEnvironment.BaseUrl = new Uri("https://gateint.telekurs.ch/scsc-qa-k/");
+            TestEnvironment.BrowserProfileName = ScsRes.FirefoxProfile_Certificate;
+            TestEnvironment.Application = new Common.Scs();
+            TestEnvironment.Authentication = new CertificateAuthentication(ScsRes.mandant_qa_K);
+            TestEnvironment.SeleniumConfig = new SeleniumConfig
+            {
+                Timeouts = new Timeouts {ImplicitlyWait = 5, SetScriptTimeout = 10, SetPageLoadTimeout = 30}
+            };
+        }
+
+        public static void QaL()
+        {
+            TestEnvironment.BaseUrl = new Uri("https://gateint.telekurs.ch/scsc-qa-l/");
+            TestEnvironment.BrowserProfileName = ScsRes.FirefoxProfile_Certificate;
+            TestEnvironment.Application = new Common.Scs();
+            TestEnvironment.Authentication = new CertificateAuthentication(ScsRes.mandant_qa_L);
+            TestEnvironment.SeleniumConfig = new SeleniumConfig
+            {
+                Timeouts = new Timeouts {ImplicitlyWait = 5, SetScriptTimeout = 10, SetPageLoadTimeout = 30}
+            };
+        }
+    }
+}
