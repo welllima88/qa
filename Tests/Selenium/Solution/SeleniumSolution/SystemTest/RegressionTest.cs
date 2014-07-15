e042d6e97429ed1e6c74fbd34952dbc60cbd13cf
Marc Siegmund <TKCDEV\siegmund>
2014-07-15 08:34:09 +0000

    completely refactored the system test
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52154

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/RegressionTest.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Six.Scs.QA.TestData.ValueObjects;
+
+namespace Six.Scs.QA.Selenium.SystemTest
+{
+    [TestFixture]
+    public class RegressionTest
+    {
+        private static TerminalData _terminalLocation;
+        private static TerminalData _terminalCustomer;
+        private static CustomerData _customer;
+        private static LocationData _location;
+        private static ContactPersonData _personOnCustomer;
+        private static ContactPersonData _personOnLocation;
+        private static UserData _user;
+        private static List<string> _duplicatedTerminals;
+
+        /* [TestFixtureTearDown]
+        public static void CleanRegressionTest()
+        {
+            DeleteCustomer();
+            DeleteLocation();
+            DeletePersons();
+            DeleteTerminals();            
+            DeleteMpd();            
+            DeleteUser();            
+        }
+
+        [TestFixtureSetUp]
+        public static void PrepareRegressionTest()
+        {            
+        }
+         
+        [Test]
+        [Ignore]
+        [Category("Regression")]
+        public void BrandsOnTerminalAreCreated()
+        {
+            Terminal.Open(_terminalLocation);
+            StringAssert.Contains("[283]", BrandPortlet.Brand("283").Text);
+            StringAssert.Contains("[332]", BrandPortlet.Brand("332").Text);
+            StringAssert.Contains("[361]", BrandPortlet.Brand("361").Text);
+            StringAssert.Contains("[363]", BrandPortlet.Brand("363").Text);
+            StringAssert.Contains("[811]", BrandPortlet.Brand("811").Text);
+        }
+        */
+
+        [Test]
+        public static void ExecuteRegressiontest()
+        {
+            _customer = Customer.Create();
+            _location = Location.Create(_customer);
+            _personOnCustomer = Contact.Create(_customer);
+            _terminalCustomer = Terminal.Create(_customer);
+
+            _customer = Customer.Edit(_customer);
+            Infotext.Create(_customer);
+            Mpd.Create(_customer);
+
+            Brands.Create(_terminalCustomer);
+
+            _terminalLocation = Terminal.Create(_location);
+            Infotext.Create(_location);
+
+            _location = Location.Edit(_location);
+            Infotext.Create(_terminalLocation);
+
+            _personOnLocation = Contact.Create(_location);
+            _personOnLocation = Contact.Edit(_personOnLocation);
+
+            _user = User.Create(_customer);
+            _user = User.Edit(_user);
+
+            _duplicatedTerminals = Terminal.Duplicate(_terminalLocation);
+
+            Brands.Create(_duplicatedTerminals[1]);
+        }
+    }
+}
