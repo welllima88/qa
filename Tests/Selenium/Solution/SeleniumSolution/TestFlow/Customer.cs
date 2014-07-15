6774f45a78357d305e5df7d89b7fb6d0226b6d41
Marc Siegmund <TKCDEV\siegmund>
2014-07-15 13:38:42 +0000

    extracted check method and further test flow for customer
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52205

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Customer.cs
@@ -0,0 +1,74 @@
+﻿using NUnit.Framework;
+using Six.Scs.QA.Selenium.Customer;
+using Six.Scs.QA.Selenium.Extension;
+using Six.Scs.QA.TestData.Factory;
+using Six.Scs.QA.TestData.ValueObjects;
+using Six.Scs.QA.Workflow;
+
+namespace Six.Scs.QA.TestFlow
+{
+    public class Customer
+    {
+        public static CustomerData Edit(CustomerData customer)
+        {
+            Open(customer);
+            CustomerData _customer = CustomerFactory.Edit();
+            CustomerService.Edit(_customer);
+            Check(_customer);
+
+            LobbyService.OpenLatestElement();
+            Check(_customer);
+            return _customer;
+        }
+
+        public static CustomerData Create()
+        {
+            CustomerData customer = CustomerFactory.Create();
+            CustomerService.Create(customer);
+            Check(customer);
+            LobbyService.OpenLatestElement();
+            Check(customer);
+            return customer;
+        }
+
+        public static void Open(CustomerData customer)
+        {
+            SearchService.CustomerCanBeFoundByCustomerNumber(customer.CustomerNumber);
+        }
+
+        public static void Check(CustomerData c)
+        {
+            Assert.AreEqual(c.CustomerNumber, CustomerView.CustomerNumber);
+            Assert.AreEqual(c.CustomerName, CustomerView.CustomerName);
+            Assert.AreEqual(c.Supplier, CustomerView.Supplier);
+            Assert.AreEqual(c.SbsBillingTenant, CustomerView.SbsBillingTenant);
+            Assert.AreEqual(c.SbsCurrency, CustomerView.SbsCurrency);
+            Assert.AreEqual(c.Location.SbsDebitNumber, CustomerView.SbsDebitNumber);
+            // Todo: not reliable check: 
+            // Assert.AreEqual(c.SbsAdressNumber, CustomerView.SbsAdressNumber);
+            Assert.AreEqual(c.Location.SapNumber, CustomerView.SapNumber);
+            Assert.AreEqual(c.Supplier, CustomerView.Supplier);
+            Assert.AreEqual(c.SbsBillingTenant, CustomerView.SbsBillingTenant);
+            StringAssert.Contains(c.SbsCurrency, CustomerView.SbsCurrency);
+
+            StringAssert.IsMatch(TestRegExpPatterns.SbsDebitorNo, CustomerView.SbsDebitNumber);
+            StringAssert.IsMatch(TestRegExpPatterns.SbsAdressNoOpt, CustomerView.SbsAdressNumber);
+            StringAssert.IsMatch(TestRegExpPatterns.Ep2MerchantId, CustomerView.Ep2MerchantId);
+
+            Assert.AreEqual(c.Location.Adress.Po, CustomerView.Po);
+            Assert.AreEqual(c.Location.Adress.AdressAddition, CustomerView.AdressAddition);
+            Assert.AreEqual(c.Location.Adress.Region, CustomerView.Region);
+            Assert.AreEqual(c.Location.Adress.StreetNo, CustomerView.StreetNo);
+            Assert.AreEqual(c.Location.Adress.Zip, CustomerView.Zip);
+            Assert.AreEqual(c.Location.Adress.City, CustomerView.City);
+            StringAssert.Contains(c.Agency, CustomerView.Agency);
+            Assert.AreEqual(c.Location.Contact.Language, CustomerView.Language);
+            Assert.AreEqual(c.Location.Adress.Country, CustomerView.Country);
+            Assert.AreEqual(c.Location.Contact.Email, CustomerView.Email);
+            Assert.AreEqual(c.Location.Contact.Telephone, CustomerView.Telephone);
+            Assert.AreEqual(c.Location.Contact.Mobile, CustomerView.Mobile);
+            Assert.AreEqual(c.Location.Contact.Fax, CustomerView.Fax);
+            Assert.AreEqual(c.Location.Contact.Web, CustomerView.Web);
+        }
+    }
+}
