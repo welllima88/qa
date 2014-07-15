cca3e5acd1991555cf003f9a3b7d63bc5b120b01
Marc Siegmund <TKCDEV\siegmund>
2014-07-15 14:28:48 +0000

    added check and separated logic for creating info texts
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52218

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Infotext.cs
@@ -0,0 +1,44 @@
+using NUnit.Framework;
+using Six.Scs.QA.Selenium.Common.Infotext;
+using Six.Scs.QA.Selenium.Common.Menu;
+using Six.Scs.QA.TestData.ValueObjects;
+using Six.Scs.QA.Workflow;
+
+namespace Six.Scs.QA.Testflow
+{
+    public class Infotext
+    {
+        public static void Create(CustomerData customer)
+        {
+            Customer.Open(customer);
+
+            CreateAndSaveInfotext("Customer");
+        }
+
+        public static void Create(LocationData location)
+        {
+            Location.Open(location);
+            CreateAndSaveInfotext("Location");
+        }
+
+        public static void Create(TerminalData terminal)
+        {
+            Terminal.Open(terminal);
+            CreateAndSaveInfotext("Terminal");
+        }
+
+        private static void CreateAndSaveInfotext(string parameter)
+        {
+            CustomerMenu.Infotexts.Click();
+            InfoTextListView.CreateButton.Click();
+
+            InfoText.Create(parameter);
+            Check(parameter);
+        }
+
+        public static void Check(string infotext)
+        {
+            Assert.IsTrue(InfoTextListView.List.Contains(infotext));
+        }
+    }
+}
