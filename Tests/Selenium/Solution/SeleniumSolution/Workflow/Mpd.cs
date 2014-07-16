baace17effd7f33fb43a7823d97d22821dbb581f
Marc Siegmund <TKCDEV\siegmund>
2014-07-16 09:30:55 +0000

    added mpd workflow and implemented edit flow
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52230

---
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/Mpd.cs
@@ -0,0 +1,36 @@
+﻿using Six.Scs.QA.Selenium.Common.Menu;
+using Six.Scs.QA.Selenium.Mpd;
+using Six.Scs.QA.TestData.ValueObjects;
+
+namespace Six.Scs.QA.Workflow
+{
+    public static class Mpd
+    {
+        public static void Create(MpdData m)
+        {
+            CustomerMenu.AllMpds.Click();
+            MpdListView.CreateButton.Click();
+
+            MpdCreate.Description = m.Description;
+            MpdCreate.Adress = m.Adress;
+            MpdCreate.Port = m.Port;
+
+            MpdCreate.SaveButton.Click();
+
+            m.Id = MpdView.Id;
+        }
+
+        public static void Edit(MpdData m)
+        {
+            MpdView.EditButton.Click();
+            
+            MpdCreate.Description = m.Description;
+            MpdCreate.Adress = m.Adress;
+            MpdCreate.Port = m.Port;
+
+            MpdCreate.SaveButton.Click();
+
+            m.Id = MpdView.Id;
+        }
+    }
+}
