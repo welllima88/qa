a4ec0ab693115755c6e24169e8a2d0e470d85ba2
Marc Siegmund <TKCDEV\siegmund>
2014-07-16 15:29:38 +0000

    added smoketests
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52264

---
+++ Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Agency/AgencyCreateTest.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Six.Scs.QA.Selenium.Extension.TestData;
+using Six.Scs.QA.Selenium.Extension.TestData.Factory;
+using Six.Scs.QA.Selenium.Extension.TestObjects.Administration.Agency;
+using Six.Scs.QA.Selenium.Extension.WebDriver;
+
+namespace Six.Scs.QA.Selenium.Tests.Administration.Agency
+{
+    [TestClass]
+    public class AgencyCreateTest
+    {
+        private static string _nameDyn;
+
+        [ClassInitialize]
+        public static void ClassInit(TestContext testContext)
+        {
+            _nameDyn = "SYR " + Factory.GenerateTestId();
+
+            TestDirector.Navigate("Agency/Create");
+
+            AgencyCreate.Name = _nameDyn;
+            AgencyCreate.DisplayName = _nameDyn + "D";
+            AgencyCreate.StreetNo = "Agentur-Str. 12a";
+            AgencyCreate.Zip = "10235";
+            AgencyCreate.City = "Hamburg";
+            AgencyCreate.Country = "DE";
+            AgencyCreate.Language = "de";
+            AgencyCreate.Telephone = "+41 58 399 6237";
+            AgencyCreate.Supplier = "Alle Fremdhersteller";
+            AgencyCreate.SbsAgentId = "881144";
+
+            AgencyCreate.SaveButton.Click();
+        }
+
+        [TestMethod]
+        public void Name()
+        {
+            Assert.AreEqual(_nameDyn, AgencyView.Name);
+        }
+
+        [TestMethod]
+        public void DisplayName()
+        {
+            Assert.AreEqual(_nameDyn + "D", AgencyView.DisplayName);
+        }
+
+        [TestMethod]
+        public void StreetNo()
+        {
+            Assert.AreEqual("Agentur-Str. 12a", AgencyView.StreetNo);
+        }
+
+        [TestMethod]
+        public void Zip()
+        {
+            Assert.AreEqual("10235", AgencyView.Zip);
+        }
+
+        [TestMethod]
+        public void City()
+        {
+            Assert.AreEqual("Hamburg", AgencyView.City);
+        }
+
+        [TestMethod]
+        public void Country()
+        {
+            Assert.AreEqual("Deutschland", AgencyView.Country);
+        }
+
+        [TestMethod]
+        public void Language()
+        {
+            Assert.AreEqual("Deutsch [de]", AgencyView.Language);
+        }
+
+        [TestMethod]
+        public void Telephone()
+        {
+            Assert.AreEqual("+41 58 399 6237", AgencyView.Telephone);
+        }
+
+        [TestMethod]
+        public void Supplier()
+        {
+            Assert.AreEqual("Alle Fremdhersteller", AgencyView.Supplier);
+        }
+
+        [TestMethod]
+        public void SbsAgentId()
+        {
+            Assert.AreEqual("881144", AgencyView.SbsAgentId);
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SmokeTest/User/UserViewTest.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Six.Scs.QA.Selenium.Extension.TestObjects.User;
+using Six.Scs.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Tests.Menu;
+
+namespace Six.Scs.QA.Selenium.Tests.User
+{
+    [TestClass]
+    public class UserViewTest
+    {
+        [ClassInitialize]
+        public static void NavigateToUser(TestContext testContext)
+        {
+            TestDirector.Navigate("User/Index?UserName=six_scs_auto");
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void UserName()
+        {
+            Assert.AreEqual("six_scs_auto", UserView.UserName);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void UserPassword()
+        {
+            Assert.AreEqual("", UserView.Password);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void UserSalutation()
+        {
+            Assert.AreEqual("Herr", UserView.Salutation);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void FirstName()
+        {
+            Assert.AreEqual("Automatisierer", UserView.FirstName);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void LastName()
+        {
+            Assert.AreEqual("SCS", UserView.Name);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void Email()
+        {
+            Assert.AreEqual("marc.siegmund@six-group.com", UserView.Email);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void Language()
+        {
+            Assert.AreEqual("Deutsch [de]", UserView.Language);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void SecureId()
+        {
+            Assert.AreEqual("tksyr", UserView.SecurId);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void WesMandant()
+        {
+            StringAssert.Contains(UserView.WesMandant, "TKCPOS");
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void LastLogOn()
+        {
+            Assert.IsTrue(DateTime.Now.CompareTo(DateTime.Parse(UserView.LastLogOn)) > 0);
+            Assert.IsTrue(DateTime.Now.AddDays(-1).CompareTo(DateTime.Parse(UserView.LastLogOn)) < 0);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void Created()
+        {
+            StringAssert.Contains(UserView.Created, "25.07.2012");
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void Comment()
+        {
+            Assert.AreEqual("DO NOT DELETE", UserView.Comment);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void EditButton()
+        {
+            Assert.IsTrue(UserView.EditButton.Displayed);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void ReactivateButton()
+        {
+            Assert.IsTrue(UserView.ReactivateButton.Displayed);
+        }
+
+        [TestMethod]
+        [TestCategory("SmokeTest")]
+        public void MenuTest()
+        {
+            MenusTest.UserMenuCheck();
+        }
+    }
+}
