61118952fa61a2824e640dcb8977eb3c40fa7094
Marc Siegmund <TKCDEV\siegmund>
2014-07-18 11:50:33 +0000

    added search result div for modular use
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52318

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/SearchResult/SearchDivLocator.cs
@@ -0,0 +1,8 @@
+namespace Six.Scs.QA.Selenium.SearchResult
+{
+    public abstract class SearchDivLocator
+    {
+        public abstract string ResultId { get; }
+        public abstract string LinkPart { get; }
+    }
+}
