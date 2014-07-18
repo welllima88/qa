423c6a2ebe2f6fa2fe173a40bb6bbf75638ad861
Marc Siegmund <TKCDEV\siegmund>
2014-07-18 08:49:26 +0000

    added test data and factory for simcard data
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52313

--- Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/Factory.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/Factory.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Six.Scs.QA.TestData.ValueObjects;
 
 namespace Six.Scs.QA.TestData.Factory
 {
@@ -7,6 +8,37 @@ namespace Six.Scs.QA.TestData.Factory
         public static string GenerateTestId()
         {
             return DateTime.Now.Ticks.ToString();
+        }
+
+        public static class Simcard
+        {
+            public static SimCardData Create()
+            {
+                return new SimCardData
+                {
+                    NetProvider = "SIX via Swisscom",
+                    SimCardNumber = "0123456789012345678",
+                    MobileNumber = "1234567890",
+                    Pin = "4321",
+                    Puk = "22446688",
+                    Status = true,
+                    TerminalId = "21012730"
+                };
+            }
+
+            public static SimCardData Edit()
+            {
+                return new SimCardData
+                {
+                    MobileNumber = "0123456789",
+                    NetProvider = "SIX via Swisscom",
+                    Pin = "0123",
+                    Puk = "01234567",
+                    Region = "Schweiz",
+                    SimCardNumber = "0123456789012345679",
+                    Usage = "Kauf"
+                };
+            }
         }
     }
 }
---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/ValueObjects/SimCardData.cs
@@ -0,0 +1,15 @@
+﻿namespace Six.Scs.QA.TestData.ValueObjects
+{
+    public class SimCardData
+    {
+        public string MobileNumber;
+        public string NetProvider;
+        public string Pin;
+        public string Puk;
+        public string Region;
+        public string SimCardNumber;
+        public bool Status;
+        public string TerminalId;
+        public string Usage;
+    }
+}
