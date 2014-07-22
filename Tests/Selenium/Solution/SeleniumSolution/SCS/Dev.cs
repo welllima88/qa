cca79f6e9869689d5e813cf25af9a14d70f33097
Marc Siegmund <TKCDEV\siegmund>
2014-07-22 16:13:11 +0000

    added test environment for scs old
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52405

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS/Dev.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Six.Scs.QA.Selenium.Extension.Authentication.Method;
+using Six.Scs.QA.Selenium.Extension.Authentication.WebPages;
+using Six.Scs.QA.Selenium.Extension.Environment;
+using Six.Scs.QA.Selenium.Extension.Properties;
+
+namespace Six.Scs.QA.Selenium
+{
+    public class Dev : TestEnvironment
+    {
+        public Dev()
+        {
+            BaseUrl = new Uri("https://mdzhwcweb01/mgmt/");
+            BrowserProfileName = ScsRes.FirefoxProfile_Plain;
+            Application = new Common.Scs();
+            Authentication = new UacAuthentication(new UacLogOnPageOld(),
+                ScsRes.uac_username,
+                ScsRes.uac_passwort);
+            SeleniumConfig = new SeleniumConfig
+            {
+                Timeouts = new Timeouts
+                {
+                    ImplicitlyWait = 5,
+                    SetScriptTimeout = 10,
+                    SetPageLoadTimeout = 30
+                }
+            };
+        }
+    }
+}
