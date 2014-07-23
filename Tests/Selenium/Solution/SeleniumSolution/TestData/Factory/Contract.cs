51067d1fb365576f0074c123c2064e018273cb76
Marc Siegmund <TKCDEV\siegmund>
2014-07-23 11:49:02 +0000

    created contract factory
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52426

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/Contract.cs
@@ -0,0 +1,19 @@
+﻿namespace Six.Scs.QA.TestData.Factory
+{
+    public class Contract
+    {
+        public static ValueObjects.Brand.Contract Create()
+        {
+            return new ValueObjects.Brand.Contract
+            {
+                Acquirer =
+                {
+                    Id = "",
+                    Status = true,
+                    NetAdress = "syr"
+                },
+                Brands = Brand.SixBrands()
+            };
+        }
+    }
+}
