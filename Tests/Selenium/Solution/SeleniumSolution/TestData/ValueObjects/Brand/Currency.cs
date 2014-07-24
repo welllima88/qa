db9e6470b7bfcdc8294fb1e53ec42b8cd9524be5
Marc Siegmund <TKCDEV\siegmund>
2014-07-24 09:38:30 +0000

    added currency object
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52449

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/ValueObjects/Brand/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace Six.Scs.QA.TestData.ValueObjects.Brand
+{
+    public class Currency
+    {
+        // http://de.wikipedia.org/wiki/ISO_4217
+        public string IsoCode;
+        public string Name;
+        public string Number;
+    }
+}
