d6a0510ddaf1ada2c91ad3ea4538d413b83c53e0
Marc Siegmund <TKCDEV\siegmund>
2014-07-25 13:21:06 +0000

    added methods to workflow for selecting the brands when create
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52495

--- Tests/Selenium/Solution/SeleniumSolution/Workflow/Brand.cs
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/Brand.cs
@@ -1,15 +1,44 @@
-using Six.Scs.QA.Selenium.Terminal.Dashboard.Brand;
+using System.Collections.Generic;
+using Six.Scs.QA.TestData.ValueObjects.Brand;
 
 namespace Six.Scs.QA.Workflow
 {
     public class Brand
     {
-        public static void Select(Acquirer acquirerBrands)
+        /// <summary>
+        ///     Selects all brands in a the given List
+        /// </summary>
+        /// <param name="brands"></param>
+        public static void Select(IEnumerable<TestData.ValueObjects.Brand.Brand> brands)
+        {
+            foreach (TestData.ValueObjects.Brand.Brand brand in brands)
+            {
+            }
+        }
+
+        /// <summary>
+        ///     Selects the acquirer with all its brands
+        /// </summary>
+        /// <param name="acquirer"></param>
+        public static void Select(Acquirer acquirer)
         {
         }
 
-        public static void Create(Acquirer acquirerBrands)
+        /// <summary>
+        ///     Selects all brands of all given contracts with its brands
+        /// </summary>
+        /// <param name="contracts"></param>
+        public static void Select(IEnumerable<Contract> contracts)
         {
+            foreach (Contract contract in contracts)
+            {
+                Select(contract.Brands);
+            }
+        }
+
+        public static void Create(Contract contract)
+        {
+            // BrandCreate.
         }
     }
 }
