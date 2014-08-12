ab8384a3fa0d4bcab5eab421bc423d1f0c327664
Marc Siegmund <TKCDEV\siegmund>
2014-08-12 09:01:34 +0000

    infotext test logic completed
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52719

--- Tests/Selenium/Solution/SeleniumSolution/TestFlow/Infotext.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Infotext.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Six.Scs.QA.Selenium.Common.Infotext;
 using Six.Scs.QA.Selenium.Common.Menu;
+using Six.Scs.QA.Selenium.Terminal.Dashboard.Portlets;
 using Six.Scs.QA.Workflow;
 
 namespace Six.Scs.QA.Testlogic
@@ -10,27 +11,28 @@ namespace Six.Scs.QA.Testlogic
         public static void Create(TestData.ValueObjects.Customer customer)
         {
             Customer.Open(customer);
-
+            CustomerMenu.Infotexts.Click();
             CreateAndSaveInfotext("Customer");
         }
 
         public static void Create(TestData.ValueObjects.Location location)
         {
             Location.Open(location);
+            LocationMenu.Infotexts.Click();
             CreateAndSaveInfotext("Location");
         }
 
         public static void Create(TestData.ValueObjects.Terminal terminal)
         {
             Terminal.Open(terminal);
+            TerminalTexts.Infotexts.Click();
             CreateAndSaveInfotext("Terminal");
         }
 
         private static void CreateAndSaveInfotext(string parameter)
         {
-            CustomerMenu.Infotexts.Click();
+            
             InfoTextListView.CreateButton.Click();
-
             string infotext = InfoText.Create(parameter);
             Check(infotext);
         }
