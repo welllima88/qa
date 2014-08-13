5d15e40b05c0811b34a71971935d0816e83f8a96
Marc Siegmund <TKCDEV\siegmund>
2014-08-13 09:48:51 +0000

    added test object for the terminal quit view
    git-tfs-id: [http://mdzhwctfs01:8080/tfs/DefaultCollection]$/ScsDev/QA;C52746

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Terminal/Quit/TerminalQuit.cs
@@ -0,0 +1,28 @@
+﻿using OpenQA.Selenium;
+using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
+
+namespace Six.Scs.QA.Selenium.Terminal.Quit
+{
+    public class TerminalQuit : WebObject
+    {
+        public static string State
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("StateAndSubState")).Selector().SelectByText(value); }
+        }
+
+        public static string Delivery
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("SelectedDeliveryType")).Selector().SelectByText(value); }
+        }
+
+        public static string Reason
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("Motivation")).TextField().TypeText(value); }
+        }
+
+        public static IWebElementAdapter Save
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("save")); }
+        }
+    }
+}
