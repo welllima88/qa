c1f6017043e5e863f2f2afa1084693ca65564798
Marc Siegmund <marc.siegmund@six-group.com>
2014-09-03 16:09:21 +0200

    added test object for entering data when terminal replace

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Terminal/Replace.cs
@@ -0,0 +1,53 @@
+using OpenQA.Selenium;
+using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
+
+namespace Six.Scs.QA.Selenium.Terminal
+{
+    public class Replace : WebObject
+    {
+        public static string InstallationType
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("InstallTypeId")).Selector().SelectByText(value); }
+        }
+
+        public static string SalesContract
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("HandoverTypeId")).Selector().SelectByText(value); }
+        }
+
+        public static string SupportContract
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("SupportContractId")).Selector().SelectByText(value); }
+        }
+
+        public static string Contactless
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("ContactlessId")).Selector().SelectByText(value); }
+        }
+
+        public static string InstallationRemark
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("InstallTypeRemark")).TextArea().TypeText(value); }
+        }
+
+        public static string Infotext
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("InfoText")).TextArea().TypeText(value); }
+        }
+
+        public static IWebElementAdapter SaveButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("save")); }
+        }
+
+        public static string DeliveryDate
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("deliveryDateId")).TextField().TypeText(value); }
+        }
+
+        public static bool SendNotifiaction
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("SendToMultipay")).CheckBox().Set(value); }
+        }
+    }
+}
