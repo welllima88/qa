a75ea9d03d28504d90bb1a83ba02fac5cfcfadb3
Marc Siegmund <marc.siegmund@six-group.com>
2014-09-04 15:43:04 +0200

    added TestData object and factory

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/Ticket.cs
@@ -0,0 +1,25 @@
+using Six.Scs.QA.TestData.ValueObjects;
+
+namespace Six.Scs.QA.TestData.Factory
+{
+    public class Ticket
+    {
+        public static TroubleTicket TroubleTicket()
+        {
+            return new TroubleTicket
+            {
+                MyTeam = "Kundendienst",
+                Forward = "Werkstatt",
+                Contact = "SYR",
+                AvailableTime = "",
+                Phone = "+31 - 58 399 6237",
+                Fax = "+33 - 58 399 6237",
+                Email = "marc.siegmund@six-group.proof",
+                Mobile = "+32 - 58 399 6237",
+                Category = "",
+                Solution = "",
+                Description = "SYR Trouble Ticket" + Factory.GenerateTestId();
+            };
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/ValueObjects/TroubleTicket.cs
@@ -0,0 +1,17 @@
+namespace Six.Scs.QA.TestData.ValueObjects
+{
+    public class TroubleTicket
+    {
+        public string MyTeam { get; set; }
+        public string Forward { get; set; }
+        public string Contact { get; set; }
+        public string AvailableTime { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Category { get; set; }
+        public string Solution { get; set; }
+        public string Description { get; set; }
+    }
+}
