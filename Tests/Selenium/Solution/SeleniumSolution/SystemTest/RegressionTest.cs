57d238e20814ebfaa0d7f03a5f7d1c37df998658
Marc Siegmund <marc.siegmund@six-group.com>
2014-09-04 15:55:10 +0200

    added create trouble ticket test to regression test

--- Tests/Selenium/Solution/SeleniumSolution/SystemTest/RegressionTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/RegressionTest.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using NUnit.Framework;
 using Six.Scs.QA.Testlogic;
+using Six.Scs.QA.Testlogic.Tickets;
 using Contact = Six.Scs.QA.Testlogic.Contact;
 using SimCard = Six.Scs.QA.TestData.ValueObjects.SimCard;
 
@@ -34,13 +36,14 @@ namespace Six.Scs.QA.Selenium.SystemTest
             _customer = Testlogic.Customer.Edit(_customer);
 
             Brands.Create(_terminalCustomer.Id);
-            
+
             Infotext.Create(_customer);
-            
+
             _mpd = Testlogic.Mpd.Create(_customer);
             Testlogic.Terminal.Assign(_mpd, _terminalCustomer);
-            
+
             _terminalLocation = Testlogic.Terminal.Create(_location);
+            TroubleTicket.Create(_terminalLocation);
             Infotext.Create(_location);
 
             _location = Testlogic.Location.Edit(_location);
@@ -59,13 +62,13 @@ namespace Six.Scs.QA.Selenium.SystemTest
 
             _duplicatedTerminals = Testlogic.Terminal.Duplicate(_terminalLocation);
             Testlogic.User.Create(_personOnLocation);
-            
+
             Testlogic.User.AddService(_user);
 
             Brands.Create(_duplicatedTerminals[1]); // {0,1,..} means create brands on second terminal
 
             Testlogic.Terminal.Replace(_terminalLocation);
-            
+
             _sim = Testlogic.SimCard.Create();
             Testlogic.SimCard.Edit(_sim);
         }
