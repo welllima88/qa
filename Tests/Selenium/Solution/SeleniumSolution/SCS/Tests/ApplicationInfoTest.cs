6ca8d0d15832a0573c4191c559c4408be3946edc
Marc Siegmund <marc.siegmund@six-group.com>
2014-09-12 13:42:09 +0200

    added basic smoketest for old scs

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS/Tests/ApplicationInfoTest.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Six.Scs.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Old.Common;
+
+namespace Six.Scs.QA.Selenium.Old.Tests
+{
+    /// <summary>
+    ///     be careful with menu expander because they prevent some actions and need special handling
+    /// </summary>
+    [TestFixture]
+    public class ApplicationInfoTest
+    {
+        [TestFixtureSetUp]
+        public static void ClassInit()
+        {
+            TestDirector.Navigate();
+        }
+
+        [Test]
+        public void CompanyLogo()
+        {
+            Assert.IsTrue(HeadMenu.CompanyLogo.Displayed);
+        }
+
+        [Test]
+        public void System()
+        {
+            Assert.IsTrue(HeadMenu.System.Displayed);
+        }
+    }
+}
