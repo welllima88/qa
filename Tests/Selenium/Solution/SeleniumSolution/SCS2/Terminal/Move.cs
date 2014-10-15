4448f75b074826a50b78628f9f03e2827a0fea29
Marc Siegmund <marc.siegmund@six-group.com>
2014-10-15 13:33:17 +0200

    added test object for terminal Move

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Terminal/Move.cs
@@ -0,0 +1,23 @@
+using OpenQA.Selenium;
+using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
+
+namespace Six.Scs.QA.Selenium.Terminal
+{
+    public class Move : WebObject
+    {
+        public static bool UpdateReceipt
+        {
+            set { WebDriver.FindAdaptedElement(By.Id("TakeAddressFromNewLocation")).CheckBox().Set(value); }
+        }
+
+        public static IWebElementAdapter SaveButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("save")); }
+        }
+
+        public static void SelectLocation(string companyName)
+        {
+            WebDriver.FindAdaptedElement(By.Id("LocationId")).Selector().SelectByText(companyName);
+        }
+    }
+}
