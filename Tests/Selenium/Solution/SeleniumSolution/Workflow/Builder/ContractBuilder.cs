fec06f58cc66b37ebfdbe385e272de6ebf72455a
Marc Siegmund <marc.siegmund@six-group.com>
2014-11-13 15:49:19 +0100

    added abstract ContractBuilder with default create logic when

---
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/Builder/ContractBuilder.cs
@@ -0,0 +1,37 @@
+using System;
+using Six.Scs.QA.Selenium.Brand;
+using Six.Scs.QA.TestData.ValueObjects.Brand;
+
+namespace Six.Scs.QA.Workflow.Builder
+{
+    public abstract class ContractBuilder : IBuilder
+    {
+        public Contract Contract { get; protected set; }
+
+        public void Create()
+        {
+            ChooseBrands();
+            ConfirmSelection();
+            SetBrandDetails();
+            ClickCreate();
+        }
+
+        public abstract void Edit();
+
+        public abstract void Check();
+
+        private void ConfirmSelection()
+        {
+            Selection.ConfirmButton().Click();
+        }
+
+        private void ClickCreate()
+        {
+            AcquirerCreate.SaveAndCreate();
+        }
+
+        protected abstract void SetBrandDetails();
+
+        protected abstract void ChooseBrands();
+    }
+}
