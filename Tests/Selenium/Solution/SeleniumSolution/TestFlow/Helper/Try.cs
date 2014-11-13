f08767bcd0e3ea848af3de0ac8ebdef9898080ea
Marc Siegmund <marc.siegmund@six-group.com>
2014-11-13 13:29:36 +0100

    added test helper for easy way of retrying anything

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Helper/Try.cs
@@ -0,0 +1,35 @@
+namespace Six.Scs.QA.Testlogic.Helper
+{
+    public class Try
+    {
+        private int _maxNumberOfTries;
+
+        /// <summary>
+        ///     Initiates a helper which counts the numbers of tries
+        /// </summary>
+        /// <param name="maxNumberOfTries">how often a retry can be done</param>
+        public Try(int maxNumberOfTries)
+        {
+            _maxNumberOfTries = maxNumberOfTries;
+        }
+
+        /// <summary>
+        /// This method is used after again() has returned false to check, if the limit has exceeded
+        /// </summary>
+        /// <returns>returns true if numbers of retries exceeded else false</returns>
+        public bool TooOften()
+        {
+            return _maxNumberOfTries <= 0;
+        }
+
+        /// <summary>
+        ///     Call of this method repeats a step if necessary and decreases counter
+        /// </summary>
+        /// <returns></returns>
+        public bool Again()
+        {
+            _maxNumberOfTries--;
+            return _maxNumberOfTries > 0;
+        }
+    }
+}
