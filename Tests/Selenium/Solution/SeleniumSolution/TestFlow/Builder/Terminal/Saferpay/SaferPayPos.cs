22b0d40273dba60530787debb6a9990c03ab2ae7
Marc Siegmund <marc.siegmund@six-group.com>
2014-11-18 10:51:39 +0100

    added terminal builder to create a SaferPayPos terminal which uses also the SIX brands via GICC

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Builder/Terminal/Saferpay/SaferPayPos.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Six.Scs.QA.Selenium.Terminal;
+using Six.Scs.QA.TestData.Factory;
+using Six.Scs.QA.Workflow.Builder;
+
+namespace Six.Scs.QA.Testlogic.Builder.Terminal.Saferpay
+{
+    public class SaferPayPos : TerminalBuilder
+    {
+        public override void Edit()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Check()
+        {
+            throw new NotImplementedException();
+        }
+
+        protected override void ChooseArticle()
+        {
+            ArticleChooser.Filter = "Saferpay";
+            ArticleChooser.Select = "Saferpay POS Terminal";
+        }
+
+        protected override void SetBasics()
+        {
+            ConfigCreate.Infotext = "E-Link" + Factory.GenerateTestId();
+        }
+
+        protected override void SetDetails()
+        {
+            // nothing
+        }
+    }
+}
