48c4650ebd9ee36740b6fa541689549f1beeb96c
Marc Siegmund <marc.siegmund@six-group.com>
2014-11-19 14:03:07 +0100

    intialized Terminal object and added some asserts removed comment

--- Tests/Selenium/Solution/SeleniumSolution/TestFlow/Builder/Terminal/Saferpay/SaferPayPos.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Builder/Terminal/Saferpay/SaferPayPos.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using NUnit.Framework;
 using Six.Scs.QA.Selenium.Terminal;
+using Six.Scs.QA.Selenium.Terminal.Dashboard.Portlets;
 using Six.Scs.QA.TestData.Factory;
 using Six.Scs.QA.Workflow.Builder;
 
@@ -7,6 +9,11 @@ namespace Six.Scs.QA.Testlogic.Builder.Terminal.Saferpay
 {
     public class SaferPayPos : TerminalBuilder
     {
+        public SaferPayPos()
+        {
+            Terminal = new TestData.ValueObjects.Terminal {Type = "Saferpay POS Terminal"};
+        }
+
         public override void Edit()
         {
             throw new NotImplementedException();
@@ -14,13 +21,13 @@ namespace Six.Scs.QA.Testlogic.Builder.Terminal.Saferpay
 
         public override void Check()
         {
-            throw new NotImplementedException();
+            Assert.AreEqual(Terminal.Type, BusinessViewpoint.TerminalType);
         }
 
         protected override void ChooseArticle()
         {
             ArticleChooser.Filter = "Saferpay";
-            ArticleChooser.Select = "Saferpay POS Terminal";
+            ArticleChooser.Select = Terminal.Type;
         }
 
         protected override void SetBasics()
@@ -30,7 +37,6 @@ namespace Six.Scs.QA.Testlogic.Builder.Terminal.Saferpay
 
         protected override void SetDetails()
         {
-            // nothing
         }
     }
 }
