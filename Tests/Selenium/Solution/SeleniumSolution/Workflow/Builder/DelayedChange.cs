d85f0d2b4992ad45407e7807f6d00ca64e27281a
Marc Siegmund <marc.siegmund@six-group.com>
2014-12-09 13:48:36 +0100

    added DelayedChanger, clean

--- Tests/Selenium/Solution/SeleniumSolution/Workflow/Builder/DelayedChange.cs
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/Builder/DelayedChange.cs
@@ -1,14 +1,16 @@
+using System;
 using Six.Scs.QA.Selenium.Common;
 using Six.Scs.QA.TestData.Factory;
 
 namespace Six.Scs.QA.Workflow.Builder
 {
-    public class DefaultChange : IPerform
+    public class DelayedChange : IPerform
     {
         public void Do()
         {
             ChangeForm.Reason = "Daten Korrektur (ohne Versand)";
-            ChangeForm.Remark = "SYR " + Factory.GenerateTestId() + " automated change remark";            
+            ChangeForm.Remark = "SYR " + Factory.GenerateTestId() + " automated delayed change remark";
+            ChangeForm.DelayTime(new TimeSpan().Add(TimeSpan.FromMinutes(2)));
         }
 
         public void Check()
