2d1fea5206380075a334d8cf376a0f01693c14b0
Marc Siegmund <marc.siegmund@six-group.com>
2014-12-09 17:06:26 +0100

    updated locators

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Location/BillingAddress/View.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Location/BillingAddress/View.cs
@@ -8,72 +8,72 @@ namespace Six.Scs.QA.Selenium.Location.BillingAddress
     {
         public static string CompanyName
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_LocationName")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("BillingName")).Text; }
         }
 
         public static string StreetName
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Street")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("StreetNo")).Text; }
         }
 
         public static string Zip
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_ZIP")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Zip")).Text; }
         }
 
         public static string City
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_City")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("City")).Text; }
         }
 
         public static string Language
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_LanguageId")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Language")).Text; }
         }
 
         public static string Country
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_CountryId")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Country")).Text; }
         }
 
         public static string Po
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_PO")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Po")).Text; }
         }
 
         public static string AdressAddition
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_AddressAddition")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("AddressAddition")).Text; }
         }
 
         public static string Email
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Email")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Email")).Text; }
         }
 
         public static string Web
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Web")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Web")).Text; }
         }
 
         public static string Telephone
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Telephone")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Telephone")).Text; }
         }
 
         public static string Mobile
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Mobile")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Mobile")).Text; }
         }
 
         public static string Fax
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("LocationData_Fax")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("Fax")).Text; }
         }
 
         public static IWebElementAdapter EditButton
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("ctl00_bodyContentPlaceHolder_btnEdit")); }
+            get { return WebDriver.FindAdaptedElement(By.Id("edit")); }
         }
 
         public static string SbsDebitNumber
