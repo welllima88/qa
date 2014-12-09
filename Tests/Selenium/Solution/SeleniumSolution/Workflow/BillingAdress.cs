bd6653fe248489786044473b7e5ebcbba9b0c61c
Marc Siegmund <marc.siegmund@six-group.com>
2014-12-09 16:37:01 +0100

    changed parameters to concrete type and renamed variables

--- Tests/Selenium/Solution/SeleniumSolution/Workflow/BillingAdress.cs
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/BillingAdress.cs
@@ -5,57 +5,57 @@ namespace Six.Scs.QA.Workflow
 {
     public static class BillingAdress
     {
-        public static void Create(TestData.ValueObjects.Location l)
+        public static void Create(TestData.ValueObjects.BillingAddress b)
         {
             CustomerMenu.BillingAdressCreate.Click();
 
-            Selenium.Location.BillingAddress.Create.CompanyName = l.CompanyName;
-            Selenium.Location.BillingAddress.Create.StreetNo = l.Adress.StreetNo;
-            Selenium.Location.BillingAddress.Create.Zip = l.Adress.Zip;
-            Selenium.Location.BillingAddress.Create.City = l.Adress.City;
-            Selenium.Location.BillingAddress.Create.Po = l.Adress.Po;
-            Selenium.Location.BillingAddress.Create.AdressAddition = l.Adress.AdressAddition; // ??
-            Selenium.Location.BillingAddress.Create.Region = l.Adress.Region;
+            Selenium.Location.BillingAddress.Create.CompanyName = b.CompanyName;
+            Selenium.Location.BillingAddress.Create.StreetNo = b.Adress.StreetNo;
+            Selenium.Location.BillingAddress.Create.Zip = b.Adress.Zip;
+            Selenium.Location.BillingAddress.Create.City = b.Adress.City;
+            Selenium.Location.BillingAddress.Create.Po = b.Adress.Po;
+            Selenium.Location.BillingAddress.Create.AdressAddition = b.Adress.AdressAddition; // ??
+            Selenium.Location.BillingAddress.Create.Region = b.Adress.Region;
 
-            Selenium.Location.BillingAddress.Create.Language = l.Contact.Language;
-            Selenium.Location.BillingAddress.Create.Country = l.Adress.Country;
-            Selenium.Location.BillingAddress.Create.Email = l.Contact.Email;
-            Selenium.Location.BillingAddress.Create.Telephone = l.Contact.Telephone;
-            Selenium.Location.BillingAddress.Create.Mobile = l.Contact.Mobile;
-            Selenium.Location.BillingAddress.Create.Fax = l.Contact.Fax;
-            Selenium.Location.BillingAddress.Create.Web = l.Contact.Web;
+            Selenium.Location.BillingAddress.Create.Language = b.Contact.Language;
+            Selenium.Location.BillingAddress.Create.Country = b.Adress.Country;
+            Selenium.Location.BillingAddress.Create.Email = b.Contact.Email;
+            Selenium.Location.BillingAddress.Create.Telephone = b.Contact.Telephone;
+            Selenium.Location.BillingAddress.Create.Mobile = b.Contact.Mobile;
+            Selenium.Location.BillingAddress.Create.Fax = b.Contact.Fax;
+            Selenium.Location.BillingAddress.Create.Web = b.Contact.Web;
 
             Selenium.Location.BillingAddress.Create.SaveButton.Click();
 
-            l.SbsDebitNumber = View.SbsDebitNumber;
+            b.SbsDebitNumber = View.SbsDebitNumber;
         }
 
         /// <summary>
         ///     Edits billing address
         /// </summary>
-        /// <param name="l">given location data</param>
-        public static void Edit(TestData.ValueObjects.Location l)
+        /// <param name="b">given location data</param>
+        public static void Edit(TestData.ValueObjects.BillingAddress b)
         {
-            l.SbsDebitNumber = View.SbsDebitNumber;
+            b.SbsDebitNumber = View.SbsDebitNumber;
 
             View.EditButton.Click();
 
-            Selenium.Location.BillingAddress.Edit.CompanyName = l.CompanyName;
-            Selenium.Location.BillingAddress.Edit.StreetNo = l.Adress.StreetNo;
-            Selenium.Location.BillingAddress.Edit.Zip = l.Adress.Zip;
-            Selenium.Location.BillingAddress.Edit.City = l.Adress.City;
-            Selenium.Location.BillingAddress.Edit.Po = l.Adress.Po;
-            Selenium.Location.BillingAddress.Edit.Region = l.Adress.Region;
-            Selenium.Location.BillingAddress.Edit.AdressAddition = l.Adress.AdressAddition; //
+            Selenium.Location.BillingAddress.Edit.CompanyName = b.CompanyName;
+            Selenium.Location.BillingAddress.Edit.StreetNo = b.Adress.StreetNo;
+            Selenium.Location.BillingAddress.Edit.Zip = b.Adress.Zip;
+            Selenium.Location.BillingAddress.Edit.City = b.Adress.City;
+            Selenium.Location.BillingAddress.Edit.Po = b.Adress.Po;
+            Selenium.Location.BillingAddress.Edit.Region = b.Adress.Region;
+            Selenium.Location.BillingAddress.Edit.AdressAddition = b.Adress.AdressAddition; //
 
-            Selenium.Location.BillingAddress.Edit.Email = l.Contact.Email;
-            Selenium.Location.BillingAddress.Edit.Telephone = l.Contact.Telephone;
-            Selenium.Location.BillingAddress.Edit.Mobile = l.Contact.Mobile;
-            Selenium.Location.BillingAddress.Edit.Fax = l.Contact.Fax;
-            Selenium.Location.BillingAddress.Edit.Web = l.Contact.Web;
+            Selenium.Location.BillingAddress.Edit.Email = b.Contact.Email;
+            Selenium.Location.BillingAddress.Edit.Telephone = b.Contact.Telephone;
+            Selenium.Location.BillingAddress.Edit.Mobile = b.Contact.Mobile;
+            Selenium.Location.BillingAddress.Edit.Fax = b.Contact.Fax;
+            Selenium.Location.BillingAddress.Edit.Web = b.Contact.Web;
 
-            Selenium.Location.BillingAddress.Edit.Language = l.Contact.Language;
-            Selenium.Location.BillingAddress.Edit.Country = l.Adress.Country;
+            Selenium.Location.BillingAddress.Edit.Language = b.Contact.Language;
+            Selenium.Location.BillingAddress.Edit.Country = b.Adress.Country;
 
             Selenium.Location.BillingAddress.Edit.SaveButton.Click();
         }
