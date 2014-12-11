85a5efbc62745827b72ae3887c3bc4ec3b33f0a5
Marc Siegmund <marc.siegmund@six-group.com>
2014-12-11 17:35:49 +0100

    updated locator
    added method for open first element (also there tricky locator)

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Search/Result/SearchResult.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Search/Result/SearchResult.cs
@@ -1,13 +1,21 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Support.UI;
 using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
 
-namespace Six.Scs.QA.Selenium.Search.Result
+namespace Six.Scs.QA.Selenium.Search
 {
     public class SearchResult : WebObject
     {
+        private readonly string _section;
+
+        public SearchResult(string section)
+        {
+            _section = section;
+        }
+
         /// <summary>
         ///     It is recommended to set the prefix property to the appropiate container element
         /// </summary>
@@ -16,28 +24,31 @@ namespace Six.Scs.QA.Selenium.Search.Result
             get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content h1")).Text; }
         }
 
-        public static TextFieldElement FilterTextField(SearchDivLocator searchDivLocator)
+        public TextFieldElement FilterTextField()
         {
             return WebDriver.FindAdaptedElement(By.CssSelector("span input[id$='Filter']")).TextField();
         }
 
-        public static IWebElementAdapter LoadMoreLink(SearchDivLocator searchDivLocator)
+        public IWebElementAdapter LoadMoreLink()
         {
             return WebDriver.FindAdaptedElement(By.CssSelector(" span input[id^='loadMore']"));
         }
 
-        public static IWebElementAdapter First(SearchDivLocator searchDivLocator)
-        {
-            return Result(searchDivLocator).FirstOrDefault(d => d.Displayed);
-        }
-
-        private static IEnumerable<IWebElementAdapter> Result(SearchDivLocator searchDivLocator)
+        public IWebElementAdapter First()
         {
             WaitForSearchHasFinished();
+            string cssb =
+                String.Format("div[ng-app='searchApp']>div[ng-controller='{0}']>div>table>tbody>tr>td>a strong",
+                //String.Format("div[ng-app='searchApp']>div[ng-controller='{0}']>div>table>tbody>tr>td:nth-child(3)>a",
+                    _section);
+            return WebDriver.FindAdaptedElement(By.CssSelector(cssb));
+        }
 
-            string cssb = "div#" + searchDivLocator.Section + " tbody#" + searchDivLocator.ResultId + " tr td a[href*='" +
-                          searchDivLocator.LinkPart + "']";
-            return WebDriver.FindAdaptedElements(By.CssSelector(cssb));
+        public IEnumerable<IWebElementAdapter> Result()
+        {
+            WaitForSearchHasFinished();
+            string cssb = String.Format("div[ng-app='searchApp']>div[ng-controller='{0}']>div>table>tbody>tr", _section);
+            return WebDriver.FindAdaptedElements(By.CssSelector(cssb)).Where(r => r.Displayed);
         }
 
         private static void WaitForSearchHasFinished()
