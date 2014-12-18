2c8dbcf302b48365a95dde4c4bf7bd4fb924ed02
Marc Siegmund <marc.siegmund@six-group.com>
2014-12-18 13:16:17 +0100

    renamed

--- Tests/EP2/Solution/COI/Coi/CoiTest.cs
+++ Tests/EP2/Solution/COI/Coi/CoiTest.cs
@@ -1,13 +1,16 @@
-﻿using SIX.EP2.Client;
+﻿using NUnit.Framework;
+using SIX.EP2.Client;
 using SIX.EP2.Core.Comm;
 using SIX.EP2.Core.MessageHandling;
 using SIX.EP2.Core.Protocol;
 
 namespace SIX.SCS.QA.Tests.EP2.Coi
 {
-    internal class Program
+    [TestFixture]
+    public class CoiTest
     {
-        private static void Main(string[] args)
+        [Test]
+        public void SendMessage()
         {
             IMessageVersionMapper messageMapper =
                 MessageVersionMapper.Builder.AddFromAssemblyOfType<ConfigRequest>().Build();
