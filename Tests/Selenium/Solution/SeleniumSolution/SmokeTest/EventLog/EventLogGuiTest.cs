3b01413c32f125914463d8907c4307f96aa8fe88
Marc Siegmund <marc.siegmund@six-group.com>
2015-01-13 10:41:52 +0100

    renamed

--- Tests/Selenium/Solution/SeleniumSolution/SmokeTest/EventLog/EventLogGuiTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SmokeTest/EventLog/EventLogGuiTest.cs
@@ -18,28 +18,28 @@ namespace Six.Scs.QA.Selenium.SmokeTest.EventLog
         public void EventList()
         {
             Assert.IsNotNull(View.EventList, "at least Table with events displayed");
-            // CollectionAssert.Contains(TerminalEventLog.EventList, "at least List with");
+            // CollectionAssert.Contains(View.EventList, "at least List with");
         }
 
         [Test]
         public void EventTable()
         {
             Assert.IsTrue(View.EventTable.Displayed, "at least Table with events displayed");
-            // CollectionAssert.Contains(TerminalEventLog.EventList, "at least List with");
+            // CollectionAssert.Contains(View.EventList, "at least List with");
         }
 
         [Test]
         public void EventTypes()
         {
             Assert.IsNotNull(View.EventTypes);
-            // Assert.IsTrue(TerminalEventLog.EventTypes.Options.Contains("1000 - Startup"));
+            // Assert.IsTrue(View.EventTypes.Options.Contains("1000 - Startup"));
         }
 
         [Test]
         public void Export()
         {
             Assert.IsTrue(View.Export.Displayed, "number of time span options");
-            // CollectionAssert.Contains(TerminalEventLog.TimeSpan.Options.Contains("24h"));
+            // CollectionAssert.Contains(View.TimeSpan.Options.Contains("24h"));
         }
 
         [Test]
@@ -52,7 +52,7 @@ namespace Six.Scs.QA.Selenium.SmokeTest.EventLog
         public void TimeSpan()
         {
             Assert.IsTrue(View.TimeSpan.Options.Count() == 5, "number of time span options");
-            // CollectionAssert.Contains(TerminalEventLog.TimeSpan.Options.Contains("24h"));
+            // CollectionAssert.Contains(View.TimeSpan.Options.Contains("24h"));
         }
     }
 }
