0fede28e100ab27691d4cea87485e6c5f7c5b8ac
Marc Siegmund <marc.siegmund@six-group.com>
2015-01-22 17:06:50 +0100

    removed scs old automation, because not used and not planned to use in future anymore

--- Tests/Selenium/Solution/SeleniumSolution/SCS/MainObject.cs
+++
@@ -1,9 +0,0 @@
-using Six.Scs.QA.Selenium.Extension.WebDriver;
-
-namespace Six.Scs.QA.Selenium.Old
-{
-    public class MainObject
-    {
-        public static IWebDriverAdapter WebDriver;
-    }
-}
