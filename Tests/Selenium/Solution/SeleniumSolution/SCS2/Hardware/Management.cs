ffda54561d715509de751985ecd4b2ddec8269a2
Marc Siegmund <marc.siegmund@six-group.com>
2015-01-27 16:23:38 +0100

    added test objects for hardware Management

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Hardware/Management.cs
@@ -0,0 +1,18 @@
+using OpenQA.Selenium;
+using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
+
+namespace Six.Scs.QA.Selenium.Hardware
+{
+    public class Management : WebObject
+    {
+        public static ButtonElement CheckButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("check")).Button(); }
+        }
+
+        public static TextFieldElement SerialNumber
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("serialNumber")).TextField(); }
+        }
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Hardware/SerialNumberImport.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
+
+namespace Six.Scs.QA.Selenium.Hardware
+{
+    public class SerialNumberImport : WebObject
+    {
+        public static string Title
+        {
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(By.CssSelector("td#content>h1"))
+                        .Text;
+            }
+        }
+
+        public static string Description
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content div>div>p")).Text; }
+        }
+
+        public static IWebElementAdapter FileChooser
+        {
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("div span input[type='file']")); }
+        }
+
+        public static IWebElementAdapter FileUploadButton
+        {
+            get { return WebDriver.FindAdaptedElement(By.Id("upload")); }
+        }
+
+        public static string DownloadTemplate
+        {
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(By.CssSelector("a[href$='/HWManagementImportSerialNumber/StreamFile']"))
+                        .Text;
+            }
+        }
+    }
+}
