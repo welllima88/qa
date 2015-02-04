59c8418d1c2a810c4420d587869507c094753127
Marc Siegmund <marc.siegmund@six-group.com>
2015-02-04 17:11:46 +0100

    instead of direct open use the last element

--- Tests/Selenium/Solution/SeleniumSolution/TestFlow/Administration/Agency.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Administration/Agency.cs
@@ -10,18 +10,24 @@ namespace Six.Scs.QA.Testlogic.Administration
         {
             TestData.ValueObjects.Agency agency = TestData.Factory.Agency.Default();
             Workflow.Agency.Create(agency);
-
+            
             Check(agency);
+            Workflow.Lobby.OpenLatestElement();
+            Check(agency);
+            
             return agency;
         }
 
         public static TestData.ValueObjects.Agency Edit(TestData.ValueObjects.Agency agency)
         {
-            Open(agency);
+            Workflow.Lobby.OpenLatestElement();
             agency = TestData.Factory.Agency.Edit();
-            Workflow.Agency.Create(agency);
+            Workflow.Agency.Edit(agency);
 
             Check(agency);
+            Workflow.Lobby.OpenLatestElement();
+            Check(agency);
+
             return agency;
         }
 
@@ -36,7 +42,7 @@ namespace Six.Scs.QA.Testlogic.Administration
             Assert.That(View.Name, Is.EqualTo(agency.Name));
             Assert.That(View.DisplayName, Is.EqualTo(agency.DisplayName));
             Assert.That(View.City, Is.EqualTo(agency.Adress.City));
-            Assert.That(View.Country, Is.EqualTo(agency.Adress.Country));
+            Assert.That(agency.Adress.Country, Is.StringMatching(View.Country)); // "Schweiz" vs "Schweiz [CH]"
             Assert.That(View.Telephone, Is.EqualTo(agency.Contact.Telephone));
             Assert.That(View.Language, Is.EqualTo(agency.Contact.Language));
             Assert.That(View.SbsAgentId, Is.EqualTo(agency.SbsAgentId));
@@ -45,10 +51,12 @@ namespace Six.Scs.QA.Testlogic.Administration
 
         public static void Deactivate(TestData.ValueObjects.Agency agency)
         {
-            Open(agency);
+            Workflow.Lobby.OpenLatestElement();
             Workflow.Agency.Deactivate();
             agency.Status = false;
 
+            Check(agency);
+            Workflow.Lobby.OpenLatestElement();
             Check(agency);
         }
     }
