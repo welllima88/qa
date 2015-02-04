5d55abe04a4954789853c041e5a91adcce066282
Marc Siegmund <marc.siegmund@six-group.com>
2015-02-04 16:35:45 +0100

    added Workflow for Agency

---
+++ Tests/Selenium/Solution/SeleniumSolution/Workflow/Agency.cs
@@ -0,0 +1,50 @@
+﻿namespace Six.Scs.QA.Workflow
+{
+    public static class Agency
+
+    {
+        public static void Create(TestData.ValueObjects.Agency agency)
+        {
+            Selenium.Administration.Agency.List.Create.Click();
+
+            Selenium.Administration.Agency.Create.Name = agency.Name;
+            Selenium.Administration.Agency.Create.DisplayName = agency.DisplayName;
+            Selenium.Administration.Agency.Create.StreetNo = agency.Adress.StreetNo;
+            Selenium.Administration.Agency.Create.Zip = agency.Adress.Zip;
+            Selenium.Administration.Agency.Create.City = agency.Adress.City;
+            Selenium.Administration.Agency.Create.Country = agency.Adress.Country;
+            Selenium.Administration.Agency.Create.Language = agency.Contact.Language;
+            Selenium.Administration.Agency.Create.Telephone = agency.Contact.Telephone;
+            Selenium.Administration.Agency.Create.Supplier = agency.Supplier;
+            Selenium.Administration.Agency.Create.SbsAgentId = agency.SbsAgentId;
+            Selenium.Administration.Agency.Create.Status = agency.Status;
+
+            Selenium.Administration.Agency.Create.SaveButton.Click();
+        }
+
+        public static void Edit(TestData.ValueObjects.Agency agency)
+        {
+            Selenium.Administration.Agency.View.Edit();
+            Selenium.Administration.Agency.Create.DisplayName = agency.DisplayName;
+            Selenium.Administration.Agency.Create.DisplayName = agency.DisplayName;
+            Selenium.Administration.Agency.Create.StreetNo = agency.Adress.StreetNo;
+            Selenium.Administration.Agency.Create.Zip = agency.Adress.Zip;
+            Selenium.Administration.Agency.Create.City = agency.Adress.City;
+            Selenium.Administration.Agency.Create.Country = agency.Adress.Country;
+            Selenium.Administration.Agency.Create.Language = agency.Contact.Language;
+            Selenium.Administration.Agency.Create.Telephone = agency.Contact.Telephone;
+            Selenium.Administration.Agency.Create.Supplier = agency.Supplier;
+            Selenium.Administration.Agency.Create.SbsAgentId = agency.SbsAgentId;
+            Selenium.Administration.Agency.Create.Status = agency.Status;
+
+            Selenium.Administration.Agency.Create.SaveButton.Click();
+        }
+
+        public static void Deactivate()
+        {
+            Selenium.Administration.Agency.Create.Status = false;
+
+            Selenium.Administration.Agency.Create.SaveButton.Click();
+        }
+    }
+}
