e2f98cd1678d4bbd86019ce845da688c595399a3
Marc Siegmund <marc.siegmund@six-group.com>
2015-02-27 11:23:53 +0100

    changed to page object design

--- Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Common/MetaNavBar.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Common/MetaNavBar.cs
@@ -1,4 +1,3 @@
-using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
 using Six.Scs.QA.Selenium.Extension.WebDriver.WebElements;
 
@@ -6,15 +5,6 @@ namespace Six.Saferpay.QA.Selenium.Common
 {
     public class MetaNavBar
     {
-        [FindsBy
-        public static IWebElementAdapter LogOff
-        {
-            get { return WebDriver.FindAdaptedElement(By.CssSelector("li#text_logout")); }
-        }
-
-        public static IWebElementAdapter LogOff
-        {
-            get { return WebDriver.FindAdaptedElement(By.CssSelector("li#text_logout")); }
-        }
+        [FindsBy(How = How.CssSelector, Using = "li#text_logout a")] public static IWebElementAdapter LogOff;
     }
 }
