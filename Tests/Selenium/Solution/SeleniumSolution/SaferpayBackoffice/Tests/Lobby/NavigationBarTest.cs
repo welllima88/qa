e53c167a3191bfc8e7fada8ac6cca7e126c3bd0b
Marc Siegmund <marc.siegmund@six-group.com>
2015-03-05 16:59:30 +0100

    added basic test for Saferpay Backoffice

---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Lobby/NavigationBarTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Saferpay.QA.Selenium.Views.Common;
+
+namespace Six.Saferpay.QA.Selenium.Tests.Lobby
+{
+    /// <summary>
+    ///     be careful with menu expander because they prevent some actions and need special handling
+    /// </summary>
+    [TestFixture]
+    public class NavigationBarTest
+    {
+        [TestFixtureSetUp]
+        public static void ClassInit()
+        {
+            TestDirector.Navigate();
+        }
+
+        [Test]
+        [Category("LobbyCheck")]
+        public void MySaferpay()
+        {
+            Assert.That(NavigationBar.MySaferpay.Displayed);
+        }
+
+        [Test]
+        [Category("LobbyCheck")]
+        public void OnlineSupport()
+        {
+            Assert.That(NavigationBar.OnlineSupport.Displayed);
+        }
+    }
+}
