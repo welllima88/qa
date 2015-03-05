bb3ec93d9bab00c8aa8fc531a50c14969e9322ae
Marc Siegmund <marc.siegmund@six-group.com>
2015-03-05 18:08:13 +0100

    corrected members to IWebElement (not adapter)

--- Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Views/Common/NavigationBar.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Views/Common/NavigationBar.cs
@@ -1,12 +1,12 @@
+using OpenQA.Selenium;
 using OpenQA.Selenium.Support.PageObjects;
-using Six.QA.Selenium.Extension.WebDriver.WebElements;
 
 namespace Six.Saferpay.QA.Selenium.Views.Common
 {
     public class NavigationBar
     {
-        [FindsBy(How = How.CssSelector, Using = "li#topic_home a")] public static IWebElementAdapter MySaferpay;
+        [FindsBy(How = How.CssSelector, Using = "li#topic_home a")] public IWebElement MySaferpay;
 
-        [FindsBy(How = How.CssSelector, Using = "li#topic_support a")] public static IWebElementAdapter OnlineSupport;
+        [FindsBy(How = How.CssSelector, Using = "li#topic_support a")] public IWebElement OnlineSupport;
     }
 }
