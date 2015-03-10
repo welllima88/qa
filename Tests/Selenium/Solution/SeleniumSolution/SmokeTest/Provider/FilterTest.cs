3277da644a1ff30b75f11b0552168456eb571efe
Marc Siegmund <marc.siegmund@six-group.com>
2015-03-10 12:24:13 +0100

    created tests for provider view

---
+++ Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Provider/FilterTest.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+
+namespace Six.Scs.QA.Selenium.SmokeTest.Provider
+{
+    [TestFixture]
+    public class FilterTest
+    {
+        [TestFixtureSetUp]
+        public void Navigate()
+        {
+            TestDirector.Navigate("Provider/");
+        }
+
+        [Test]
+        public void ByName()
+        {
+        }
+
+        [Test]
+        public void ByReference()
+        {
+        }
+    }
+}
