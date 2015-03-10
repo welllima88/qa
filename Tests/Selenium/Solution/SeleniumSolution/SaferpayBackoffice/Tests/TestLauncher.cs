354b1832bc53e3dc714114f81b9e6da5fe9cf9e1
Marc Siegmund <marc.siegmund@six-group.com>
2015-03-10 08:36:04 +0100

    added TestLauncher

---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/TestLauncher.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Support.PageObjects;
+using Six.QA.Selenium.Extension.Environment;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Saferpay.QA.Selenium.Environment;
+using Six.Saferpay.QA.Selenium.Views.Common;
+
+namespace Six.Saferpay.QA.Selenium.Tests
+{
+    /// <summary>
+    ///     The TestLauncher prepares Browser with TestDirector and keeps this
+    /// </summary>
+    [SetUpFixture]
+    public class TestLauncher
+    {
+        [SetUp]
+        public void LaunchTestDirector()
+        {
+            TestDirector.TestEnvironment = new Dev();
+            TestDirector.PrepareBrowser(Grid.Local);
+            TestDirector.Start();
+
+            PageFactory.InitElements(TestDirector.WebDriver, TestDirector.TestEnvironment.Application);
+            Assert.IsTrue(MetaNavBar.LogOff.Displayed);
+        }
+
+        [TearDown]
+        public void StopTestDirector()
+        {
+            TestDirector.Stop();
+        }
+    }
+}
