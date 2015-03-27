3a394dc99f998346b6374b684591106d99790bef
Marc Siegmund <marc.siegmund@six-group.com>
2015-03-27 14:29:06 +0100

    added/changed Testlogic for Location (part of refactoring)

--- Tests/Selenium/Solution/SeleniumSolution/TestFlow/Location.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Location.cs
@@ -1,65 +1,34 @@
-﻿using NUnit.Framework;
-using Six.QA.Selenium.Extension;
-using Six.Scs.QA.Selenium.Location;
-using Six.Scs.QA.Workflow;
+﻿using Six.Scs.QA.Workflow;
+using Six.Scs.QA.Workflow.Builder;
 
 namespace Six.Scs.QA.Testlogic
 {
     public class Location
     {
-        public static TestData.ValueObjects.Location Edit(TestData.ValueObjects.Location location)
+        public static LocationBuilder Edit(TestData.ValueObjects.Location location, LocationBuilder locationBuilder)
         {
             Open(location);
-            TestData.ValueObjects.Location _location = TestData.Factory.Location.Edit();
-            Workflow.Location.Edit(_location);
-            Check(_location);
-
+            locationBuilder.Edit();
+            locationBuilder.Check();
             Lobby.OpenLatestElement();
-            Check(_location);
-            return _location;
+            locationBuilder.Check();
+            return locationBuilder;
         }
 
-        public static TestData.ValueObjects.Location Create(TestData.ValueObjects.Customer customer)
+        public static LocationBuilder Create(TestData.ValueObjects.Customer customer, LocationBuilder locationBuilder)
         {
             Customer.Open(customer);
-            TestData.ValueObjects.Location location = TestData.Factory.Location.Create();
-            Workflow.Location.Create(location);
-            Check(location);
-
+            locationBuilder.Create();
+            locationBuilder.Check();
             Lobby.OpenLatestElement();
-            Check(location);
-            return location;
+            locationBuilder.Check();
+            return locationBuilder;
         }
 
         public static void Open(TestData.ValueObjects.Location location)
         {
             Search.LocationCanBeFoundByLocationName(location.CompanyName);
             // Assert.AreEqual(location.Guid, LocationView.Guid);
-        }
-
-        /// <summary>
-        ///     Verifies the location
-        /// </summary>
-        /// <param name="l">given location data</param>
-        public static void Check(TestData.ValueObjects.Location l)
-        {
-            Assert.AreEqual(l.CompanyName, View.CompanyName);
-            StringAssert.IsMatch(TestRegExpPatterns.SbsDebitorNo, View.SbsDebitNumber);
-            // StringAssert.IsMatch(TestRegExpPatterns.SbsAdressNoOpt, LocationView.SbsAdressNumber);
-            StringAssert.IsMatch(TestRegExpPatterns.Ep2MerchantId, View.Ep2MerchantId);
-            Assert.AreEqual(l.Adress.StreetNo, View.StreetNo);
-            Assert.AreEqual(l.Adress.Po, View.Po);
-            Assert.AreEqual(l.Adress.Zip, View.Zip);
-            Assert.AreEqual(l.Adress.City, View.City);
-            StringAssert.Contains(l.Adress.Region, View.Region);
-            Assert.AreEqual(l.Adress.AdressAddition, View.AdressAddition);
-            Assert.AreEqual(l.Contact.Language, View.Language);
-            Assert.AreEqual(l.Adress.Country, View.Country);
-            Assert.AreEqual(l.Contact.Email, View.Email);
-            StringAssert.Contains(l.Contact.Telephone, View.Telephone);
-            StringAssert.Contains(l.Contact.Mobile, View.Mobile);
-            StringAssert.Contains(l.Contact.Fax, View.Fax);
-            Assert.AreEqual(l.Contact.Web, View.Web);
         }
     }
 }
