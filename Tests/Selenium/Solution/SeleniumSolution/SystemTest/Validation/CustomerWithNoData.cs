8c9b41113441f791cdaad2bdfe1cb297b3201c43
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-02 18:42:07 +0200

    added validation test (not sure if this should be in a seperate project)

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/CustomerWithNoData.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Common;
+using Six.Scs.QA.Selenium.Common.Menu;
+using Six.Scs.QA.Selenium.Customer;
+
+namespace Six.Scs.QA.Selenium.SystemTest.Validation
+{
+    [TestFixture]
+    public class CreateCustomerWithNoInput
+    {
+        [SetUp]
+        public void GoToCreateCustomer()
+        {
+            TestDirector.Navigate();
+            CustomerMenu.CustomerCreate.Click();
+            Create.Supplier = "SIX Payment Services AG";
+            Create.Confirm.Click();
+            Create.SaveButton.Click();
+        }
+
+        [Test]
+        public void CustomerName()
+        {
+            
+            Assert.That(FormAlert.FormAlertList, Contains.Item("Dies ist ein Pflichtfeld!"));
+        }
+    }
+}
