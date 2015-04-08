9dbd0cba8ccc0594ab64dbc210e0c9ac19188951
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-08 16:29:01 +0200

    added validation test for create a person with no user input

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Person/CreateWithNoInput.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Common;
+using Six.Scs.QA.Selenium.Common.Menu;
+using Six.Scs.QA.Selenium.Person;
+
+namespace Six.Scs.QA.Selenium.SystemTest.Validation.Person
+{
+    [TestFixture]
+    public class CreateWithNoInput
+    {
+        [TestFixtureSetUp]
+        public void GoToCreateContact()
+        {
+            TestDirector.Navigate("Customer/?CUSTOMERID=1");
+            CustomerMenu.ContactCreate.Click();
+            Create.SaveButton.Click();
+        }
+
+        [Test]
+        public void Name()
+        {
+            Assert.That(FormAlert.InvalidElementNames.Contains("PersonData.LastName"));
+        }
+    }
+}
