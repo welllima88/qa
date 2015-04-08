473208bea8934dc25a9309d83d2c079ea85c33a7
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-08 17:05:52 +0200

    added validation test for mpd create with no input

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Mpd/CreateWithNoInput.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Administration.Agency;
+using Six.Scs.QA.Selenium.Common;
+using Six.Scs.QA.Selenium.Common.Menu;
+using List = Six.Scs.QA.Selenium.Mpd;
+
+namespace Six.Scs.QA.Selenium.SystemTest.Validation.Mpd
+{
+    [TestFixture]
+    public class CreateWithNoInput
+    {
+        [TestFixtureSetUp]
+        public void GoToCreateContact()
+        {
+            TestDirector.Navigate("Customer/?CUSTOMERID=1");
+            CustomerMenu.AllMpds.Click();
+            List.ListView.CreateButton.Click();
+            Create.SaveButton.Click();
+        }
+
+        [Test]
+        public void Address()
+        {
+            Assert.That(FormAlert.InvalidElementNames.Contains("InetAddr"));
+        }
+
+        [Test]
+        public void Description()
+        {
+            Assert.That(FormAlert.InvalidElementNames.Contains("Description"));
+        }
+
+        [Test]
+        public void Port()
+        {
+            Assert.That(FormAlert.InvalidElementNames.Contains("InetPort"));
+        }
+    }
+}
