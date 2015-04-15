3907fd2ed1fe61482140141db3fd50df02374e40
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-15 17:09:59 +0200

    added Booking dummy data and test

---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Data/Payment/Booking.cs
@@ -0,0 +1,17 @@
+namespace Six.Saferpay.QA.Selenium.Data.Payment
+{
+    public class Booking
+    {
+        public static string ExpiryMonth;
+        public static string ExpiryYear;
+        public static string CardHolderName;
+        public static string VerificationCode;
+        public static string Currency;
+        public static string Amount;
+        public static string OrderId;
+        public static string Description;
+        public static string PayerNote;
+        public static string Pay;
+        public string CardNumber;
+    }
+}
---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Payment/Booking.cs
@@ -0,0 +1,17 @@
+using Six.QA.Selenium.Extension.Helper;
+
+namespace Six.Saferpay.QA.Selenium.Tests.Payment
+{
+    public class Booking:IPerform
+    {
+        public void Check()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Do()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
