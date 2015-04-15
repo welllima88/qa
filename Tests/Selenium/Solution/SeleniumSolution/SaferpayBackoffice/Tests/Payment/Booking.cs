e6f4490d3f4546d246082e5356180f07a58b1659
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-15 18:00:27 +0200

    create dummy test

---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Payment/Booking.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Six.Saferpay.QA.Selenium.Tests.Payment
+{
+    [TestFixture]
+    public class Booking
+    {
+        [Test]
+        public void CreateBooking()
+        {
+        }
+    }
+}
