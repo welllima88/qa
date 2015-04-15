bfb750b74750e54a9e5c4a54dd24e76314518e5d
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-15 11:23:20 +0200

    added mind maps for Smoke Test and Regression Test

---
+++ Tests/Selenium/Solution/SeleniumSolution/Documents/Regression Test.mm
@@ -0,0 +1,88 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1429089080366" ID="ID_1403822952" MODIFIED="1429089174421" TEXT="Regression Test">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1429089108174" ID="ID_1891176767" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/" MODIFIED="1429089158486" POSITION="right" TEXT="Administration">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089108174" ID="ID_1540927255" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/AgencyTest.cs" MODIFIED="1429089141986" TEXT="AgencyTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089108246" ID="ID_344051995" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Defaults/" MODIFIED="1429089156406" POSITION="right" TEXT="Defaults">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089108246" ID="ID_10965537" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Defaults/MandantIsSavedForNextCustomerCreate.cs" MODIFIED="1429089141986" TEXT="MandantIsSavedForNextCustomerCreate.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089108286" ID="ID_1729600920" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Saferpay/" MODIFIED="1429089143659" POSITION="left" TEXT="Saferpay">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089108286" ID="ID_1940585686" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Saferpay/ELinkTest.cs" MODIFIED="1429089141996" TEXT="ELinkTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089108296" ID="ID_376435183" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Saferpay/GiccTest.cs" MODIFIED="1429089141996" TEXT="GiccTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089108296" ID="ID_1601601046" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/" MODIFIED="1429089141996" POSITION="right" TEXT="Validation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089108296" ID="ID_578412158" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Agency/" MODIFIED="1429089231974" TEXT="Agency">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108296" ID="ID_1189844578" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Agency/CreateWithNoInput.cs" MODIFIED="1429089141996" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089108296" ID="ID_211007544" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Customer/" MODIFIED="1429089161416" TEXT="Customer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108296" ID="ID_173600243" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Customer/CreateWithInvalidInput.cs" MODIFIED="1429089141996" TEXT="CreateWithInvalidInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1429089108296" ID="ID_918507851" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Customer/CreateWithNoInput.cs" MODIFIED="1429089141996" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089108296" ID="ID_1710715867" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Location/" MODIFIED="1429089163371" TEXT="Location">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108296" ID="ID_1018632939" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Location/CreateWithNoInput.cs" MODIFIED="1429089142006" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089108306" ID="ID_630088912" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Mpd/" MODIFIED="1429089164884" TEXT="Mpd">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108306" ID="ID_314608974" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Mpd/CreateWithNoInput.cs" MODIFIED="1429089142006" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089108306" ID="ID_119274310" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Person/" MODIFIED="1429089165534" TEXT="Person">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108306" ID="ID_1053751805" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Person/CreateWithNoInput.cs" MODIFIED="1429089142006" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089108306" ID="ID_758429329" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/User/" MODIFIED="1429089166374" TEXT="User">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089108306" ID="ID_1832433155" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/User/CreateWithNoInput.cs" MODIFIED="1429089142006" TEXT="CreateWithNoInput.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089108306" ID="ID_634485847" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SystemTest/RegressionTest.cs" MODIFIED="1429089142016" POSITION="right" TEXT="RegressionTest.cs">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+</node>
+</node>
+</map>
---
+++ Tests/Selenium/Solution/SeleniumSolution/Documents/Smoke Test.mm
@@ -0,0 +1,200 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1429089244416" ID="ID_534108498" MODIFIED="1429089304272" TEXT="Smoke Test">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1429089274886" ID="ID_866828901" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Agency/" MODIFIED="1429089379149" POSITION="right" TEXT="Agency">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_1886152894" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Agency/AgencyCreateTest.cs" MODIFIED="1429089304212" TEXT="AgencyCreateTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_1148901284" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Agency/AgencyDeleteTest.cs" MODIFIED="1429089304212" TEXT="AgencyDeleteTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_230067443" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Agency/AgencyEditTest.cs" MODIFIED="1429089304212" TEXT="AgencyEditTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274886" ID="ID_680454027" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Article/" MODIFIED="1429089306162" POSITION="left" TEXT="Article">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_1756198315" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Article/ChooserLookupTest.cs" MODIFIED="1429089304222" TEXT="ChooserLookupTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_14348591" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Article/DirectSelectTest.cs" MODIFIED="1429089304222" TEXT="DirectSelectTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274886" ID="ID_1180227567" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Article/ViewTest.cs" MODIFIED="1429089304222" TEXT="ViewTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274906" ID="ID_220576238" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/EventLog/" MODIFIED="1429089307492" POSITION="left" TEXT="EventLog">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274906" ID="ID_199105429" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/EventLog/EventLogGuiTest.cs" MODIFIED="1429089304222" TEXT="EventLogGuiTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274906" ID="ID_255172549" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/HardwareManagement/" MODIFIED="1429089381164" POSITION="right" TEXT="HardwareManagement">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274906" ID="ID_1242447145" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/HardwareManagement/ImportTest.cs" MODIFIED="1429089304222" TEXT="ImportTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274906" ID="ID_1284349429" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/HardwareManagement/ManagementViewTest.cs" MODIFIED="1429089304222" TEXT="ManagementViewTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274916" ID="ID_195850546" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/" MODIFIED="1429089310072" POSITION="left" TEXT="Lobby">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_1925024219" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/ApplicationInfoTest.cs" MODIFIED="1429089304232" TEXT="ApplicationInfoTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_571884670" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/FooterTest.cs" MODIFIED="1429089304232" TEXT="FooterTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_919569172" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/MenuTest.cs" MODIFIED="1429089304232" TEXT="MenuTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_1147518567" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/MetaNavigationBarTest.cs" MODIFIED="1429089304232" TEXT="MetaNavigationBarTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_1983526222" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/NavigationBarTest.cs" MODIFIED="1429089304232" TEXT="NavigationBarTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274916" ID="ID_614027724" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/QuickSearchTest.cs" MODIFIED="1429089304232" TEXT="QuickSearchTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1850420218" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/RecentElementsTest.cs" MODIFIED="1429089304232" TEXT="RecentElementsTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1449656863" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Lobby/RecentMassJobsTest.cs" MODIFIED="1429089304232" TEXT="RecentMassJobsTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274926" ID="ID_615481385" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/" MODIFIED="1429089383327" POSITION="right" TEXT="Massmutation">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_903898478" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/AcquirerChangeTest.cs" MODIFIED="1429089304232" TEXT="AcquirerChangeTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1212053382" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/ContractMassmuationTest.cs" MODIFIED="1429089304232" TEXT="ContractMassmuationTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1026098683" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/CustomerMassmuationTest.cs" MODIFIED="1429089304252" TEXT="CustomerMassmuationTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1731557102" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/LocationMassmuationTest.cs" MODIFIED="1429089304262" TEXT="LocationMassmuationTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274926" ID="ID_1314288042" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Massmutation/TerminalMassmuationTest.cs" MODIFIED="1429089304262" TEXT="TerminalMassmuationTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274946" ID="ID_1447870357" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/" MODIFIED="1429089308862" POSITION="left" TEXT="Search">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274946" ID="ID_101794088" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/Customer/" MODIFIED="1429089314592" TEXT="Customer">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089274946" ID="ID_1975510629" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/Customer/ByIdTest.cs" MODIFIED="1429089304262" TEXT="ByIdTest.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+<node COLOR="#990000" CREATED="1429089274956" ID="ID_392596483" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/Customer/ByNameTest.cs" MODIFIED="1429089304262" TEXT="ByNameTest.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1429089274956" ID="ID_1450980502" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/Location/" MODIFIED="1429089313202" TEXT="Location">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1429089274956" ID="ID_1775441781" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Search/Location/ByIdTest.cs" MODIFIED="1429089304262" TEXT="ByIdTest.cs">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274956" ID="ID_1765339369" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SerialNumberImport/" MODIFIED="1429089386237" POSITION="right" TEXT="SerialNumberImport">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274956" ID="ID_1141493251" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SerialNumberImport/SerialNumberImportTest.cs" MODIFIED="1429089304262" TEXT="SerialNumberImportTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274956" ID="ID_281078641" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SerialNumberImport/SerialNumberPreviewTest.cs" MODIFIED="1429089304262" TEXT="SerialNumberPreviewTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274956" ID="ID_977072924" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SIMCard/" MODIFIED="1429089316317" POSITION="left" TEXT="SIMCard">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274956" ID="ID_1451112044" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SIMCard/SimCardViewTest.cs" MODIFIED="1429089304262" TEXT="SimCardViewTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274956" ID="ID_1133185184" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SoftwareExchange/" MODIFIED="1429089387182" POSITION="right" TEXT="SoftwareExchange">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274956" ID="ID_570384302" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SoftwareExchange/SoftwareExchangeGuiTest.cs" MODIFIED="1429089304272" TEXT="SoftwareExchangeGuiTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_147224671" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/SoftwareExchange/SoftwareExchangeTest.cs" MODIFIED="1429089304272" TEXT="SoftwareExchangeTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274966" ID="ID_1527274912" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Terminal/" MODIFIED="1429089317477" POSITION="left" TEXT="Terminal">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_1888414570" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Terminal/FoMessage.cs" MODIFIED="1429089304272" TEXT="FoMessage.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_1386454496" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Terminal/Portlets.cs" MODIFIED="1429089304272" TEXT="Portlets.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_866461105" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Terminal/Quit.cs" MODIFIED="1429089304272" TEXT="Quit.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_1119953836" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/Terminal/Replace.cs" MODIFIED="1429089304272" TEXT="Replace.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1429089274966" ID="ID_215648808" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/User/" MODIFIED="1429089388252" POSITION="right" TEXT="User">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1429089274966" ID="ID_1953001855" LINK="file:/D:/git/QA/Tests/Selenium/Solution/SeleniumSolution/SmokeTest/User/UserViewTest.cs" MODIFIED="1429089304272" TEXT="UserViewTest.cs">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
