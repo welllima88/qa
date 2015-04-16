205bf328b63c38805cd9a8c65e53674a2e57324e
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-16 13:36:12 +0200

    added payment with Reservation test

---
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Payment/Reservation.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace Six.Saferpay.QA.Selenium.Tests.Payment
+{
+    [TestFixture]
+    public class Reservation
+    {
+        [Test]
+        public void CreateReservation()
+        {
+        }
+    }
+}
