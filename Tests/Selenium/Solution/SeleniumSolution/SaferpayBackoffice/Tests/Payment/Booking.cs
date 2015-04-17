5d4eeb55ac7262161bda0998dba30e2a258cd405
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-17 08:49:19 +0200

    added navigation through menu

--- Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Payment/Booking.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SaferpayBackoffice/Tests/Payment/Booking.cs
@@ -1,10 +1,20 @@
 using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Saferpay.QA.Selenium.Pages.Menu;
 
 namespace Six.Saferpay.QA.Selenium.Tests.Payment
 {
     [TestFixture]
     public class Booking
     {
+        [TestFixtureSetUp]
+        public void GoToBooking()
+        {
+            TestDirector.Navigate();
+            Menu.Home().MySaferpay.SecurePayGate.Click();
+            Menu.Home().Payments.Booking.Click();
+        }
+
         [Test]
         public void CreateBooking()
         {
