b51c7955a1bbd3f72350c17e91b4b03b3e9a3629
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-22 14:30:42 +0200

    updated globally used locators for mass mutations

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Massmuation/FileUpload.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Massmuation/FileUpload.cs
@@ -8,32 +8,33 @@ namespace Six.Scs.QA.Selenium.Massmuation
     {
         public static string Title
         {
-            get
-            {
-                return
-                    WebDriver.FindAdaptedElement(By.CssSelector("span#ctl00_bodyContentPlaceHolder_lblMainTitle>h1"))
-                        .Text;
-            }
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content>h1")).Text; }
         }
 
         public static string Description
         {
-            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content>span>p")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content form>p")).Text; }
         }
 
         public static IWebElementAdapter FileChooser
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("ctl00_bodyContentPlaceHolder_fileUpload")); }
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("td#content input[name='batchfile']")); }
         }
 
         public static IWebElementAdapter FileUploadButton
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("ctl00_bodyContentPlaceHolder_btnUpload")); }
+            get { return WebDriver.FindAdaptedElement(By.CssSelector("button#upload")); }
         }
 
         public static string DownloadTemplate
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("ctl00_bodyContentPlaceHolder_lbDownload")).Text; }
+            get
+            {
+                return
+                    WebDriver.FindAdaptedElement(
+                        By.CssSelector("td#content form div a[href$='/Download']"))
+                        .Text;
+            }
         }
     }
 }
