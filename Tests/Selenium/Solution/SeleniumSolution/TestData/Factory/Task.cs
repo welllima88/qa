d9b07b3adf4c0e607071d986d92e052f7b3b296e
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-22 11:23:21 +0200

    added factory

---
+++ Tests/Selenium/Solution/SeleniumSolution/TestData/Factory/Task.cs
@@ -0,0 +1,22 @@
+namespace Six.Scs.QA.TestData.Factory
+{
+    public class Task
+    {
+        public static ValueObjects.Task Create()
+        {
+            return new ValueObjects.Task
+            {
+                Arguments = "Arguments /-aa ? :XX01",
+                Interval = "1",
+                MachineName = "Machine SYR Auto A",
+                MaxRetries = "11",
+                Module = "Module",
+                RetryInterval = "2",
+                TaskName = "Task SYR Auto A",
+                Type = "Executable",
+                WaitOnShutdown = "22",
+                WorkingDirectory = @"c:\create\\Task_SYR\HOME"
+            };
+        }
+    }
+}
