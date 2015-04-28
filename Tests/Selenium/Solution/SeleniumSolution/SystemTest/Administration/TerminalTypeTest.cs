acaebca531719a414d047b084a56bd52a508ec2d
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-28 20:27:29 +0200

    added test for editing TerminalTypes (to avoid bug when providers are beeing removed)

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TerminalTypeTest.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using Six.QA.Selenium.Extension.WebDriver;
+using Six.Scs.QA.Selenium.Model.ValueObjects;
+
+namespace Six.Scs.QA.Selenium.SystemTest.Administration
+{
+    [TestFixture]
+    public class TerminalTypeTest
+    {
+        [SetUp]
+        public static void Navigate()
+        {
+            TestDirector.Navigate("TerminalType?TerminalTypeId=1025");
+        }
+
+        private TerminalType _terminalType;
+        private TerminalType _originTerminalType;
+
+        [Test]
+        [Category("TerminalType"), Category("Create"), Category("Edit"), Category("Deactivate")]
+        public void EditTerminalType()
+        {
+            _originTerminalType = Testlogic.Administration.TerminalType.View();
+            _terminalType = Testlogic.Administration.TerminalType.Edit(_terminalType);
+            _terminalType = Testlogic.Administration.TerminalType.Edit(_originTerminalType); // restore state
+        }
+    }
+}
