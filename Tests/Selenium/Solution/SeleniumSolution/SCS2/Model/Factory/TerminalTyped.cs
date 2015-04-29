aa57b1b207099104c1721d5293a8ee77935d08eb
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-29 18:31:42 +0200

    added test data object

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Model/Factory/TerminalTyped.cs
@@ -0,0 +1,68 @@
+﻿namespace Six.Scs.Test.Model.Factory
+{
+    public static class TerminalType
+
+    {
+        public static ValueObjects.TerminalType XentissimoMod()
+        {
+            return new ValueObjects.TerminalType
+            {
+                Supplier = "Alle Fremdhersteller",
+                ReferenceSystem = "GICC",
+                DeviceType = "24",
+                DefaultCapabilities = "FF00FF",
+                DefaultAdditionalCapabilities = "FF00FF00FF",
+                Providers = new[]
+                {
+                    "Accarda ep2",
+                    "Aduno EP2"
+                },
+                Suppliers = new[]
+                {
+                    "CETREL S.A.",
+                    "Coop ep2"
+                }
+            };
+        }
+
+        public static ValueObjects.TerminalType XentissimoOrigin()
+        {
+            return new ValueObjects.TerminalType
+            {
+                Supplier = "Alle Fremdhersteller",
+                ReferenceSystem = "GICC",
+                DeviceType = "24",
+                DefaultCapabilities = "FF00FF",
+                DefaultAdditionalCapabilities = "FF00FF00FF",
+                Providers = new[]
+                {
+                    "CETREL S.A.",
+                    "Coop ep2",
+                    "Hypercom PayLife ACP Austria",
+                    "Migros ep2",
+                    "Paylife",
+                    "PayLife DATALAN",
+                    "PayLife Printec",
+                    "PayLife Schweiz",
+                    "PayLife Tend",
+                    "SIX Payment Services AG",
+                    "SIX Payment Services AT"
+                },
+                Suppliers = new[]
+                {
+                    "CETREL S.A.",
+                    "Coop ep2",
+                    "Hypercom PayLife ACP Austria",
+                    "Migros ep2",
+                    "Paylife",
+                    "PayLife DATALAN",
+                    "PayLife Printec",
+                    "PayLife Schweiz",
+                    "PayLife Tend",
+                    "SIX Payment Services AG",
+                    "SIX Payment Services AT"
+                }
+            };
+        }
+    }
+}
