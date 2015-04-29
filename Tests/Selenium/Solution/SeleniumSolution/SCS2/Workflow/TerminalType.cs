7bcac1c4aae33457f3d0fb778c01815bda7e45be
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-29 18:32:58 +0200

    added workflow for TerminalType Edit with helpers for setting the suppliers or providers

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/TerminalType.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Six.Scs.Test.Workflow
+{
+    public static class TerminalType
+    {
+        public static void Edit(Model.ValueObjects.TerminalType terminalType)
+        {
+            View.Administration.TerminalType.View.EditButton.Click();
+
+            View.Administration.TerminalType.Edit.Supplier = terminalType.Supplier;
+            View.Administration.TerminalType.Edit.ReferenceSystem = terminalType.ReferenceSystem;
+            View.Administration.TerminalType.Edit.DeviceType = terminalType.DeviceType;
+            View.Administration.TerminalType.Edit.DefaultCapabilities = terminalType.DefaultCapabilities;
+            View.Administration.TerminalType.Edit.DefaultAdditionalCapabilities =
+                terminalType.DefaultAdditionalCapabilities;
+            SetProviders(terminalType.Providers);
+            SetSuppliers(terminalType.Suppliers);
+
+            // View.Administration.TerminalType.Edit.SaveButton.Click();
+        }
+
+        private static void SetSuppliers(IEnumerable<string> suppliers)
+        {
+            var needed = suppliers.Intersect(View.Administration.TerminalType.Edit.AvailableSuppliers);
+            View.Administration.TerminalType.Edit.AvailableSuppliers = needed;
+
+            var not_needed = suppliers.Where(s => !View.Administration.TerminalType.Edit.SelectedSuppliers.Contains(s));
+            View.Administration.TerminalType.Edit.SelectedSuppliers = not_needed;
+        }
+
+        private static void SetProviders(IEnumerable<string> providers)
+        {
+            var needed = View.Administration.TerminalType.Edit.AvailableProviders.Intersect(providers);
+            View.Administration.TerminalType.Edit.AvailableProviders = needed;
+
+            var not_needed = providers.Where(s => !View.Administration.TerminalType.Edit.SelectedProviders.Contains(s));
+            View.Administration.TerminalType.Edit.SelectedProviders = not_needed;
+        }
+    }
+}
