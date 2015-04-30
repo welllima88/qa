5350fb07d7b6a35598091c0cfff61aee4877970e
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-30 11:48:28 +0200

    check existing lists of selected suppliers and providers (they won't be modified during test)

--- Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TerminalTypeTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TerminalTypeTest.cs
@@ -27,6 +27,10 @@ namespace Six.Scs.Test.Administration
         public void EditTerminalType()
         {
             _terminalType = Model.Factory.TerminalType.XentissimoMod();
+            // use existin configuration (for checking), due to UI problems:
+            _terminalType.Providers = _originTerminalType.Providers;
+            _terminalType.Suppliers = _originTerminalType.Suppliers;
+
             _terminalType = TerminalType.Edit(_terminalType);
         }
     }
