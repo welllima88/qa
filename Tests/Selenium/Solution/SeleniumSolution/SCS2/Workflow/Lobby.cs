8763486a531d602636fb29cfccef4948dc00fd5f
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-30 20:07:04 +0200

    implemented choose language from UI

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Lobby.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Lobby.cs
@@ -8,7 +8,7 @@ namespace Six.Scs.Test.Workflow
         public static void ChooseLanguage(string language)
         {
             MetaNavBar.Languages.Click();
-            MetaNavBar.LanguageItems.FirstOrDefault(e => e.Text.Contains(language));
+            MetaNavBar.LanguageItems.FirstOrDefault(e => e.Text.Contains(language)).Click();
         }
 
         public static void OpenLatestElement()
