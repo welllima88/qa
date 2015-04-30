181badc7b029293290bab00b5f53b723fb7e4061
Marc Siegmund <marc.siegmund@six-group.com>
2015-04-30 18:04:38 +0200

    refactored and created restor method

--- Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TerminalTypeTest.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TerminalTypeTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using Six.Test.Selenium.WebDriver;
 
 namespace Six.Scs.Test.Administration
@@ -11,12 +12,25 @@ namespace Six.Scs.Test.Administration
         {
             TestDirector.Navigate("TerminalType?TerminalTypeId=1025");
             _originTerminalType = TerminalType.View();
+
+            _terminalType = Model.Factory.TerminalType.XentissimoMod();
+            // use existin configuration (for checking), due to UI problems:
+            _terminalType.Providers = _originTerminalType.Providers;
+            _terminalType.SoftwareIds = _originTerminalType.SoftwareIds;
+            _terminalType.Suppliers = _originTerminalType.Suppliers;
+            _terminalType.Range = _originTerminalType.Range;
         }
 
         [TestFixtureTearDown]
         public void Restore()
         {
-            TerminalType.Edit(_originTerminalType);
+            _terminalType = Model.Factory.TerminalType.XentissimoOrigin();
+            // use existin configuration (for checking), due to UI problems:
+            _terminalType.Providers = _originTerminalType.Providers;
+            _terminalType.SoftwareIds = _originTerminalType.SoftwareIds;
+            _terminalType.Suppliers = _originTerminalType.Suppliers;
+            _terminalType.Range = _originTerminalType.Range;
+            TerminalType.Edit(_terminalType);
         }
 
         private Model.ValueObjects.TerminalType _terminalType;
@@ -26,13 +40,6 @@ namespace Six.Scs.Test.Administration
         [Category("TerminalType"), Category("View"), Category("Edit")]
         public void EditTerminalType()
         {
-            _terminalType = Model.Factory.TerminalType.XentissimoMod();
-            // use existin configuration (for checking), due to UI problems:
-            _terminalType.Providers = _originTerminalType.Providers;
-            _terminalType.SoftwareIds = _originTerminalType.SoftwareIds;
-            _terminalType.Suppliers = _originTerminalType.Suppliers;
-            _terminalType.Range = _originTerminalType.Range;
-
             TerminalType.Edit(_terminalType);
         }
     }
