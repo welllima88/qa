3f4e020f33e477a3c4ecc0febf3190aa42e60f2a
Marc Siegmund <marc.siegmund@six-group.com>
2015-05-22 16:28:56 +0200

    removed unnecessary constructor

--- Tests/EP2/Solution/COI/Message/Elements/ListAID.cs
+++ Tests/EP2/Solution/COI/Message/Elements/ListAID.cs
@@ -5,11 +5,6 @@ namespace SIX.SCS.QA.Tests.EP2.Message.Elements
 {
     public class ListAID
     {
-        public ListAID()
-        {
-            AID = new List<string>();
-        }
-
         [Ep2DataElement]
         public IList<string> AID { get; set; }
     }
