3f407f3cfa11c5dbf606c2ab42b795183fea760f
Marc Siegmund <marc.siegmund@six-group.com>
2015-06-09 08:43:02 +0200

    added handler for Aisd Requests

---
+++ Tests/EP2/Solution/COI/Message/Handler/AisdRequestHandler.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using Six.Scs.Ep2.SI.Config.Messages.Containers;
+using SIX.EP2.Core.ContentHandling;
+using SIX.EP2.Core.MessageHandling;
+using SIX.EP2.Core.Protocol;
+
+namespace Six.Scs.Ep2.Coi.Message.Handler
+{
+    public class AisdRequestHandler :
+        IClientSessionHandler,
+        IStartWith<ConfigDataRequest>,
+        IHandleMessage<ConfigDataResponse>,
+        IHandleMessage<ErrorNotification>
+
+    {
+        private readonly ConfigDataRequest _request;
+        public Aisd Aisd;
+        public ErrorNotification Error;
+
+        public AisdRequestHandler(ConfigDataRequest request)
+        {
+            _request = request;
+        }
+
+        public void EnrichErrorMessage(ErrorNotification errorNotification, Exception ex)
+        {
+        }
+
+        public bool IsDone
+        {
+            get { return true; }
+        }
+
+        public IMessage Respond(ConfigDataResponse response)
+        {
+            Aisd = response.Aisd;
+            return response;
+        }
+
+        public IMessage Respond(ErrorNotification err)
+        {
+            Console.Out.WriteLine("An Eror occured: [{0}] {1}", err.ErrorCode, err.ErrorDescription);
+            Error = err;
+            return err;
+        }
+
+        public void FirstMessage(ConfigDataRequest request)
+        {
+            request.ConfDataObj = _request.ConfDataObj;
+            request.ScId = _request.ScId;
+            request.AcqId = _request.AcqId;
+        }
+    }
+}
