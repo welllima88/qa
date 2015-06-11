1ee940d68cb440387856fc6981bc905ee443c119
Marc Siegmund <marc.siegmund@six-group.com>
2015-06-11 09:37:39 +0200

    added Change for delayed or now changes on the base class for Builder (re-use)

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Helper/IBuilder.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Helper/IBuilder.cs
@@ -4,6 +4,25 @@ namespace Six.Scs.Test.Workflow.Helper
 {
     public abstract class Builder : ICreate, IEdit, ICheck
     {
+        private IPerform _changeMode;
+
+        public Builder Change(IPerform changeMode)
+        {
+            _changeMode = changeMode;
+            return this;
+        }
+
+        protected void EnterChangeData()
+        {
+            if (_changeMode == null)
+            {
+                new DefaultChange().Do();
+            }
+            else
+            {
+                _changeMode.Do();
+            }
+        }
 
         public abstract void Create();
         public abstract void Edit();
