85eafad1744f1e42f011befd9a33ef9c0a59a835
Marc Siegmund <marc.siegmund@six-group.com>
2015-06-17 14:23:46 +0200

    added factory method for creating an task object when Edit

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Model/Factory/Task.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Model/Factory/Task.cs
@@ -10,12 +10,29 @@ namespace Six.Scs.Test.Model.Factory
                 Interval = "1",
                 MachineName = "Machine SYR Auto A",
                 MaxRetries = "11",
-                Module = "Module",
+                Module = "Module A",
                 RetryInterval = "2",
                 TaskName = "Task SYR Auto A",
                 Type = "Executable",
                 WaitOnShutdown = "22",
                 WorkingDirectory = @"c:\create\\Task_SYR\HOME"
+            };
+        }
+
+        public static ValueObjects.Task Edit()
+        {
+            return new ValueObjects.Task
+            {
+                Arguments = "Arguments /-bb ? :XX01",
+                Interval = "2",
+                MachineName = "Machine SYR Auto B",
+                MaxRetries = "22",
+                Module = "Module B",
+                RetryInterval = "4",
+                TaskName = "Task SYR Auto B",
+                Type = "IScheduledTask",
+                WaitOnShutdown = "44",
+                WorkingDirectory = @"c:\edit\\Task_SYR\HOME"
             };
         }
     }
