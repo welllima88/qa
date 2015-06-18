39d247b4e3bd4299d48e31f3d5eb936872effae0
Marc Siegmund <marc.siegmund@six-group.com>
2015-06-18 14:06:18 +0200

    added test for TasksScheduler

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Administration/TasksScheduler.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+using Six.Scs.Test.Model.ValueObjects;
+using Six.Test.Selenium.WebDriver;
+
+namespace Six.Scs.Test.Administration
+{
+    [TestFixture]
+    [Category("TaskScheduler")]
+    [Ignore]
+    public class TasksSchedulerTest
+    {
+        private Task _task;
+
+        [TestFixtureSetUp]
+        public void ClassInit()
+        {
+            TestDirector.Navigate("TaskScheduler/TaskSchedulerAdministration");
+        }
+
+        [Test]
+        [Category("Task"), Category("Edit"), Category("Create"), Category("Delete")]
+        public void BatchCreateButton()
+        {
+            _task = TasksScheduler.Create();
+            _task = TasksScheduler.Edit();
+            TasksScheduler.Delete(_task);
+        }
+    }
+}
