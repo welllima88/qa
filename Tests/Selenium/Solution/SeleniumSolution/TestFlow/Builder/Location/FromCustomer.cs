97e2fcbe07b9ac2027abb69189f429dd11648eef
Marc Siegmund <marc.siegmund@six-group.com>
2015-07-09 17:25:31 +0200

    use verifier to continue test execution

--- Tests/Selenium/Solution/SeleniumSolution/TestFlow/Builder/Location/FromCustomer.cs
+++ Tests/Selenium/Solution/SeleniumSolution/TestFlow/Builder/Location/FromCustomer.cs
@@ -20,23 +20,26 @@ namespace Six.Scs.Test.Builder.Location
 
         public override void Check()
         {
-            // Assert.AreEqual(Location.CompanyName, View.Location.View.CompanyName); TODO : Refactor
-            StringAssert.IsMatch(TestRegExpPatterns.SbsDebitorNo, View.Location.View.SbsDebitNumber);
-            StringAssert.IsMatch(TestRegExpPatterns.Ep2MerchantId, View.Location.View.Ep2MerchantId);
-            Assert.AreEqual(Location.Adress.StreetNo, View.Location.View.StreetNo);
-            Assert.AreEqual(Location.Adress.Po, View.Location.View.Po);
-            Assert.AreEqual(Location.Adress.Zip, View.Location.View.Zip);
-            Assert.AreEqual(Location.Adress.City, View.Location.View.City);
-            StringAssert.Contains(Location.Adress.Region, View.Location.View.Region);
-            Assert.AreEqual(Location.Adress.AdressAddition, View.Location.View.AdressAddition);
-            Assert.AreEqual(Location.Contact.Language, View.Location.View.Language);
-            Assert.AreEqual(Location.Adress.Country, View.Location.View.Country);
-            Assert.AreEqual(Location.Contact.Email, View.Location.View.Email);
-            StringAssert.Contains(Location.Contact.Telephone, View.Location.View.Telephone);
-            StringAssert.Contains(Location.Contact.Mobile, View.Location.View.Mobile);
-            StringAssert.Contains(Location.Contact.Fax, View.Location.View.Fax);
-            Assert.AreEqual(Location.Contact.Web, View.Location.View.Web);
-            Assert.AreEqual(Location.Agency, View.Location.View.Agency);
+            Verify.With(new Action[]
+            {
+                () => Assert.AreEqual(Location.CompanyName, View.Location.View.CompanyName),
+                () => StringAssert.IsMatch(TestRegExpPatterns.SbsDebitorNo, View.Location.View.SbsDebitNumber),
+                () => StringAssert.IsMatch(TestRegExpPatterns.Ep2MerchantId, View.Location.View.Ep2MerchantId),
+                () => Assert.AreEqual(Location.Adress.StreetNo, View.Location.View.StreetNo),
+                () => Assert.AreEqual(Location.Adress.Po, View.Location.View.Po),
+                () => Assert.AreEqual(Location.Adress.Zip, View.Location.View.Zip),
+                () => Assert.AreEqual(Location.Adress.City, View.Location.View.City),
+                () => StringAssert.Contains(Location.Adress.Region, View.Location.View.Region),
+                () => Assert.AreEqual(Location.Adress.AdressAddition, View.Location.View.AdressAddition),
+                () => Assert.AreEqual(Location.Contact.Language, View.Location.View.Language),
+                () => Assert.AreEqual(Location.Adress.Country, View.Location.View.Country),
+                () => Assert.AreEqual(Location.Contact.Email, View.Location.View.Email),
+                () => StringAssert.Contains(Location.Contact.Telephone, View.Location.View.Telephone),
+                () => StringAssert.Contains(Location.Contact.Mobile, View.Location.View.Mobile),
+                () => StringAssert.Contains(Location.Contact.Fax, View.Location.View.Fax),
+                () => Assert.AreEqual(Location.Contact.Web, View.Location.View.Web),
+                () => Assert.AreEqual(Location.Agency, View.Location.View.Agency)
+            }).Check();
         }
 
         protected override void EditData()
