c08ba67216b75512c017af787a705cffbe9065b3
Marc Siegmund <marc.siegmund@six-group.com>
2015-07-10 14:27:47 +0200

    added Dummy test for some experiments updated project

---
+++ Tests/Selenium/Solution/SeleniumSolution/SystemTest/Validation/Dummy.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using Six.Scs.Test.Helper;
+
+namespace Six.Scs.Test.Validation
+{
+    //[TestFixture]
+    [Ignore("For experiments only")]
+    public class Dummy
+    {
+        [Test]
+        public void Fail()
+        {
+            Verify.With(new Action[]
+            {
+                () => Assert.That(false)
+            }).Check();
+        }
+    }
+}
