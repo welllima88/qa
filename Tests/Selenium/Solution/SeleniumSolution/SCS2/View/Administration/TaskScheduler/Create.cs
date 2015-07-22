7ff0ae05af374de0045559b4f2eaa6a7a41e127b
Marc Siegmund <marc.siegmund@six-group.com>
2015-07-22 16:12:09 +0200

    corrected call via textfield element

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/View/Administration/TaskScheduler/Create.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/View/Administration/TaskScheduler/Create.cs
@@ -8,13 +8,13 @@ namespace Six.Scs.Test.View.Administration.TaskScheduler
     {
         public static string MachineName
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Machine")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Machine")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Machine")).TextField().TypeText(value); }
         }
 
         public static string TaskName
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_DisplayName")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_DisplayName")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_DisplayName")).TextField().TypeText(value); }
         }
 
@@ -26,49 +26,49 @@ namespace Six.Scs.Test.View.Administration.TaskScheduler
 
         public static string Module
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Name")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Name")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Name")).TextField().TypeText(value); }
         }
 
         public static string Arguments
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Args")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Args")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Args")).TextField().TypeText(value); }
         }
 
         public static string WorkingDirectory
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WorkingDirectory")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WorkingDirectory")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WorkingDirectory")).TextField().TypeText(value); }
         }
 
         public static string NextRun
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_NextRun")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_NextRun")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_NextRun")).TextField().TypeText(value); }
         }
 
         public static string Interval
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Interval")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Interval")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_Interval")).TextField().TypeText(value); }
         }
 
         public static string RetryInterval
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_RetryInterval")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_RetryInterval")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_RetryInterval")).TextField().TypeText(value); }
         }
 
         public static string MaxRetries
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_MaxRetries")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_MaxRetries")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_MaxRetries")).TextField().TypeText(value); }
         }
 
         public static string WaitOnShutdown
         {
-            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WaitOnShutdown")).Text; }
+            get { return WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WaitOnShutdown")).TextField().Text(); }
             set { WebDriver.FindAdaptedElement(By.Id("TaskScheduler_WaitOnShutdown")).TextField().TypeText(value); }
         }
 
