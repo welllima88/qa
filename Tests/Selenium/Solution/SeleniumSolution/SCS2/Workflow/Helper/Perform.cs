70df2409f32b8799a1035a0f1336c698a05d746e
Marc Siegmund <marc.siegmund@six-group.com>
2015-08-07 11:14:47 +0200

    made public

--- Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Helper/Perform.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/Workflow/Helper/Perform.cs
@@ -1,6 +1,6 @@
 namespace Six.Scs.Test.Workflow.Helper
 {
-    abstract class Perform : IPerform
+    public abstract class Perform : IPerform
     {
         public abstract void Check();
         public abstract void Do();
