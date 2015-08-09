76a32db743cad8fc3e5beabf9cebd0ea8ae2ccf6
Marc Siegmund <marc.siegmund@six-group.com>
2015-08-09 22:20:18 +0200

    grid changes, only from build server now

--- Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Environment/Grid.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SeleniumExtension/Environment/Grid.cs
@@ -2,19 +2,9 @@
 {
     public static class Grid
     {
-        public static string OldBuild
-        {
-            get { return "http://wkbuild03:4488/wd/hub"; }
-        }
-
         public static string Build
         {
             get { return "http://build:4488/wd/hub"; }
-        }
-
-        public static string Local
-        {
-            get { return "http://localhost:4488/wd/hub"; }
         }
 
         public static string NoGrid
