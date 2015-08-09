15d3cda231ffdd3fd4e64b0f59bb334526f07cd3
Marc Siegmund <marc.siegmund@six-group.com>
2015-08-10 01:16:17 +0200

    added brand object to fetch all rows with each column linked to inputs

---
+++ Tests/Selenium/Solution/SeleniumSolution/SCS2/View/AcquirerExchange/Brand.cs
@@ -0,0 +1,15 @@
+using Six.Test.Selenium.WebDriver.WebElements;
+
+namespace Six.Scs.Test.View.AcquirerExchange
+{
+    public class Brand
+    {
+        public TextFieldElement ContractNumber;
+        public SelectElement DccProvider;
+        public string Id;
+        public SelectElement Mcc;
+        public WebElement Name;
+        public SelectElement NewBrand;
+        public CheckBoxElement Selected;
+    }
+}
