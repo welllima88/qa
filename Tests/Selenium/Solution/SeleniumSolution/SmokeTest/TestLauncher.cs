b47d3ebe9beff254001da64fe812b89c2826dcf9
Marc Siegmund <marc.siegmund@six-group.com>
2015-09-03 17:41:37 +0200

    updated namespace

--- Tests/Selenium/Solution/SeleniumSolution/SmokeTest/TestLauncher.cs
+++ Tests/Selenium/Solution/SeleniumSolution/SmokeTest/TestLauncher.cs
@@ -1,6 +1,6 @@
 ﻿using NUnit.Framework;
-using Six.Scs.Test.View.Common;
-using Six.Scs.Test.View.Environments;
+using Six.Scs.Test.UI.Common;
+using Six.Scs.Test.UI.Environments;
 using Six.Test.Selenium.Environment;
 using Six.Test.Selenium.WebDriver;
 
